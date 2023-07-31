91169e9ad787e9b08606216d1bdc660acfaf9506
Tobias Kampmann <tobias.kampmann@tngtech.com>
2023-07-31 13:32:27 +0200

    feat(backend): initial commit

---
+++ backend/src/test/kotlin/org/pathoplexus/backend/BackendApplicationTest.kt
@@ -0,0 +1,12 @@
+package org.pathoplexus.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTest {
+
+	@Test
+	fun contextLoads() {
+	}
+}
