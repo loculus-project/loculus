deb150cf970bddf9249382ddb3c23cf7d67944e1
Tobias Kampmann <tobias.kampmann@tngtech.com>
2023-08-02 17:43:02 +0200

    feat(backend): add endpoint for getting unprocessed data
    * respond with streamed ndjson

--- backend/src/main/resources/database/init.sql
+++ backend/src/main/resources/database/init.sql
@@ -2,6 +2,7 @@ create table sequences (
     sequence_id bigserial primary key,
     submitter text not null,
     submitted_at timestamp not null,
+    started_processing_at timestamp,
     status text not null,
     original_data jsonb not null
 );
