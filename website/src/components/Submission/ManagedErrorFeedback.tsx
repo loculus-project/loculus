eb0cd76aa257257611115bd2fd1faa0134818f5a
Tobias Kampmann <tobias.kampmann@tngtech.com>
2023-08-14 17:18:44 +0200

    feat(website): add submission page

---
+++ website/src/components/Submission/ManagedErrorFeedback.tsx
@@ -0,0 +1,26 @@
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import type { FC } from 'react';
+
+type ErrorFeedbackProps = {
+    message: string;
+    open: boolean;
+    onClose: () => void;
+};
+
+export const ManagedErrorFeedback: FC<ErrorFeedbackProps> = ({ message, open, onClose }) => {
+    const action = (
+        <Button color='secondary' size='small' onClick={onClose} sx={{ color: 'white' }}>
+            CLOSE
+        </Button>
+    );
+    return (
+        <Snackbar
+            open={open}
+            anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
+            message={message}
+            action={action}
+            onClose={onClose}
+        />
+    );
+};
