f11efc391805b8d7d21a012c79f3a05bb87085ff
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-09-21 14:31:26 +0200

    build(website): reduce Docker image size by a bit and improve it's config #191

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind(), react()],
-  adapter: node({
-    mode: 'standalone'
-  }),
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
+    output: 'server',
+    integrations: [tailwind(), react()],
+    adapter: node({
+        mode: 'standalone',
+    }),
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+    },
 });
