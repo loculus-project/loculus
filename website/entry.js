414ab97afe7bf00cf8b0b0c2b7e9e319e8807117
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-10-06 15:22:37 +0200

    feat(website): configure a logger for the express server

--- website/entry.js
+++ website/entry.js
@@ -4,8 +4,31 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import path from 'path';
 import fs from 'fs';
 import 'dotenv/config';
+import winston from 'winston';
 
 const configDir = process.env.CONFIG_DIR;
+const logDir = process.env.LOG_DIR;
+
+if (logDir === undefined) {
+    console.log('No logger configured. Set environment variable LOG_DIR to enable logging.');
+}
+
+function logProvider() {
+    if (logDir === undefined) {
+        return {
+            log: () => {},
+            debug: () => {},
+            info: () => {},
+            warn: () => {},
+            error: () => {},
+        };
+    }
+    return winston.createLogger({
+        level: 'info',
+        format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        transports: new winston.transports.File({ filename: `${logDir}/website.log` }),
+    });
+}
 
 if (typeof configDir !== 'string' || configDir === '') {
     throw new Error('CONFIG_DIR environment variable is not set');
@@ -19,6 +42,7 @@ const backendProxy = createProxyMiddleware('/backendProxy/**', {
     pathRewrite: {
         '^/backendProxy/': '/',
     },
+    logProvider,
 });
 
 const lapisProxy = createProxyMiddleware('/lapisProxy/**', {
@@ -27,11 +51,25 @@ const lapisProxy = createProxyMiddleware('/lapisProxy/**', {
     pathRewrite: {
         '^/lapisProxy/': '/',
     },
+    logProvider,
 });
 
 const app = express();
 
 const base = '/';
+
+const logger = logProvider();
+if (logger !== undefined) {
+    app.use((req, res, next) => {
+        if (req.url.startsWith('/_astro/')) {
+            logger.debug(`Request: ${req.url}`);
+        } else {
+            logger.info(`Request: ${req.url}`);
+        }
+        next();
+    });
+}
+
 app.use(ssrHandler);
 app.use(base, express.static('dist/client/'), backendProxy, lapisProxy);
 
