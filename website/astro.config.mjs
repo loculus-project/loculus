21208fa27ed27d8c043afbff6698d2a255241d58
Theo Sanderson <theo@theo.io>
2023-10-20 16:11:41 +0100

    exclude fsevents from optimisedeps

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -15,4 +15,10 @@ export default defineConfig({
         port: 3000,
         host: '0.0.0.0',
     },
+    vite: {
+        optimizeDeps: {
+            exclude: ['fsevents'],
+        },
+    
+    },
 });
