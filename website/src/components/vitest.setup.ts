f80a3fdc90e57a9a7cb0862a884f4797392df2ca
kellerer <jonas.kellerer@tngtechc.om>
2023-10-25 17:17:33 +0200

    Revert "fix(deps-dev): fix mock request in vitest.setup.ts"
    This reverts commit 6876575de0221da06be9e5cace55248da6dd947a.

--- website/src/components/vitest.setup.ts
+++ website/src/components/vitest.setup.ts
@@ -1,7 +1,7 @@
 // Extend Jest "expect" functionality with Testing Library assertions.
 import '@testing-library/jest-dom';
 
-import { http, HttpResponse } from 'msw';
+import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { afterAll, afterEach, beforeAll } from 'vitest';
 
@@ -25,8 +25,8 @@ export const testServer = setupServer();
 export const mockRequest = {
     submit: (statusCode: number = 200, response: any = []) => {
         testServer.use(
-            http.post(`${testConfig.forServer.backendUrl}/submit`, () => {
-                return HttpResponse.json(response, { status: statusCode });
+            rest.post(`${testConfig.forServer.backendUrl}/submit`, (_, res, ctx) => {
+                return res(ctx.status(statusCode), ctx.json(response));
             }),
         );
     },
