5b77fea31a5215da4a792a05adff86b17838cf82
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-07 14:53:14 +0100

    feat(backend): validate that the only nucleotide segment is called "main"
    https://github.com/pathoplexus/pathoplexus/pull/478#discussion_r1384804533

---
+++ backend/src/test/kotlin/org/pathoplexus/backend/config/ReferenceGenomeTest.kt
@@ -0,0 +1,25 @@
+package org.pathoplexus.backend.config
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsString
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ReferenceGenomeTest {
+    @Test
+    fun `GIVEN single nucleotide segment is not called main THEN throws exception`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            ReferenceGenome(
+                listOf(
+                    ReferenceSequence(
+                        name = "not main",
+                        sequence = "does not matter",
+                    ),
+                ),
+                emptyList(),
+            )
+        }
+
+        assertThat(exception.message, containsString("must be named 'main'"))
+    }
+}
