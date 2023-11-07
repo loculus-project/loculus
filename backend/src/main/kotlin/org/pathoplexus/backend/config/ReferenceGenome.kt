a3ad4b4064e8a19b7a1ff54ac3abd4e187dcea80
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-07 14:53:14 +0100

    feat(backend): validate that the only nucleotide segment is called "main"
    https://github.com/pathoplexus/pathoplexus/pull/478#discussion_r1384804533

--- backend/src/main/kotlin/org/pathoplexus/backend/config/ReferenceGenome.kt
+++ backend/src/main/kotlin/org/pathoplexus/backend/config/ReferenceGenome.kt
@@ -5,7 +5,13 @@ import org.pathoplexus.backend.api.NucleotideSequence
 data class ReferenceGenome(
     val nucleotideSequences: List<ReferenceSequence>,
     val genes: List<ReferenceSequence>,
-)
+) {
+    init {
+        if (nucleotideSequences.size == 1 && nucleotideSequences.single().name != "main") {
+            throw IllegalArgumentException("If there is only one nucleotide sequence, it must be named 'main'")
+        }
+    }
+}
 
 data class ReferenceSequence(
     val name: String,
