9273594917ee3de0125c42496cb4080ca043d14b
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-14 10:14:28 +0100

    tests: don't fail while collecting kubernetes logs #108

---
+++ .github/scripts/collect_kubernetes_logs.sh
@@ -0,0 +1,9 @@
+pods=$(kubectl get pods -l app=pathoplexus -n pathoplexus -o jsonpath='{.items[*].metadata.name}' || true)
+for pod in $pods; do
+  containers=$(kubectl get pod "$pod" -n pathoplexus -o jsonpath='{.spec.containers[*].name}' || true)
+  for container in $containers; do
+    echo "Logs from $pod - $container:" >> kubernetes_logs.txt
+    kubectl logs "$pod" -n pathoplexus -c "$container" >> kubernetes_logs.txt 2>/dev/null || true
+    echo "==============================" >> kubernetes_logs.txt
+  done
+done
