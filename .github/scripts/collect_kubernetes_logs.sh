61d53590b530468ec1a9c0999771578f846215fa
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-21 16:54:42 +0100

    ci: store logs in separate files #570

--- .github/scripts/collect_kubernetes_logs.sh
+++ .github/scripts/collect_kubernetes_logs.sh
@@ -6,8 +6,9 @@ pods=$(kubectl get pods -l app=pathoplexus -n pathoplexus -o jsonpath='{.items[*
 for pod in $pods; do
   containers=$(kubectl get pod "$pod" -n pathoplexus -o jsonpath='{.spec.containers[*].name}' || true)
   for container in $containers; do
-    echo "Logs from $pod - $container:" >> kubernetes_logs.txt
-    kubectl logs "$pod" -n pathoplexus -c "$container" >> kubernetes_logs.txt 2>/dev/null || true
-    echo "==============================" >> kubernetes_logs.txt
+    mkdir "kubernetes_logs" -p
+    file="kubernetes_logs/$pod-$container.txt"
+    echo "Logs from $pod - $container:" >> "$file"
+    kubectl logs "$pod" -n pathoplexus -c "$container" >> "$file" 2>/dev/null || true
   done
 done
