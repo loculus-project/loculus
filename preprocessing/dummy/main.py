loculus-project/loculus

commit 5c3a4e01ee81d06d8e7ebc36ab51eca5f733fe4f

refactor: renaming #523

preprocessing/dummy/main.py
@@ -14,7 +14,7 @@ parser.add_argument("--backend-host", type=str, default="127.0.0.1:8079",
 parser.add_argument("--watch", action="store_true", help="Watch and keep running. Fetches new data every 10 seconds.")
 parser.add_argument("--withErrors", action="store_true", help="Add errors to processed data.")
 parser.add_argument("--withWarnings", action="store_true", help="Add warnings to processed data.")
-parser.add_argument("--maxSequences", type=int, help="Max number of sequences to process.")
+parser.add_argument("--maxSequences", type=int, help="Max number of sequence entry versions to process.")
 
 args = parser.parse_args()
 host = "http://{}".format(args.backend_host)
@@ -34,7 +34,7 @@ class ProcessingAnnotation:
 
 @dataclass
 class Sequence:
-    sequenceId: int
+    accession: int
     version: int
     data: dict
     errors: Optional[List[ProcessingAnnotation]] = field(default_factory=list)
@@ -56,7 +56,7 @@ def parse_ndjson(ndjson_data: str) -> List[Sequence]:
     for json_str in json_strings:
         if json_str:
             json_object = json.loads(json_str)
-            entries.append(Sequence(json_object["sequenceId"], json_object["version"], json_object["data"]))
+            entries.append(Sequence(json_object["accession"], json_object["version"], json_object["data"]))
     return entries
 
 def process(unprocessed: List[Sequence]) -> List[Sequence]:
@@ -70,7 +70,7 @@ def process(unprocessed: List[Sequence]) -> List[Sequence]:
         metadata["pango_lineage"] = random.choice(possible_lineages)
 
         updated_sequence = Sequence(
-            sequence.sequenceId,
+            sequence.accession,
             sequence.version,
             {"metadata": metadata, **mock_sequences},
         )

preprocessing/nextclade/main.py
@@ -36,7 +36,7 @@ GENES = [
 ]
 
 
-IdVersion = str
+AccessionVersion = str
 
 
 @dataclass
@@ -47,7 +47,7 @@ class UnprocessedData:
 
 @dataclass
 class UnprocessedEntry:
-    idVersion: IdVersion  # {sequenceId}.{version}
+    accessionVersion: AccessionVersion  # {accession}.{version}
     data: UnprocessedData
 
 
@@ -75,7 +75,7 @@ class ProcessingAnnotation:
 
 @dataclass
 class ProcessedEntry:
-    sequenceId: int
+    accession: int
     version: int
     data: ProcessedData
     errors: Optional[List[ProcessingAnnotation]] = field(default_factory=list)
@@ -112,7 +112,7 @@ def parse_ndjson(ndjson_data: str) -> Sequence[UnprocessedEntry]:
             ],
         )
         entry = UnprocessedEntry(
-            idVersion=f"{json_object['sequenceId']}.{json_object['version']}",
+            accessionVersion=f"{json_object['accession']}.{json_object['version']}",
             data=unprocessed_data,
         )
         entries.append(entry)
@@ -121,13 +121,13 @@ def parse_ndjson(ndjson_data: str) -> Sequence[UnprocessedEntry]:
 
 def run_nextclade(
     unprocessed: Sequence[UnprocessedEntry], dataset_dir: str
-) -> Mapping[IdVersion, NextcladeResult]:
+) -> Mapping[AccessionVersion, NextcladeResult]:
     with tempfile.TemporaryDirectory() as result_dir:
         # TODO: Generalize for multiple segments (flu)
         input_file = result_dir + "/input.fasta"
         with open(input_file, "w") as f:
             for sequence in unprocessed:
-                f.write(f">{sequence.idVersion}\n")
+                f.write(f">{sequence.accessionVersion}\n")
                 f.write(f"{sequence.data.unalignedNucleotideSequences['main']}\n")
         command = (
             "nextclade run "
@@ -139,7 +139,7 @@ def run_nextclade(
         if exit_code != 0:
             raise Exception("nextclade failed with exit code {}".format(exit_code))
         processed = {
-            unprocessed_sequence.idVersion: {
+            unprocessed_sequence.accessionVersion: {
                 "unalignedNuc": unprocessed_sequence.data.unalignedNucleotideSequences,
                 "alignedNuc": "",
                 "alignedTranslations": {},
@@ -185,11 +185,11 @@ def run_nextclade(
         return processed
 
 
-def id_from_str(id_str: IdVersion) -> int:
+def id_from_str(id_str: AccessionVersion) -> int:
     return int(id_str.split(".")[0])
 
 
-def version_from_str(id_str: IdVersion) -> int:
+def version_from_str(id_str: AccessionVersion) -> int:
     return int(id_str.split(".")[1])
 
 
@@ -199,7 +199,7 @@ def process(
     nextclade_results = run_nextclade(unprocessed, dataset_dir)
     processed = [
         ProcessedEntry(
-            sequenceId=id_from_str(sequence_id),
+            accession=id_from_str(sequence_id),
             version=version_from_str(sequence_id),
             data=ProcessedData(
                 metadata=nextclade_results[sequence_id]["metadata"],

commit f39e117f0d49af778d0ce10548292b71b8f2c6ac

fix: numberOfSequences -> numberOfSequenceEntries renaming
Caused by #527

preprocessing/dummy/main.py
@@ -43,7 +43,7 @@ class Sequence:
 
 def fetch_unprocessed_sequences(n: int) -> List[Sequence]:
     url = host + "/extract-unprocessed-data"
-    params = {"numberOfSequences": n}
+    params = {"numberOfSequenceEntries": n}
     response = requests.post(url, data=params)
     if not response.ok:
         raise Exception("Fetching unprocessed data failed. Status code: {}".format(response.status_code), response.text)

preprocessing/nextclade/main.py
@@ -87,7 +87,7 @@ NextcladeResult = Mapping[str, str]
 
 def fetch_unprocessed_sequences(n: int) -> Sequence[UnprocessedEntry]:
     url = host + "/extract-unprocessed-data"
-    params = {"numberOfSequences": n}
+    params = {"numberOfSequenceEntries": n}
     response = requests.post(url, data=params)
     if not response.ok:
         raise Exception(