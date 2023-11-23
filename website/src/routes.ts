98d78b94e5c9a13e306e9d2ba5720842a0425a13
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2023-11-23 09:20:35 +0100

    refactor(website): extract navigation items #543

--- website/src/routes.ts
+++ website/src/routes.ts
@@ -1,6 +1,45 @@
 import type { AccessionVersion } from './types/backend.ts';
 import type { Filter } from './types/config.ts';
 
+export const navigationItems = {
+    top: [
+        {
+            text: 'Search',
+            path: '/search',
+        },
+        {
+            text: 'Submit',
+            path: '/submit',
+        },
+        {
+            text: 'Revise',
+            path: '/revise',
+        },
+        {
+            text: 'User',
+            path: '/user',
+        },
+    ],
+    bottom: [
+        {
+            text: 'About',
+            path: '/about',
+        },
+        {
+            text: 'Api documentation',
+            path: '/api_documentation',
+        },
+        {
+            text: 'Governance',
+            path: '/governance',
+        },
+        {
+            text: 'Status',
+            path: '/status',
+        },
+    ],
+};
+
 export const routes = {
     searchPage: (searchFilter: Filter[] = [], page: number = 1) =>
         `/search?${buildSearchParams(searchFilter, page).toString()}`,
