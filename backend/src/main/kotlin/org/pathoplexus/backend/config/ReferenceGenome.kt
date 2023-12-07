9bad519c6a64d9af7449b14b84123dccbc4add30
Tobias Kampmann <tobias.kampmann@tngtech.com>
2023-12-07 16:48:15 +0100

    feat(backend): compress amino acid sequences in processed data

--- backend/src/main/kotlin/org/pathoplexus/backend/config/ReferenceGenome.kt
+++ backend/src/main/kotlin/org/pathoplexus/backend/config/ReferenceGenome.kt
@@ -1,5 +1,6 @@
 package org.pathoplexus.backend.config
 
+import org.pathoplexus.backend.api.AminoAcidSequence
 import org.pathoplexus.backend.api.NucleotideSequence
 
 data class ReferenceGenome(
@@ -15,9 +16,13 @@ data class ReferenceGenome(
     fun getNucleotideSegmentReference(segmentName: String): NucleotideSequence? = nucleotideSequences.find {
         it.name == segmentName
     }?.sequence
+
+    fun getAminoAcidGeneReference(gene: String): AminoAcidSequence? = genes.find {
+        it.name == gene
+    }?.sequence
 }
 
 data class ReferenceSequence(
     val name: String,
-    val sequence: NucleotideSequence,
+    val sequence: String,
 )
