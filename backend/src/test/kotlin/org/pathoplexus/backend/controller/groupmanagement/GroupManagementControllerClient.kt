dc17234be5e9ea857c5b36d6243021543742a47b
Tobias Kampmann <tobias.kampmann@tngtech.com>
2023-12-18 16:57:05 +0100

    feat(backend): create `GET /groups` endpoint to query a list of groups of a user

--- backend/src/test/kotlin/org/pathoplexus/backend/controller/groupmanagement/GroupManagementControllerClient.kt
+++ backend/src/test/kotlin/org/pathoplexus/backend/controller/groupmanagement/GroupManagementControllerClient.kt
@@ -4,18 +4,24 @@ import org.pathoplexus.backend.controller.jwtForDefaultUser
 import org.pathoplexus.backend.controller.withAuth
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
 class GroupManagementControllerClient(private val mockMvc: MockMvc) {
     fun createNewGroup(groupName: String = DEFAULT_GROUP_NAME, jwt: String? = jwtForDefaultUser): ResultActions =
         mockMvc.perform(
-            MockMvcRequestBuilders.post("/groups/$groupName")
+            post("/groups/$groupName")
                 .withAuth(jwt),
         )
 
     fun getDetailsOfGroup(groupName: String = DEFAULT_GROUP_NAME, jwt: String? = jwtForDefaultUser): ResultActions =
         mockMvc.perform(
-            MockMvcRequestBuilders.get("/groups/$groupName")
+            get("/groups/$groupName")
                 .withAuth(jwt),
         )
+
+    fun getGroupsOfUser(jwt: String? = jwtForDefaultUser): ResultActions = mockMvc.perform(
+        get("/groups")
+            .withAuth(jwt),
+    )
 }
--- backend/src/test/kotlin/org/pathoplexus/backend/controller/groupmanagement/GroupManagementControllerTest.kt
+++ backend/src/test/kotlin/org/pathoplexus/backend/controller/groupmanagement/GroupManagementControllerTest.kt
@@ -2,11 +2,14 @@ package org.pathoplexus.backend.controller.groupmanagement
 
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import org.pathoplexus.backend.controller.expectUnauthorizedResponse
 import org.pathoplexus.backend.controller.submission.DEFAULT_USER_NAME
 import org.pathoplexus.backend.controller.submission.EndpointTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -18,10 +21,11 @@ class GroupManagementControllerTest(
     @Autowired private val client: GroupManagementControllerClient,
 ) {
 
-    @Test
-    fun `GIVEN invalid authorization token THEN returns 401 Unauthorized`() {
-        expectUnauthorizedResponse(isModifyingRequest = true) {
-            client.createNewGroup(jwt = it)
+    @ParameterizedTest
+    @MethodSource("authorizationTestCases")
+    fun `GIVEN invalid authorization token WHEN performing action THEN returns 401 Unauthorized`(scenario: Scenario) {
+        expectUnauthorizedResponse(isModifyingRequest = scenario.isModifying) {
+            scenario.testFunction(it, client)
         }
     }
 
@@ -51,5 +55,38 @@ class GroupManagementControllerTest(
             .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
             .andExpect(jsonPath("\$.detail").value("Group does not exist"))
             .andReturn()
+    }
+
+    @Test
+    fun `GIVEN WHEN I query details of a non-existing group THEN expect a 404`() {
+        client.getDetailsOfGroup()
+            .andExpect(status().isNotFound())
+            .andReturn()
+    }
+
+    @Test
+    fun `GIVEN a group is created WHEN I query the groups of the user THEN expect that the group is returned`() {
+        client.createNewGroup()
+            .andExpect(status().isNoContent())
+
+        client.getGroupsOfUser()
+            .andExpect(status().isOk())
+            .andExpect { jsonPath("\$.size()", `is`(1)) }
+            .andExpect { jsonPath("\$[0].groupName", `is`(DEFAULT_GROUP_NAME)) }
+            .andReturn()
+    }
+
+    companion object {
+        data class Scenario(
+            val testFunction: (String?, GroupManagementControllerClient) -> ResultActions,
+            val isModifying: Boolean,
+        )
+
+        @JvmStatic
+        fun authorizationTestCases(): List<Scenario> = listOf(
+            Scenario({ jwt, client -> client.createNewGroup(jwt = jwt) }, true),
+            Scenario({ jwt, client -> client.getDetailsOfGroup(jwt = jwt) }, false),
+            Scenario({ jwt, client -> client.getGroupsOfUser(jwt = jwt) }, false),
+        )
     }
 }
