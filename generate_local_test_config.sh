2880a31329819856cb432401c75056d0345752d1
Chaoran Chen <mail@chaoran-chen.de>
2024-01-04 21:20:53 +0100

    fix: generate_local_test_config.sh

--- generate_local_test_config.sh
+++ generate_local_test_config.sh
@@ -7,7 +7,7 @@ helmChart="$root"/kubernetes/loculus
 
 backend_config="$root"/website/tests/config/backend_config.json
 helm template name-does-not-matter "$helmChart" --show-only templates/loculus-backend-config.yaml | \
- yq eval '.data."backend_config.json"' - > "backend_config"
+ yq eval '.data."backend_config.json"' - > "$backend_config"
 echo "wrote backend config to $backend_config"
 
 website_config="$root"/website/tests/config/website_config.json
