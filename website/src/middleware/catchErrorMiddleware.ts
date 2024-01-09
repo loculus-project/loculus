71337ed8f70d0552f0024d0595961ad3e5e11cc2
Tobias Kampmann <tobias.kampmann@tngtech.com>
2024-01-09 17:07:43 +0100

    fix(website): error handling middleware
     * configure dev flow properly

---
+++ website/src/middleware/catchErrorMiddleware.ts
@@ -0,0 +1,14 @@
+import { defineMiddleware } from 'astro/middleware';
+
+import { getInstanceLogger } from '../logger.ts';
+
+const logger = getInstanceLogger('ErrorMiddleware');
+
+export const catchErrorMiddleware = defineMiddleware(async (context, next) => {
+    try {
+        return next();
+    } catch (error) {
+        logger.error(`Error for path (${context.url.pathname}): ${error}`);
+        return context.redirect('/500');
+    }
+});
