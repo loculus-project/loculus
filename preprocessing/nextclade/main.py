7449e517d28819f63c6deae88e2f916e7f63d7ca
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-01-18 21:02:17 +0100

    Configure real virus instance for test purposes
    Add "clade" field
    
    Disable dummy preprocessing for manual testing
    
    Add insdc ingest user to keycloak
    
    Include stop codon in translation (nextclade does so)
    
    fix: dateToSortBy -> collection_date
    
    fix: partitionBy: pango_lineage, see whether this works without pango lineages...
    
    ok that failed, try without partitionBy
    
    Reduce metadata to minimum in config for easier testing
    
    wip
    
    wip: preprocessing & ingest (for mpox for now)
    
    Create mpox instance _with_ basic automatic preprocessing
    
    fix: add organism CLI arg

--- preprocessing/nextclade/main.py
+++ preprocessing/nextclade/main.py
@@ -1,51 +1,83 @@
 import argparse
 import dataclasses
 import json
+import logging
 import os
-import tempfile
+import time
+import requests
+import yaml
+from Bio import SeqIO
 from collections.abc import Mapping, Sequence
 from dataclasses import dataclass, field
 from typing import List, Literal, Optional
+from urllib.parse import urljoin
+from tempfile import TemporaryDirectory
 
-import requests
-from Bio import SeqIO
+
+@dataclass
+class Config:
+    backend_host: str = "http://127.0.0.1:8079"
+    keycloak_host: str = "http://172.0.0.1:8083"
+    keycloak_user: str = "dummy_preprocessing_pipeline"
+    keycloak_password: str = "dummy_preprocessing_pipeline"
+    keycloak_token_path: str = "realms/loculusRealm/protocol/openid-connect/token"
+    nextclade_dataset_name: str = "nextstrain/mpox/all-clades"
+    nextclade_dataset_version: str = "2024-01-16--20-31-02Z"
+    config_file: Optional[str] = None
+    organism: str = "dummy-organism"
+    log_level: str = "DEBUG"
+    genes: dict[str, int] = field(default_factory=dict)
+    keep_tmp_dir: bool = False
+    reference_length: int = 197209
+    batch_size: int = 5
+
+
+def load_config_from_yaml(config_file: str, config: Config):
+    with open(config_file, "r") as file:
+        yaml_config = yaml.safe_load(file)
+    for key, value in yaml_config.items():
+        if hasattr(config, key):
+            setattr(config, key, value)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--backend-host",
     type=str,
-    default="127.0.0.1",
     help="Host address of the Loculus backend",
 )
-parser.add_argument("--keycloak-host", type=str, default="http://172.0.0.1:8083", help="Host address of Keycloak")
-parser.add_argument("--keycloak-user", type=str, default="dummy_preprocessing_pipeline",
-                    help="Keycloak user to use for authentication")
-parser.add_argument("--keycloak-password", type=str, default="dummy_preprocessing_pipeline",
-                    help="Keycloak password to use for authentication")
-parser.add_argument("--keycloak-token-path", type=str, default="/realms/loculusRealm/protocol/openid-connect/token",
-                    help="Path to Keycloak token endpoint")
+parser.add_argument("--keycloak-host", type=str, help="Host address of Keycloak")
+parser.add_argument(
+    "--keycloak-user", type=str, help="Keycloak user to use for authentication"
+)
+parser.add_argument(
+    "--keycloak-password", type=str, help="Keycloak password to use for authentication"
+)
+parser.add_argument(
+    "--keycloak-token-path", type=str, help="Path to Keycloak token endpoint"
+)
+parser.add_argument("--config-file", type=str, help="Path to config file")
+parser.add_argument("--log-level", type=str, help="Log level")
+parser.add_argument("--keep-tmp-dir", action="store_true", help="Keep tmp dir")
+parser.add_argument("--organism", type=str, help="Organism")
+
+# Config precedence: CLI args > config file > default
 
 args = parser.parse_args()
-host = "http://{}:8079".format(args.backend_host)
-keycloakHost = args.keycloak_host
-keycloakUser = args.keycloak_user
-keycloakPassword = args.keycloak_password
-keycloakTokenPath = args.keycloak_token_path
 
-GENES = [
-    "ORF1a",
-    "ORF1b",
-    "S",
-    "ORF3a",
-    "E",
-    "M",
-    "ORF6",
-    "ORF7a",
-    "ORF7b",
-    "ORF8",
-    "N",
-    "ORF9b",
-]
+config = Config()
+
+if args.config_file:
+    load_config_from_yaml(args.config_file, config)
+
+for key, value in vars(args).items():
+    if value is not None and hasattr(config, key):
+        setattr(config, key, value)
+# Use the final configuration values
+
+logging.basicConfig(level=config.log_level)
+
+logging.info("Using config: {}".format(config))
 
 AccessionVersion = str
 
@@ -97,9 +129,9 @@ NextcladeResult = Mapping[str, str]
 
 
 def fetch_unprocessed_sequences(n: int) -> Sequence[UnprocessedEntry]:
-    url = host + "/extract-unprocessed-data"
+    url = urljoin(config.backend_host, f"{config.organism}/extract-unprocessed-data")
     params = {"numberOfSequenceEntries": n}
-    headers = {'Authorization': 'Bearer ' + get_jwt()}
+    headers = {"Authorization": "Bearer " + get_jwt()}
     response = requests.post(url, data=params, headers=headers)
     if not response.ok:
         raise Exception(
@@ -132,30 +164,74 @@ def parse_ndjson(ndjson_data: str) -> Sequence[UnprocessedEntry]:
 
 
 def run_nextclade(
-        unprocessed: Sequence[UnprocessedEntry], dataset_dir: str
+    unprocessed: Sequence[UnprocessedEntry], dataset_dir: str
 ) -> Mapping[AccessionVersion, NextcladeResult]:
-    with tempfile.TemporaryDirectory() as result_dir:
+    with TemporaryDirectory(delete=not config.keep_tmp_dir) as result_dir:
         # TODO: Generalize for multiple segments (flu)
         input_file = result_dir + "/input.fasta"
         with open(input_file, "w") as f:
             for sequence in unprocessed:
                 f.write(f">{sequence.accessionVersion}\n")
                 f.write(f"{sequence.data.unalignedNucleotideSequences['main']}\n")
-        command = (
-                "nextclade run "
-                + f"--output-all {result_dir} "
-                + f"--input-dataset {dataset_dir} "
-                + f"-- {input_file}"
+        command = " ".join(
+            [
+                "nextclade3 run",
+                f"--output-all {result_dir}",
+                f"--input-dataset {dataset_dir}",
+                f"--output-translations {result_dir}/nextclade.cds_translation.{{cds}}.fasta",
+                "--",
+                f"{input_file}",
+            ]
         )
+        logging.debug(f"Running nextclade: {command}")
+
+        # TODO: Capture stderr and log at DEBUG level
         exit_code = os.system(command)
         if exit_code != 0:
             raise Exception("nextclade failed with exit code {}".format(exit_code))
+
+        logging.debug(f"Nextclade results available in {result_dir}")
+
+        # TODO: Process metadata. For now, just use "date"
+        for unprocessed_sequence in unprocessed:
+            logging.debug(f"Metadata: {unprocessed_sequence.data.metadata}")
+
+        def process_date(date: str) -> str:
+            """Parse date string. If it's incomplete, add 01-01, if year lacks, add 1900"""
+            components = date.split("-")
+
+            if len(components) == 0 or date == "":
+                # No date provided
+                return "1900-01-01"
+            elif len(components) == 1:
+                # Only year is provided
+                return f"{date}-01-01"
+            elif len(components) == 2:
+                # Year and month are provided
+                return f"{date}-01"
+            elif len(components) == 3:
+                # Full date is provided
+                return date
+            else:
+                # Invalid date format
+                raise ValueError("Invalid date format")
+
+        # TODO: Process metadata. For now, just use "date"
+        processed_metadata = {
+            unprocessed_sequence.accessionVersion: {
+                "collection_date": process_date(
+                    unprocessed_sequence.data.metadata["collection_date"]
+                )
+            }
+            for unprocessed_sequence in unprocessed
+        }
+
         processed = {
             unprocessed_sequence.accessionVersion: {
                 "unalignedNuc": unprocessed_sequence.data.unalignedNucleotideSequences,
-                "alignedNuc": "",
-                "alignedTranslations": {},
-                "metadata": unprocessed_sequence.data.metadata,
+                "alignedNuc": "N" * config.reference_length,
+                "alignedTranslations": {gene: "X" * gene_length  for gene, gene_length in config.genes.items()},
+                "metadata": processed_metadata[unprocessed_sequence.accessionVersion],
             }
             for unprocessed_sequence in unprocessed
         }
@@ -166,11 +242,10 @@ def run_nextclade(
                 sequence_id = aligned_sequence.id
                 processed[sequence_id]["alignedNuc"] = str(aligned_sequence.seq)
 
-        for gene in GENES:
+        for gene in config.genes:
+            translation_path = result_dir + f"/nextclade.cds_translation.{gene}.fasta"
             try:
-                with open(
-                        result_dir + f"/nextclade_gene_{gene}.translation.fasta", "r"
-                ) as alignedTranslations:
+                with open(translation_path) as alignedTranslations:
                     aligned_translation = SeqIO.parse(alignedTranslations, "fasta")
                     for aligned_sequence in aligned_translation:
                         sequence_id = aligned_sequence.id
@@ -181,19 +256,15 @@ def run_nextclade(
                 # TODO: Add warning to each sequence
                 for id in processed.keys():
                     processed[id]["alignedTranslations"][gene] = ""
-                print(f"Gene {gene} not found in Nextclade results")
+                logging.info(
+                    f"Gene {gene} not found in Nextclade results expected at: {translation_path}"
+                )
 
         # TODO: More QC can be lifted from here
         with open(result_dir + "/nextclade.json") as nextclade_json:
             for result in json.load(nextclade_json)["results"]:
                 id = result["seqName"]
-                processed[id]["metadata"].update(
-                    {
-                        "nextcladePangoLineage": result["customNodeAttributes"][
-                            "Nextclade_pango"
-                        ]
-                    }
-                )
+                processed[id]["metadata"].update({"clade": result["clade"]})
         return processed
 
 
@@ -206,7 +277,7 @@ def version_from_str(id_str: AccessionVersion) -> int:
 
 
 def process(
-        unprocessed: Sequence[UnprocessedEntry], dataset_dir: str
+    unprocessed: Sequence[UnprocessedEntry], dataset_dir: str
 ) -> Sequence[ProcessedEntry]:
     nextclade_results = run_nextclade(unprocessed, dataset_dir)
     processed = [
@@ -225,7 +296,7 @@ def process(
                 alignedAminoAcidSequences=nextclade_results[sequence_id][
                     "alignedTranslations"
                 ],
-                aminoAcidInsertions={gene: [] for gene in GENES},
+                aminoAcidInsertions={gene: [] for gene in config.genes},
             ),
         )
         for sequence_id in nextclade_results.keys()
@@ -237,8 +308,11 @@ def process(
 def submit_processed_sequences(processed: Sequence[ProcessedEntry]):
     json_strings = [json.dumps(dataclasses.asdict(sequence)) for sequence in processed]
     ndjson_string = "\n".join(json_strings)
-    url = host + "/submit-processed-data"
-    headers = {'Content-Type': 'application/x-ndjson', 'Authorization': 'Bearer ' + get_jwt()}
+    url = urljoin(config.backend_host, f"{config.organism}/submit-processed-data")
+    headers = {
+        "Content-Type": "application/x-ndjson",
+        "Authorization": "Bearer " + get_jwt(),
+    }
     response = requests.post(url, data=ndjson_string, headers=headers)
     if not response.ok:
         raise Exception(
@@ -246,48 +320,62 @@ def submit_processed_sequences(processed: Sequence[ProcessedEntry]):
             + f"Response: {response.text}\n"
             + f"Data sent in request: {ndjson_string}\n"
         )
-    print(response.text)
+    logging.info("Processed data submitted successfully")
 
 
 def get_jwt():
-    url = keycloakHost + keycloakTokenPath
+    url = urljoin(config.keycloak_host, config.keycloak_token_path)
     data = {
         "client_id": "test-cli",
-        "username": keycloakUser,
-        "password": keycloakPassword,
-        "grant_type": "password"
+        "username": config.keycloak_user,
+        "password": config.keycloak_password,
+        "grant_type": "password",
     }
-    response = requests.post(url, data=data)
-    if not response.ok:
-        raise Exception("Fetching JWT failed. Status code: {}".format(response.status_code), response.text)
-    return response.json()["access_token"]
+
+    logging.debug(f"Requesting JWT from {url}")
+
+    with requests.post(url, data=data) as response:
+        if response.ok:
+            logging.debug("JWT fetched successfully.")
+            return response.json()["access_token"]
+        else:
+            error_msg = (
+                f"Fetching JWT failed with status code {response.status_code}: "
+                f"{response.text}"
+            )
+            logging.error(error_msg)
+            raise Exception(error_msg)
 
 
 def main():
-    # Make tempdir for Nextclade dataset
-    with tempfile.TemporaryDirectory() as dataset_dir:
-        # Download Nextclade dataset
-        dataset_download_command = (
-                "nextclade dataset get "
-                + "--name sars-cov-2 "
-                + f"--output-dir {dataset_dir}"
+    with TemporaryDirectory(delete=not config.keep_tmp_dir) as dataset_dir:
+        dataset_download_command = " ".join(
+            [
+                "nextclade3 dataset get",
+                f"--name {config.nextclade_dataset_name}",
+                f"--output-dir {dataset_dir}",
+            ]
         )
-        print(f"Downloading Nextclade dataset: {dataset_download_command}")
+
+        logging.debug(f"Downloading Nextclade dataset: {dataset_download_command}")
         if os.system(dataset_download_command) != 0:
             raise Exception("Dataset download failed")
         total_processed = 0
         while True:
-            unprocessed = fetch_unprocessed_sequences(200)
+            logging.debug("Fetching unprocessed sequences")
+            unprocessed = fetch_unprocessed_sequences(config.batch_size)
             if len(unprocessed) == 0:
-                # Later on there would be a sleep here, for now we just break
-                break
+                # sleep 1 sec and try again
+                logging.debug("No unprocessed sequences found. Sleeping for 1 second.")
+                time.sleep(1)
+                continue
             # Process the sequences, get result as dictionary
             processed = process(unprocessed, dataset_dir)
             # Submit the result
             submit_processed_sequences(processed)
             total_processed += len(processed)
-            print("Processed {} sequences".format(len(processed)))
-        print("Total processed sequences: {}".format(total_processed))
+            logging.info("Processed {} sequences".format(len(processed)))
+        logging.info("Total processed sequences: {}".format(total_processed))
 
 
 if __name__ == "__main__":
