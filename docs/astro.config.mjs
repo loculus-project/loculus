64dfe779dba2b69747c57e32c5f8f7f6ba757a84
Theo Sanderson <theo@theo.io>
2024-01-18 17:21:39 +0000

    update base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/loculus/",
   integrations: [
     starlight({
       title: "Loculus",
