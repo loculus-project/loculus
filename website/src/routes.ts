58aaf8a65dba517e9b37f15773ced2e878c83284
Theo Sanderson <theo@sndrsn.co.uk>
2024-01-23 17:34:03 +0100

    website 404: Remove unknown organism message (#808)
    * Remove unknown organism message
    
    * address comment from code review
    
    * restore message
    
    * Update website/src/routes.ts
    
    Co-authored-by: Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
    
    * Update website/src/middleware/organismValidatorMiddleware.ts
    
    Co-authored-by: Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>

--- website/src/routes.ts
+++ website/src/routes.ts
@@ -38,7 +38,7 @@ export const routes = {
     },
     userSequencesPage: (organism: string) => withOrganism(organism, `/user/seq`),
     versionPage: (organism: string, accession: string) => withOrganism(organism, `/seq/${accession}/versions`),
-    unknownOrganismPage: (organism: string) => `/404?unknownOrganism=${organism}`,
+    notFoundPage: () => `/404`,
     logout: () => '/logout',
 };
 
