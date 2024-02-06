f7063493fde8f93435f23d06284143a5912844e1
Theo Sanderson <theo@theo.io>
2024-02-06 22:22:29 +0000

    squash config processor

---
+++ kubernetes/config-processor/config-processor.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import requests
+import re
+
+def copy_structure(input_dir, output_dir):
+    # Copy the directory structure
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    shutil.copytree(input_dir, output_dir)
+
+def replace_url_with_content(file_content):
+    urls = re.findall(r'\[\[URL:(http://[^\]]+)\]\]', file_content)
+    for url in urls:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                file_content = file_content.replace(f"[[URL:{url}]]", response.text)
+        except requests.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+    return file_content
+
+def process_files(output_dir):
+    for root, dirs, files in os.walk(output_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r+') as f:
+                print(f"Processing {file_path}")
+                content = f.read()
+                new_content = replace_url_with_content(content)
+                if new_content != content:
+                    f.seek(0)
+                    f.write(new_content)
+                    f.truncate()
+
+def main(input_dir, output_dir):
+    print(f"Processing {input_dir} to {output_dir}")
+    copy_structure(input_dir, output_dir)
+    print(f"Copied directory structure from {input_dir} to {output_dir}")
+    process_files(output_dir)
+
+if __name__ == "__main__":
+    import sys
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    main(input_dir, output_dir)
---
+++ kubernetes/loculus/templates/_config-preprocessor.tpl
@@ -0,0 +1,20 @@
+{{- define "loculus.configPreprocessor" -}}
+- name: config-processor-{{ .name }}
+  image: ghcr.io/loculus-project/config-processor:{{ .dockerTag }}
+  imagePullPolicy: Always
+  volumeMounts:
+    - name: {{ .name }}
+      mountPath: /input
+    - name: {{ .name }}-processed
+      mountPath: /output
+  command: ["./config_processor.py"]
+  args: ["/input", "/output"]
+{{- end -}}
+
+{{- define "loculus.configVolume" -}}
+- name: {{ .name }}
+  configMap:
+    name: {{ if .configmap }}{{ .configmap }}{{ else }}{{ .name }}{{ end }}
+- name: {{ .name }}-processed
+  emptyDir: {}
+{{- end }}
