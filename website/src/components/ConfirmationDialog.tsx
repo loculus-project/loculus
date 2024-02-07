111331cb43dfbecbbe351866d49fd6c86a9dafdf
Theo Sanderson <theo@sndrsn.co.uk>
2024-02-07 15:40:59 +0000

    simplify confirmation dialogs (#923)

--- website/src/components/ConfirmationDialog.tsx
+++ website/src/components/ConfirmationDialog.tsx
@@ -1,22 +1,33 @@
 import { type FC } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
 
 type ConfirmationDialogProps = {
     dialogText: string;
     onConfirmation: () => void;
+    onClose: () => void;
 };
 
-export const ConfirmationDialog: FC<ConfirmationDialogProps> = ({ dialogText, onConfirmation }) => {
+type DisplayConfirmationProps = {
+    dialogText: string;
+    onConfirmation: () => void;
+};
+
+export const ConfirmationDialog: FC<ConfirmationDialogProps> = ({ dialogText, onConfirmation, onClose }) => {
     return (
         <div className='modal-box'>
             <form method='dialog'>
-                <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>✕</button>
+                <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2' onClick={onClose}>
+                    ✕
+                </button>
             </form>
 
             <h3 className='font-bold text-lg'>{dialogText}</h3>
 
             <div className='flex justify-end gap-4 mt-4'>
                 <form method='dialog'>
-                    <button className='btn btn-error'>Cancel</button>
+                    <button className='btn btn-error' onClick={onClose}>
+                        Cancel
+                    </button>
                 </form>
                 <form method='dialog'>
                     <button className='btn loculusGreen' onClick={onConfirmation}>
@@ -26,4 +37,21 @@ export const ConfirmationDialog: FC<ConfirmationDialogProps> = ({ dialogText, on
             </div>
         </div>
     );
+};
+
+export const displayConfirmationDialog = ({ dialogText, onConfirmation }: DisplayConfirmationProps) => {
+    confirmAlert({
+        closeOnClickOutside: true,
+
+        customUI: ({ onClose }) => (
+            <ConfirmationDialog
+                dialogText={dialogText}
+                onConfirmation={async () => {
+                    await onConfirmation();
+                    onClose();
+                }}
+                onClose={onClose}
+            />
+        ),
+    });
 };
