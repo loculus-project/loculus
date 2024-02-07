13802fb720c05ff055e5e039a69a365bc1a55ec1
Theo Sanderson <theo@theo.io>
2024-02-07 17:15:22 +0000

    check

--- deploy.py
+++ deploy.py
@@ -177,7 +177,7 @@ def generate_configs():
     runtime_config_path =  TEMP_DIR / 'runtime_config.json'
     generate_config(helm_chart, 'templates/loculus-website-config.yaml', runtime_config_path, codespace_name)
 
-    subprocess.run(['python', 'kubernetes/config-processor/config-processor.py', TEMP_DIR, output_dir])
+    subprocess.run(['python', 'kubernetes/config-processor/config-processor.py', TEMP_DIR, output_dir], check=True)
 
 def generate_config(helm_chart, template, output_path, codespace_name=None):
     helm_template_cmd = ['helm', 'template', 'name-does-not-matter', helm_chart, '--show-only', template]
