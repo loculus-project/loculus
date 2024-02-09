a800daed9bc0dcafb9e432d854d8015f31787654
Theo Sanderson <theo@sndrsn.co.uk>
2024-02-09 21:17:40 +0000

    title

--- keycloak/keycloakify/src/login/Template.tsx
+++ keycloak/keycloakify/src/login/Template.tsx
@@ -46,7 +46,7 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
         "bodyClassName": getClassName("kcBodyClass")
     });
 
-    useState(()=> { document.title = "Log in" });
+    useState(()=> { document.title = "Log in"; });
 
     if (!isReady) {
         return null;
