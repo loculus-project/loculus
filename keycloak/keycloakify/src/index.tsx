3af5b1499bf1f71ff6d2d9a190ddf93b8eb77a71
Theo Sanderson <theo@sndrsn.co.uk>
2024-02-09 19:04:50 +0000

    remove the app bit

--- keycloak/keycloakify/src/index.tsx
+++ keycloak/keycloakify/src/index.tsx
@@ -1,17 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode, lazy, Suspense } from "react";
-import { kcContext as kcLoginThemeContext } from "./keycloak-theme/login/kcContext";
-import { kcContext as kcAccountThemeContext } from "./keycloak-theme/account/kcContext";
+import { kcContext as kcLoginThemeContext } from "./login/kcContext";
+import { kcContext as kcAccountThemeContext } from "./account/kcContext";
 
-const KcLoginThemeApp = lazy(() => import("./keycloak-theme/login/KcApp"));
-const KcAccountThemeApp = lazy(() => import("./keycloak-theme/account/KcApp"));
-// Important note:
-// In this starter example we show how you can have your react app and your Keycloak theme in the same repo.  
-// Most Keycloakify user only want to great a Keycloak theme.  
-// If this is your case run the few commands that will remover everything that is not strictly related to the 
-//Keycloak theme:
-// https://github.com/keycloakify/keycloakify-starter?tab=readme-ov-file#i-only-want-a-keycloak-theme
-const App = lazy(() => import("./App"));
+const KcLoginThemeApp = lazy(() => import("./login/KcApp"));
+const KcAccountThemeApp = lazy(() => import("./account/KcApp"));
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
@@ -26,9 +19,13 @@ createRoot(document.getElementById("root")!).render(
                     return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
                 }
 
-                return <App />;
+                throw new Error(
+                  "This app is a Keycloak theme" +
+                  "It isn't meant to be deployed outside of Keycloak"
+                );
 
             })()}
         </Suspense>
     </StrictMode>
 );
+
