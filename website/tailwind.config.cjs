f6384456ff256ce2eebbe9c72675061c9166068b
Theo Sanderson <theo@sndrsn.co.uk>
2024-02-23 15:57:32 +0000

    muted-sky

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
 const mainTailwindColor = {
-  '50': '#f3f6fb',
-  '100': '#e4e9f5',
-  '200': '#cfdaee',
-  '300': '#aec1e2',
-  '400': '#88a1d2',
-  '500': '#6b84c6',
-  '600': '#586bb8',
-  '700': '#4d5ba8',
-  '800': '#3e467e',
-  '900': '#3a416e',
-  '950': '#272b44',
+  '50': '#f2f9fd',
+  '100': '#e4f1fa',
+  '200': '#c3e3f4',
+  '300': '#8eceeb',
+  '400': '#52b4de',
+  '500': '#2b9bcc',
+  '600': '#1c7dad',
+  '700': '#18638b',
+  '800': '#185574',
+  '900': '#194761',
+  '950': '#112d40',
 };
+
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
