d2d5f07ecc594aa20fd6c3854751335a57bef1d9
Theo Sanderson <theo@sndrsn.co.uk>
2024-02-23 15:23:39 +0000

    indigo

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
-const mainTailwindColor = colors.sky
+const mainTailwindColor = colors.indigo
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
