fe2a9c9e1d2e7274223c8d01c78983b272d224f8
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-02-26 18:00:27 +0100

    build: Fix collecting logs in ci

--- .github/scripts/collect_kubernetes_logs.sh
+++ .github/scripts/collect_kubernetes_logs.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-pods=$(kubectl get pods -l app=loculus -n loculus -o jsonpath='{.items[*].metadata.name}' || true)
+pods=$(kubectl get pods -l app=loculus -o jsonpath='{.items[*].metadata.name}' || true)
+
+echo "Collecting logs from pods: $pods"
+
 for pod in $pods; do
-  containers=$(kubectl get pod "$pod" -n loculus -o jsonpath='{.spec.containers[*].name}' || true)
+  containers=$(kubectl get pod "$pod" -o jsonpath='{.spec.containers[*].name}' || true)
   for container in $containers; do
     mkdir "kubernetes_logs" -p
     file="kubernetes_logs/$pod-$container.txt"
     echo "Logs from $pod - $container:" >> "$file"
-    kubectl logs "$pod" -n loculus -c "$container" >> "$file" 2>/dev/null || true
+    kubectl logs "$pod" -c "$container" >> "$file" 2>/dev/null || true
   done
 done
