fbe3c287945468130bf913b70835f9422dffa205
Theo Sanderson <theo@sndrsn.co.uk>
2024-02-28 11:54:13 +0000

    more attempts

--- website/tests/pages/review/review.page.ts
+++ website/tests/pages/review/review.page.ts
@@ -17,7 +17,10 @@ export class ReviewPage {
     constructor(public readonly page: Page) {
         this.approveAllButton = page.getByRole('button', { name: 'Release', exact: false });
         this.deleteFirstButton = page.getByRole('button', { name: 'Discard sequences', exact: false });
-        this.deleteAllButton = page.getByRole('button', { name: 'Discard all', exact: false, includeHidden: true });
+        this.deleteAllButton = page.getByText('Discard all', {
+            exact: false,
+        });
+
         this.confirmButton = page.getByRole('button', { name: 'Confirm', exact: false });
     }
 
