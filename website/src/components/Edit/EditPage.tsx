2a73ff7a9fe3dde5e4b5bcdd1cf2cd92100fc761
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-03-08 04:21:37 +0100

    Squashed commit of the following:
    commit b80e3490426e18d26b35854579b16de93198a4a0
    Author: Cornelius Roemer <cornelius.roemer@gmail.com>
    Date:   Fri Mar 8 03:38:52 2024 +0100
    
        fix brackets
    
    commit 481e1a5efc3d13d1c565b720280686b03b3b4a5d
    Author: Cornelius Roemer <cornelius.roemer@gmail.com>
    Date:   Fri Mar 8 03:17:54 2024 +0100
    
        fix #1261

--- website/src/components/Edit/EditPage.tsx
+++ website/src/components/Edit/EditPage.tsx
@@ -200,22 +200,19 @@ type EditableOriginalDataProps = {
 const EditableOriginalData: FC<EditableOriginalDataProps> = ({ editedMetadata, setEditedMetadata }) => (
     <>
         <Subtitle title='Metadata' />
-        {editedMetadata.map((field) => {
-            field.key = sentenceCase(field.key);
-            return (
-                <EditableDataRow
-                    key={'raw_metadata' + field.key}
-                    row={field}
-                    onChange={(editedRow: Row) =>
-                        setEditedMetadata((prevRows: Row[]) =>
-                            prevRows.map((prevRow) =>
-                                prevRow.key === editedRow.key ? { ...prevRow, value: editedRow.value } : prevRow,
-                            ),
-                        )
-                    }
-                />
-            );
-        })}
+        {editedMetadata.map((field) => (
+            <EditableDataRow
+                key={'raw_metadata' + field.key}
+                row={field}
+                onChange={(editedRow: Row) =>
+                    setEditedMetadata((prevRows: Row[]) =>
+                        prevRows.map((prevRow) =>
+                            prevRow.key === editedRow.key ? { ...prevRow, value: editedRow.value } : prevRow,
+                        ),
+                    )
+                }
+            />
+        ))}
     </>
 );
 
@@ -249,10 +246,7 @@ const ProcessedMetadata: FC<ProcessedMetadataProps> = ({ processedMetadata }) =>
     <>
         <Subtitle title='Metadata' customKey='preprocessing_metadata' />
         {Object.entries(processedMetadata).map(([key, value]) => (
-            <ProcessedDataRow
-                key={'processed' + key}
-                row={{ key: sentenceCase(key), value: displayMetadataField(value) }}
-            />
+            <ProcessedDataRow key={'processed' + key} row={{ key, value: displayMetadataField(value) }} />
         ))}
     </>
 );
