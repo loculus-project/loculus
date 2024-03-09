4b0bdf60ce780a09a658f4324b630ef5b6b861df
Theo Sanderson <theo@sndrsn.co.uk>
2024-03-09 01:02:04 +0000

    remove test

--- website/tests/pages/sequences/accession.spec.ts
+++ website/tests/pages/sequences/accession.spec.ts
@@ -58,26 +58,4 @@ test.describe('The detailed sequence page', () => {
         );
     });
 
-    test('can revoke', async ({ sequencePage, loginAsTestUser, userPage }) => {
-        await loginAsTestUser();
-
-        await userPage.goToUserPage();
-        await sequencePage.page.click('text=asdasd'); 
-        // capture a screenshot
-        /*
-        await userPage.page.screenshot({ path: 'userPage.png' });
-
-        const testSequences = getTestSequences();
-
-        await sequencePage.goto(testSequences.sequenceToRevoke);
-
-        await sequencePage.page.evaluate(() => {
-            window.scrollTo(0, document.body.scrollHeight);
-        });
-
-        await expect(sequencePage.page.getByText(`Revoke this sequence`)).toBeVisible();
-
-        await sequencePage.page.click('text=Revoke this sequence');
-        */
-    });
 });
