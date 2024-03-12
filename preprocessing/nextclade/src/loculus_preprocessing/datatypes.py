e4fe103676088009783024856b07eb0ab792788d
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-03-12 18:42:19 +0100

    feat(prepro): Make Nextclade prepro pipeline configurable through values.yaml

--- preprocessing/nextclade/src/loculus_preprocessing/datatypes.py
+++ preprocessing/nextclade/src/loculus_preprocessing/datatypes.py
@@ -1,15 +1,21 @@
-from collections.abc import Mapping
 from dataclasses import dataclass, field
 from typing import Any, Literal
 
 AccessionVersion = str
-NextcladeResult = dict[str, dict[str, Any]]
+GeneName = str
+NucleotideSequence = str
+AminoAcidSequence = str
+NucleotideInsertion = str
+AminoAcidInsertion = str
+FunctionName = str  # Name of function present in processing_functions
+ArgName = str  # Name of argument present in processing_functions
+InputField = str  # Name of field in input data, either inputMetadata or NextcladeMetadata
 
 
 @dataclass
 class UnprocessedData:
-    metadata: Mapping[str, str]
-    unalignedNucleotideSequences: Mapping[str, str]
+    metadata: dict[str, str]
+    unalignedNucleotideSequences: dict[str, NucleotideSequence]
 
 
 @dataclass
@@ -18,9 +24,30 @@ class UnprocessedEntry:
     data: UnprocessedData
 
 
+FunctionInputs = dict[ArgName, InputField]
+
+
+@dataclass
+class ProcessingSpec:
+    inputs: FunctionInputs
+    function: FunctionName
+
+
+# For single segment, need to generalize for multi segments later
+@dataclass
+class UnprocessedWithNextclade:
+    inputMetadata: dict[str, Any]  # Original user supplied metadata
+    nextcladeMetadata: dict[str, Any] | None  # Derived metadata produced by Nextclade
+    unalignedNucleotideSequences: NucleotideSequence
+    alignedNucleotideSequences: NucleotideSequence | None
+    nucleotideInsertions: list[NucleotideInsertion]
+    alignedAminoAcidSequences: dict[GeneName, AminoAcidSequence]
+    aminoAcidInsertions: dict[GeneName, list[AminoAcidInsertion]]
+
+
 @dataclass
 class ProcessedData:
-    metadata: dict[str, dict[str, Any]]
+    metadata: dict[str, Any]
     unalignedNucleotideSequences: dict[str, Any]
     alignedNucleotideSequences: dict[str, Any]
     nucleotideInsertions: dict[str, Any]
@@ -30,13 +57,13 @@ class ProcessedData:
 
 @dataclass
 class AnnotationSource:
-    field: str
-    type: Literal["metadata", "nucleotideSequence"]
+    name: str
+    type: Literal["Metadata", "NucleotideSequence"]
 
 
 @dataclass
 class ProcessingAnnotation:
-    source: AnnotationSource
+    source: list[AnnotationSource]
     message: str
 
 
@@ -52,3 +79,10 @@ class ProcessedEntry:
     data: ProcessedData
     errors: list[ProcessingAnnotation] = field(default_factory=list)
     warnings: list[ProcessingAnnotation] = field(default_factory=list)
+
+
+@dataclass
+class ProcessingResult:
+    datum: str | None
+    warnings: list[ProcessingAnnotation] = field(default_factory=list)
+    errors: list[ProcessingAnnotation] = field(default_factory=list)
