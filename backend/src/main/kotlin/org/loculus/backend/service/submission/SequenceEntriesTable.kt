95ac0c79392680774fe32869f662a4210724d48b
Theo Sanderson <theo@sndrsn.co.uk>
2024-03-17 22:19:03 +0000

    fixup

--- backend/src/main/kotlin/org/loculus/backend/service/submission/SequenceEntriesTable.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/SequenceEntriesTable.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Expression
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.not
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.notInList
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.alias
 import org.jetbrains.exposed.sql.and
@@ -106,7 +106,7 @@ class SequenceEntriesDataTable(
 
     fun statusIsOneOf(statuses: List<Status>) = statusColumn inList statuses.map { it.name }
 
-    fun statusIsNotOneOf(statuses: List<Status>) = not { statusColumn inList statuses.map { it.name } }
+    fun statusIsNotOneOf(statuses: List<Status>) = statusColumn notInList statuses.map { it.name }
 
     fun accessionVersionEquals(accessionVersion: AccessionVersionInterface) =
         (accessionColumn eq accessionVersion.accession) and
