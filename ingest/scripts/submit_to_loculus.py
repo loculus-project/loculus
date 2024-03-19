0829d5ec73b813742c78c7cdee0627c6036384d1
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-03-19 20:26:14 +0100

    Make ingest configurable

--- ingest/scripts/submit_to_loculus.py
+++ ingest/scripts/submit_to_loculus.py
@@ -1,68 +1,69 @@
-import json
 import logging
+from dataclasses import dataclass
 
 import click
 import requests
+import yaml
 
-BRANCH="demo-prepro"
-GROUP_NAME="insdc_ingest_group"
-USERNAME="insdc_ingest_user"
-PASSWORD="insdc_ingest_user"
-BACKEND_URL=f"https://backend-{BRANCH}.loculus.org"
-KEYCLOAK_TOKEN_URL=f"https://authentication-{BRANCH}.loculus.org/realms/loculusRealm/protocol/openid-connect/token"
-KEYCLOAK_CLIENT_ID="test-cli" # Apparently required to be exactly this
-ORGANISM="mpox"
 
-# Create the ingest user on instance start in kubernetes/loculus/templates/keycloak-config-map.yaml
+@dataclass
+class Config:
+    organism: str
+    backend_url: str
+    keycloak_token_url: str
+    keycloak_client_id: str
+    username: str
+    password: str
+    group_name: str
 
-def backend_url(branch):
-    return f"https://backend-{branch}.loculus.org"
 
-def get_jwt(username, password, branch):
+def organism_url(config: Config):
+    return f"{config.backend_url.rstrip('/')}/{config.organism.strip('/')}"
+
+
+def get_jwt(config: Config):
     """
     Get a JWT token for the given username and password
     """
 
     data = {
-        "username": username,
-        "password": password,
+        "username": config.username,
+        "password": config.password,
         "grant_type": "password",
-        "client_id": KEYCLOAK_CLIENT_ID,
+        "client_id": config.keycloak_client_id,
     }
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-    keycloak_token_url = f"https://authentication-{branch}.loculus.org/realms/loculus/protocol/openid-connect/token"
+    keycloak_token_url = config.keycloak_token_url
 
     response = requests.post(keycloak_token_url, data=data, headers=headers)
     response.raise_for_status()
 
     jwt_keycloak = response.json()
-    jwt = jwt_keycloak['access_token']
+    jwt = jwt_keycloak["access_token"]
     return jwt
 
-def create_group(group_name, branch):
-    # Create the ingest group
-    url = f"{backend_url(branch)}/groups"
-    token = get_jwt(USERNAME, PASSWORD, branch)
-    group_name = group_name
 
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
+def create_group(config: Config):
+    # Create the ingest group
+    url = f"{config.backend_url.rstrip('/')}/groups"
+    token = get_jwt(config)
+    group_name = config.group_name
+
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
 
     data = {
         "groupName": group_name,
         "institution": "NA",
         "address": {
-          "line1": "1234 Loculus Street",
-          "line2": "NA",
-          "city": "Dortmund",
-          "state": "NRW",
-          "postalCode": "12345",
-          "country": "Germany"
+            "line1": "1234 Loculus Street",
+            "line2": "NA",
+            "city": "Dortmund",
+            "state": "NRW",
+            "postalCode": "12345",
+            "country": "Germany",
         },
-        "contactEmail": "something@loculus.org"
+        "contactEmail": "something@loculus.org",
     }
 
     response = requests.post(url, json=data, headers=headers)
@@ -74,30 +75,29 @@ def create_group(group_name, branch):
         print(f"Error creating group: {response.json()}")
         response.raise_for_status()
 
-def submit(metadata, sequences, branch):
+
+def submit(metadata, sequences, config: Config):
     """
     Submit data to Loculus.
     """
 
-    jwt = get_jwt(USERNAME, PASSWORD, branch)
+    jwt = get_jwt(config)
 
     # Endpoint URL
-    url = f'{backend_url(branch)}/{ORGANISM}/submit'
+    url = f"{organism_url(config)}/submit"
 
     # Headers with Bearer Authentication
-    headers = {
-        'Authorization': f'Bearer {jwt}'
-    }
+    headers = {"Authorization": f"Bearer {jwt}"}
 
     # Files to be uploaded
     files = {
-        'metadataFile': open(metadata, 'rb'),
-        'sequenceFile': open(sequences, 'rb')
+        "metadataFile": open(metadata, "rb"),
+        "sequenceFile": open(sequences, "rb"),
     }
 
     # Query parameters
     params = {
-        'groupName': GROUP_NAME,
+        "groupName": config.group_name,
         "dataUseTermsType": "OPEN",
     }
 
@@ -106,28 +106,25 @@ def submit(metadata, sequences, branch):
     response.raise_for_status()
 
     # Closing files
-    files['metadataFile'].close()
-    files['sequenceFile'].close()
+    files["metadataFile"].close()
+    files["sequenceFile"].close()
 
     return response.json()
 
-def approve(branch):
+
+def approve(config: Config):
     """
     Get sequences that were preprocessed successfully and approve them.
     1. Get the ids of the sequences that were preprocessed successfully
         /ORGANISM/get-sequences
     2. Approve the sequences
     """
-    jwt = get_jwt(USERNAME, PASSWORD, branch)
+    jwt = get_jwt(config)
 
-    url = f'{backend_url(branch)}/{ORGANISM}/get-sequences'
-
+    url = f"{organism_url(config)}/get-sequences"
 
     # Headers with Bearer Authentication
-    headers = {
-        'Authorization': f'Bearer {jwt}'
-    }
-
+    headers = {"Authorization": f"Bearer {jwt}"}
 
     # POST request
     response = requests.get(url, headers=headers)
@@ -146,7 +143,7 @@ def approve(branch):
 
     payload = {"scope": "ALL"}
 
-    url = f'{backend_url(branch)}/{ORGANISM}/approve-processed-data'
+    url = f"{organism_url(config)}/approve-processed-data"
 
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
@@ -154,43 +151,61 @@ def approve(branch):
 
 # %%
 
+
 @click.command()
-@click.option('--metadata', required=False, type=click.Path(exists=True), help='Path to the metadata file')
-@click.option('--sequences', required=False, type=click.Path(exists=True), help='Path to the sequences file')
-@click.option('--branch', required=False, type=click.STRING, help='Branch to submit to', default="main")
-@click.option('--output-ids', required=False, type=click.Path(), help='Path to the output IDs file')
-@click.option('--mode', required=True, type=click.Choice(['submit', 'approve']), help='Mode to run in')
-@click.option('--log-level', default='INFO', type=click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']), help='Log level')
-# @click.option('--submit-config', required=True, type=click.Path(exists=True), help='Path to the submit configuration file')
-# def submit_to_loculus(metadata, sequences, submit_config, output_ids):
-def submit_to_loculus(metadata, sequences, branch: str, output_ids, mode, log_level):
+@click.option(
+    "--metadata",
+    required=False,
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--sequences",
+    required=False,
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--mode",
+    required=True,
+    type=click.Choice(["submit", "approve"]),
+)
+@click.option(
+    "--log-level",
+    default="INFO",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+)
+@click.option(
+    "--config-file",
+    required=True,
+    type=click.Path(exists=True),
+)
+def submit_to_loculus(metadata, sequences, mode, log_level, config_file):
     """
     Submit data to Loculus.
     """
     logging.basicConfig(level=log_level)
-    if mode == 'submit':
+    with open(config_file) as file:
+        config = Config(**yaml.safe_load(file))
+
+    if mode == "submit":
         logging.info("Submitting to Loculus")
-        logging.debug(f"Args: {metadata}, {sequences}, {output_ids}")
+        logging.debug(f"Config: {config}")
         # Create group if it doesn't exist
-        logging.info(f"Creating group {GROUP_NAME}")
-        create_group(GROUP_NAME, branch)
-        logging.info(f"Group {GROUP_NAME} created")
+        logging.info(f"Creating group {config.group_name}")
+        create_group(config)
+        logging.info(f"Group {config.group_name} created")
 
         # Submit
         logging.info("Starting submission")
-        response = submit(metadata, sequences, branch)
+        response = submit(metadata, sequences, config)
         logging.info("Submission complete")
 
-        json.dump(response, open(output_ids, 'w'), indent=4)
-        logging.info(f"IDs written to {output_ids}")
-    
-    if mode == 'approve':
+    if mode == "approve":
         logging.info("Approving sequences")
-        response = approve(branch)
+        response = approve(config)
         logging.debug(f"Approved: {response}")
 
         logging.info("Approving sequences complete")
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     submit_to_loculus()
