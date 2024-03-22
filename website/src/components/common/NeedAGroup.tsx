f76820b80974c40768fdb9890afda3df3efb40c9
Theo Sanderson <theo@sndrsn.co.uk>
2024-03-22 15:17:45 +0000

    improve UX around sub (#1439)

---
+++ website/src/components/common/NeedAGroup.tsx
@@ -0,0 +1,21 @@
+import { routes } from '../../routes/routes';
+import DashiconsGroups from '~icons/dashicons/groups';
+
+export const NeedAGroup = () => (
+    <div className='mt-6 message max-w-4xl mx-auto'>
+        <DashiconsGroups className='w-12 h-12 inline-block mr-2' />
+        <div>
+            <p>
+                Sequences can only be submitted to the database by users who are part of a <i>group</i>.
+            </p>
+            <p className='mt-3'>
+                To submit to the database, please either{' '}
+                <a href={routes.createGroup()} className='underline'>
+                    create a group
+                </a>{' '}
+                (a group with one member is not a problem!) or ask a group administrator to add you to an existing
+                group.
+            </p>
+        </div>
+    </div>
+);
