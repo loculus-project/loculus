fd4ec0f8d4db1b682e76be20c5d85461c52963d7
Theo Sanderson <theo@sndrsn.co.uk>
2024-03-22 20:08:19 +0000

    Update DataUploadForm.tsx

--- website/src/components/Submission/DataUploadForm.tsx
+++ website/src/components/Submission/DataUploadForm.tsx
@@ -489,7 +489,7 @@ const InnerDataUploadForm = ({
                     <button
                         name='submit'
                         type='submit'
-                        className='rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600'
+                        className='rounded-md bg-primary-600 px-1 pr-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600'
                         onClick={handleSubmit}
                         disabled={isLoading}
                     >
