4efba69a9feb96fb83cb72304be44f560bffc638
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-03-27 11:46:25 +0100

    fix(ci): delete silo cache properly

--- kubernetes/loculus/silo_import_job.sh
+++ kubernetes/loculus/silo_import_job.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-base_data_dir="/preprocessing/input"
-output_data_dir="/preprocessing/output"
+input_data_dir="/preprocessing/input"
+
 current_timestamp=$(date +%s)
-current_input_data_dir="$base_data_dir/$current_timestamp"
-old_input_data_dir="$base_data_dir"/$(ls -1 "$base_data_dir" | sort -n | grep -E '^[0-9]+$' | tail -n 1)
+new_input_data_dir="$input_data_dir/$current_timestamp"
+
+old_input_data_dir="$input_data_dir"/$(ls -1 "$input_data_dir" | sort -n | grep -E '^[0-9]+$' | tail -n 1)
+
+new_input_data="$new_input_data_dir/data.ndjson"
+old_input_data="$old_input_data_dir/data.ndjson"
+silo_input_data="$silo_input_data/data.ndjson"
 
 get_token() {
   if [ -z "$KEYCLOAK_TOKEN_URL" ]; then
@@ -41,63 +46,65 @@ get_token() {
   echo
 }
 
-delete_all_input() {
   echo "Deleting all input data"
-  rm -f "$base_data_dir/data.ndjson"
-  rm -rf "$current_input_data_dir"
+  rm -f "$silo_input_data"
+  rm -rf "$new_input_data_dir"
   echo
 }
 
 download_data() {
 
-  mkdir -p "$current_input_data_dir"
-  echo  "created $current_input_data_dir"
+  mkdir -p "$new_input_data_dir"
+  echo  "created $new_input_data_dir"
 
   released_data_endpoint="$BACKEND_BASE_URL/get-released-data"
   echo "calling $released_data_endpoint"
   
   set +e
-  curl -o "$current_input_data_dir/data.ndjson" --fail-with-body "$released_data_endpoint" -H "Authorization: Bearer $jwt"
+  curl -o "$new_input_data" --fail-with-body "$released_data_endpoint" -H "Authorization: Bearer $jwt"
   exit_code=$?
   set -e
 
   if [ $exit_code -ne 0 ]; then
     echo "Curl command failed with exit code $exit_code, cleaning up and exiting."
-    rm -rf "$current_input_data_dir"
+    rm -rf "$new_input_data_dir"
     exit $exit_code
   fi
 
-  echo "downloaded $(wc -l < "$current_input_data_dir/data.ndjson") sequences"
+  echo "downloaded $(wc -l < "$new_input_data") sequences"
   echo
 
-  echo "Sorting downloaded data.ndjson"
-
   echo "checking for old input data dir $old_input_data_dir"
-  if [[ "$old_input_data_dir" =~ ^[0-9]+$ ]]; then
-    old_hash=$(md5sum < "$current_input_data_dir/data.ndjson" | awk '{print $1}')
-    new_hash=$(md5sum < "$current_input_data_dir/data.ndjson" | awk '{print $1}')
+  if [[ -f "$old_input_data" ]]; then
+    old_hash=$(md5sum < "$old_input_data" | awk '{print $1}')
+    new_hash=$(md5sum < "$new_input_data" | awk '{print $1}')
     echo "old hash: $old_hash"
     echo "new hash: $new_hash"
     if [ "$new_hash" = "$old_hash" ]; then
       echo "Hashes are equal, skipping preprocessing"
-      echo "Deleting input data dir $current_input_data_dir"
-      rm -rf "$current_input_data_dir"
+      echo "Deleting input data dir $new_input_data_dir"
+      rm -rf "$new_input_data_dir"
       exit 0
     else
       echo "Hashes are unequal, deleting old input data dir"
       rm -rf "$old_input_data_dir:?}"
     fi
+  else
+    echo "No old input data dir found"
   fi
   echo
 }
 
 preprocessing() {
   # TODO: #1489  Remove emptiness test once https://github.com/GenSpectrum/LAPIS-SILO/issues/244 fixed
-  if [ -s "$current_input_data_dir" ]; then
+  if [ -s "$new_input_data_dir" ]; then
     echo "data.ndjson is not empty, starting preprocessing"
 
-    rm -f "$base_data_dir/data.ndjson"
-    cp "$current_input_data_dir/data.ndjson" "$base_data_dir/data.ndjson"
+    rm -f "$silo_input_data"
+
+    # This is necessary because silo preprocessing expects the input data to be in a specific magic location
+    # At /preprocessing/input/data.ndjson
+    cp "$new_input_data" "$silo_input_data"
     
     set +e
     time /app/siloApi --preprocessing
@@ -105,8 +112,8 @@ preprocessing() {
     set -e
 
     if [ $exit_code -ne 0 ]; then
-      echo "SiloApi command failed with exit code $exit_code, cleaning  up and exiting."
-      delete_all_input
+      echo "SiloApi command failed with exit code $exit_code, cleaning up and exiting."
+      delete_all_input # Delete input so that we don't skip preprocessing next time due to hash equality
       exit $exit_code
     fi
 
@@ -119,36 +126,40 @@ preprocessing() {
   echo
 }
 
+# Potential race condition: silo might not release non-current dir if it's still being used
 cleanup_output_data() {
-  output_data_dir="/preprocessing/output"
-  echo "Removing all but the most recent output directory in $output_data_dir"
-  cd $output_data_dir || exit
+  for dir_type in "input" "output"; do
+    dir="/preprocessing/$dir_type"
+    echo "Removing all but the most recent $dir_type directory in $dir"
+    cd $dir || exit
 
-  if [ -n "$(ls -d -- */ 2>/dev/null)" ]; then
-    directories=$(ls -dt -- */)
-    if [ "$(echo "$directories" | wc -l)" -gt 1 ]; then
-      newest_dir=$(echo "$directories" | head -n 1)
-      echo "$directories" | tail -n +2 | xargs rm -r
-      echo "Kept: $newest_dir"
+    if [ -n "$(ls -d -- */ 2>/dev/null)" ]; then
+      directories=$(ls -dt -- */)
+      if [ "$(echo "$directories" | wc -l)" -gt 1 ]; then
+        newest_dir=$(echo "$directories" | head -n 1)
+        echo "$directories" | tail -n +3 | xargs rm -r
+        echo "Kept: $newest_dir"
+      else
+        echo "No directories to delete."
+      fi
     else
-      echo "No directories to delete."
+      echo "No directories found."
     fi
-  else
-    echo "No directories found."
-  fi
 
-  cd - > /dev/null || exit
-  echo
+    cd - > /dev/null || exit
+    echo
+  done
 }
 
 main() {
   echo "----------------------------------------"
   echo "Script started at: $(date)"
 
+  # cleanup at start in case we fail later
+  cleanup_output_data
   get_token
   download_data
   preprocessing
-  cleanup_output_data
 
   echo "done"
   echo "----------------------------------------"
