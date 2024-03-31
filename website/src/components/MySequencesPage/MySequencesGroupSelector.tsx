35fe0f6e43f94a5c6fd21c41c43d53f84119d4a9
Chaoran Chen <mail@chaoran-chen.de>
2024-03-31 01:30:06 +0000

    feat(website): move group selector on view sequences page to title

--- website/src/components/MySequencesPage/MySequencesGroupSelector.tsx
+++ website/src/components/MySequencesPage/MySequencesGroupSelector.tsx
@@ -1,6 +1,8 @@
 import { type FC } from 'react';
 
 import { routes } from '../../routes/routes';
+import DashiconsGroups from '~icons/dashicons/groups';
+import IwwaArrowDown from '~icons/iwwa/arrow-down';
 
 type GroupSelectorProps = {
     groupNames: string[];
@@ -8,20 +10,37 @@ type GroupSelectorProps = {
     organism: string;
 };
 export const MySequencesGroupSelector: FC<GroupSelectorProps> = ({ groupNames, selectedGroupName, organism }) => {
+    const groupNameElement = (
+        <>
+            <DashiconsGroups className='w-6 h-6 inline-block mr-1 -mt-1' />
+            <span className='text-gray-700'>{selectedGroupName}</span>
+        </>
+    );
+
     return (
-        <select
-            className='mt-4 select select-bordered'
-            onChange={(event) => {
-                const newGroup = event.target.value;
-                const page = routes.mySequencesPage(organism, newGroup);
-                location.href = page;
-            }}
-        >
-            {groupNames.map((groupName: string) => (
-                <option selected={groupName === selectedGroupName} value={groupName} key={groupName}>
-                    {groupName}
-                </option>
-            ))}
-        </select>
+        <div className='mb-1'>
+            {groupNames.length === 1 ? (
+                groupNameElement
+            ) : (
+                <div className='dropdown'>
+                    <div tabIndex={0} role='button' className=''>
+                        {groupNameElement} <IwwaArrowDown className='inline-block -mt-1 h-5 w-5' />
+                    </div>
+                    <ul tabIndex={0} className='dropdown-content z-[1] menu p-2 shadow bg-base-100 w-52 text-gray-700'>
+                        {groupNames.map((groupName: string) => (
+                            <li key={groupName}>
+                                <a
+                                    onClick={() => {
+                                        location.href = routes.mySequencesPage(organism, groupName);
+                                    }}
+                                >
+                                    {groupName}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
     );
 };
