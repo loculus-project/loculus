aff3f5247d44df05cbce4e0e993b86e1b638965c
Chaoran Chen <mail@chaoran-chen.de>
2024-04-05 10:46:13 +0200

    refactor(website): slightly refactor an API client and add docs

--- website/src/services/backendClient.ts
+++ website/src/services/backendClient.ts
@@ -5,19 +5,17 @@ import { getInstanceLogger } from '../logger.ts';
 import { createAuthorizationHeader } from '../utils/createAuthorizationHeader.ts';
 
 export class BackendClient extends ZodiosWrapperClient<typeof backendApi> {
-    /** Somehow Typescript's type inference currently doesn't work properly in Astro files */
-    public readonly astroFileTypeHelpers = {
-        getDataToEdit: (organism: string, token: string, accession: string, version: string | number) =>
-            this.call('getDataToEdit', {
-                params: { accession, version, organism },
-                headers: createAuthorizationHeader(token),
-            }),
-    };
-
     public static create(
         backendUrl: string = getRuntimeConfig().serverSide.backendUrl,
         logger = getInstanceLogger('serverSideBackendClient'),
     ) {
         return new BackendClient(backendUrl, backendApi, (axiosError) => axiosError.data, logger, 'backend');
+    }
+
+    public getDataToEdit(organism: string, token: string, accession: string, version: string | number) {
+        return this.call('getDataToEdit', {
+            params: { accession, version, organism },
+            headers: createAuthorizationHeader(token),
+        });
     }
 }
