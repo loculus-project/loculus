72a0d2c4d121a7e6f5fea1dcda42b8d5d56d1df3
Theo Sanderson <theo@sndrsn.co.uk>
2024-04-08 11:36:29 +0000

    fix

--- website/src/components/SearchPage/SearchForm.tsx
+++ website/src/components/SearchPage/SearchForm.tsx
@@ -115,7 +115,7 @@ export const SearchForm: FC<SearchFormProps> = ({
                 if (field.grouped === true) {
                     return (
                         <div key={field.name} className='flex flex-col border p-3 mb-3 rounded-md border-gray-300'>
-                            <h3 className='text-gray-500 text-sm mb-1'>{field.label}</h3>
+                            <h3 className='text-gray-500 text-sm mb-1'>{field.displayName !==undefined ? field.displayName : field.label}</h3>
 
                             {field.groupedFields.map((groupedField) => (
                                 <SearchField
