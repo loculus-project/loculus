befe131295f421c5a59e0277b374dde16d11c4d6
bh-ethz <benedict.henshaw@bsse.ethz.ch>
2024-04-11 15:51:01 +0200

    linter

--- website/src/components/DataUseTerms/DataUseTermsSelector.tsx
+++ website/src/components/DataUseTerms/DataUseTermsSelector.tsx
@@ -1,6 +1,7 @@
 import { type FC } from 'react';
+
 import { routes } from '../../routes/routes.ts';
-import { type DataUseTermsType, openDataUseTermsType, restrictedDataUseTermsType, } from '../../types/backend.ts';
+import { type DataUseTermsType, openDataUseTermsType, restrictedDataUseTermsType } from '../../types/backend.ts';
 import Locked from '~icons/fluent-emoji-high-contrast/locked';
 import Unlocked from '~icons/fluent-emoji-high-contrast/unlocked';
 
@@ -9,10 +10,7 @@ type DataUseTermsSelectorProps = {
     setDataUseTermsType: (dataUseTermsType: DataUseTermsType) => void;
 };
 
-const DataUseTermsSelector: FC<DataUseTermsSelectorProps> = ({
-    dataUseTermsType,
-    setDataUseTermsType,
-}) => {
+const DataUseTermsSelector: FC<DataUseTermsSelectorProps> = ({ dataUseTermsType, setDataUseTermsType }) => {
     return (
         <>
             <div>
@@ -24,18 +22,18 @@ const DataUseTermsSelector: FC<DataUseTermsSelectorProps> = ({
                     checked={dataUseTermsType === openDataUseTermsType}
                     className='h-4 w-4 p-2 border-gray-300 text-iteal-600 focus:ring-iteal-600 inline-block'
                 />
-                <label
-                    htmlFor='data-use-open'
-                    className='ml-2 h-4 p-2 text-sm font-medium leading-6 text-gray-900'
-                >
+                <label htmlFor='data-use-open' className='ml-2 h-4 p-2 text-sm font-medium leading-6 text-gray-900'>
                     <Unlocked className='h-4 w-4 inline-block mr-2 -mt-1' />
                     Open
                 </label>
                 <div className='text-xs pl-8 text-gray-500 pb-4'>
-                    Anyone can use and share the data (though we believe researchers should exercise
-                    scientific etiquette, including the importance of citation). Data will be released
-                    to the INSDC databases shortly after submission.{' '}
-                    <a href={routes.datauseTermsPage()} className='text-primary-600'>Find out more</a>.
+                    Anyone can use and share the data (though we believe researchers should exercise scientific
+                    etiquette, including the importance of citation). Data will be released to the INSDC databases
+                    shortly after submission.{' '}
+                    <a href={routes.datauseTermsPage()} className='text-primary-600'>
+                        Find out more
+                    </a>
+                    .
                 </div>
             </div>
             <div>
@@ -55,9 +53,12 @@ const DataUseTermsSelector: FC<DataUseTermsSelectorProps> = ({
                     Restricted
                 </label>
                 <div className='text-xs pl-8 text-gray-500 mb-4'>
-                    Data will be restricted for a period of time. The sequences will be available but
-                    there will be limitations on how they can be used by others.{' '}
-                    <a href={routes.datauseTermsPage()} className='text-primary-600'>Find out more</a>.
+                    Data will be restricted for a period of time. The sequences will be available but there will be
+                    limitations on how they can be used by others.{' '}
+                    <a href={routes.datauseTermsPage()} className='text-primary-600'>
+                        Find out more
+                    </a>
+                    .
                 </div>
             </div>
         </>
