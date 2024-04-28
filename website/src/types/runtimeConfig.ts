57391fab6a05e52b3687cb495f2a2f2a83dd52e3
Theo Sanderson <theo@theo.io>
2024-04-28 23:21:57 +0100

    put a minimum on secret size

--- website/src/types/runtimeConfig.ts
+++ website/src/types/runtimeConfig.ts
@@ -19,6 +19,6 @@ export const serverConfig = serviceUrls.merge(
 export const runtimeConfig = z.object({
     public: serviceUrls,
     serverSide: serverConfig,
-    backendKeycloakClientSecret: z.string(),
+    backendKeycloakClientSecret: z.string().min(5),
 });
 export type RuntimeConfig = z.infer<typeof runtimeConfig>;
