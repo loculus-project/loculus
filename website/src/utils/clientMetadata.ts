fabeae8b60fb5e24d82dd243ec657be85c9fda79
Theo Sanderson <theo@sndrsn.co.uk>
2024-05-02 13:44:43 +0100

    feat(deployment): add keycloak client secret (#1732)

--- website/src/utils/clientMetadata.ts
+++ website/src/utils/clientMetadata.ts
@@ -1,7 +1,23 @@
-// TODO: #1337 Move to config
-export const clientMetadata = {
+import { getRuntimeConfig } from '../config';
+
+const clientMetadata = {
     client_id: 'backend-client',
     response_types: ['code', 'id_token'],
-    client_secret: 'someSecret',
     public: true,
+};
+
+export const getClientMetadata = () => {
+    return { ...clientMetadata, client_secret: getClientSecret() };
+};
+
+const getClientSecret = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (import.meta.env === undefined) {
+        return 'dummySecret';
+    }
+    const configDir = import.meta.env.CONFIG_DIR;
+    if (typeof configDir !== 'string' || configDir === '') {
+        return 'dummySecret';
+    }
+    return getRuntimeConfig().backendKeycloakClientSecret;
 };
