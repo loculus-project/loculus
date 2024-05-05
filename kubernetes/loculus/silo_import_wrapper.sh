9871e80a6cedb4f511f67096cdb709b75fe2c1b5
Theo Sanderson <theo@sndrsn.co.uk>
2024-05-05 22:04:32 +0100

    update

--- kubernetes/loculus/silo_import_wrapper.sh
+++ kubernetes/loculus/silo_import_wrapper.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Infinite loop to keep running the script
 while true
 do
-    # Execute your script
     sh /silo_import_job.sh
-    
-    # Wait for 30 seconds before running it again
     sleep 30
 done
