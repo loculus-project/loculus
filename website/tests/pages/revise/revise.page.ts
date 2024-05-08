ca431d7a31f5148303acccf7e60f4d3db12475e1
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-05-09 01:11:24 +0200

    test(e2e): no need to create file, can setInput from buffer

--- website/tests/pages/revise/revise.page.ts
+++ website/tests/pages/revise/revise.page.ts
@@ -1,7 +1,4 @@
-import { unlinkSync, writeFileSync } from 'fs';
-
-import type { Locator, Page } from '@playwright/test';
-import { v4 as uuid } from 'uuid';
+import type { Page } from '@playwright/test';
 
 import { routes } from '../../../src/routes/routes.ts';
 import type { Accession } from '../../../src/types/backend.ts';
@@ -9,32 +6,26 @@ import { baseUrl, dummyOrganism, sequencesTestFile } from '../../e2e.fixture';
 import { createModifiedFileContent } from '../../util/createFileContent.ts';
 
 export class RevisePage {
-    public readonly submitButton: Locator;
-    private readonly temporaryMetadataFile: string = `./tests/testData/${uuid()}_metadata.tsv`;
-
-    constructor(public readonly page: Page) {
-        this.submitButton = page.getByRole('button', { name: 'Submit' });
-    }
+    constructor(public readonly page: Page) {}
 
     public async goto(groupId: number) {
         await this.page.goto(`${baseUrl}${routes.revisePage(dummyOrganism.key, groupId)}`);
     }
 
-    public async uploadSequenceData(file: string = sequencesTestFile) {
+    public async submitRevisedData(accessions: Accession[]) {
+        await Promise.all([this.setSequenceFile(), this.setRevisedMetadataFile(accessions)]);
+        await this.page.getByRole('button', { name: 'Submit' }).click();
+    }
+
+    private async setSequenceFile(file: string = sequencesTestFile) {
         await this.page.getByLabel('Sequence file').setInputFiles(file);
     }
 
-    public async submitRevisedData(accessions: Accession[]) {
-        try {
-            await Promise.all([this.uploadSequenceData(), this.uploadRevisedMetadata(accessions)]);
-            await this.submitButton.click();
-        } finally {
-            unlinkSync(this.temporaryMetadataFile);
-        }
-    }
-
-    private async uploadRevisedMetadata(accessions: Accession[]) {
-        writeFileSync(this.temporaryMetadataFile, createModifiedFileContent(accessions).metadataContent);
-        await this.page.getByLabel('Metadata file').setInputFiles(this.temporaryMetadataFile);
+    private async setRevisedMetadataFile(accessions: Accession[]) {
+        await this.page.getByLabel('Metadata file').setInputFiles({
+            name: 'metadata.tsv',
+            mimeType: 'text/plain',
+            buffer: Buffer.from(createModifiedFileContent(accessions).metadataContent),
+        });
     }
 }
