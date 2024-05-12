40d8dbc4949475c4bbc86fc35df0ba8fc15e1f9a
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-05-12 20:42:24 +0200

    Deflake seqset tests

--- website/tests/pages/seqsets/seqset.page.ts
+++ website/tests/pages/seqsets/seqset.page.ts
@@ -7,32 +7,25 @@ export class SeqSetPage {
     constructor(public readonly page: Page) {}
 
     public async gotoList() {
-        await this.page.goto(`${baseUrl}/seqsets`, { waitUntil: 'load' });
-        await this.waitForLoad();
+        await this.page.goto(`${baseUrl}/seqsets`);
     }
 
     public async gotoDetail(seqSetName: string = testSeqSet.name) {
         await this.gotoList();
         await this.page.getByText(seqSetName).first().click();
-        await this.waitForLoad();
-    }
-
-    public async waitForLoad() {
-        await this.page.waitForLoadState('domcontentloaded', { timeout: 15000 });
-        await this.page.waitForLoadState('load', { timeout: 30000 });
-        await this.page.waitForLoadState('networkidle', { timeout: 5000 });
+        await this.page.waitForLoadState();
     }
 
     public async createTestSeqSet(seqSetName: string = testSeqSet.name) {
         await this.gotoList();
         await this.page.getByTestId('AddIcon').waitFor();
         await this.page.getByTestId('AddIcon').click();
-        await this.page.locator('#seqSet-name').fill(seqSetName);
-        await this.page.locator('#seqSet-description').fill(testSeqSet.description);
-        await this.page.locator('#loculus-focal-accession-input').fill(testSeqSet.focalLoculusAccessions);
-        await this.page.locator('#loculus-background-accession-input').fill(testSeqSet.backgroundLoculusAccessions);
+        await this.page.getByLabel('SeqSet name').fill(seqSetName);
+        await this.page.getByLabel('SeqSet description').fill(testSeqSet.description);
+        await this.page.getByLabel('Focal accessions').fill(testSeqSet.focalLoculusAccessions);
+        await this.page.getByLabel('Background accessions').fill(testSeqSet.backgroundLoculusAccessions);
         await this.page.getByRole('button', { name: 'Save' }).click();
-        await this.waitForLoad();
+        await this.page.waitForLoadState();
     }
 
     public async deleteTestSeqSet(seqSetName: string = testSeqSet.name) {
@@ -40,6 +33,6 @@ export class SeqSetPage {
         await this.page.getByText(seqSetName).first().click();
         await this.page.getByRole('button', { name: 'Delete' }).click();
         await this.page.getByRole('button', { name: 'Confirm' }).click();
-        await this.waitForLoad();
+        await this.page.waitForLoadState();
     }
 }
