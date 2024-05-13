6a338027600aafac7b9b3443f5dae96437057432
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-05-13 17:44:23 +0200

    chore: Add comments why warnings ignored

--- website/tests/util/throwOnConsole.ts
+++ website/tests/util/throwOnConsole.ts
@@ -1,15 +1,15 @@
 import { type ConsoleMessage, type Page } from '@playwright/test';
 
 const messagesToIgnore = [
-    /Ignoring Event: localhost/,
-    /\[vite\] connecting\.\.\./,
-    /\[vite\] connected\./,
-    /\[vite\] ready\./,
-    /Download the React DevTools for a better development experience: https:\/\/reactjs\.org\/link\/react-devtools/,
-    /\[astro-island\] Error hydrating .* TypeError: Importing a module script failed\./, // Fires in `astro dev` mode only
-    /downloadable font: kern: Too large subtable/,
-    /downloadable font: Table discarded/,
-    /Target page, context or browser has been closed/,
+    /Ignoring Event: localhost/, // Playwright specific warning
+    /\[vite\] connecting\.\.\./, // Astro dev specific warning
+    /\[vite\] connected\./, // Astro dev specific warning
+    /\[vite\] ready\./, // Astro dev specific warning
+    /Download the React DevTools for a better development experience: https:\/\/reactjs\.org\/link\/react-devtools/, // React info, not an error
+    /\[astro-island\] Error hydrating .* TypeError: Importing a module script failed\./, // Fires in `astro dev` mode and only on webkit, related to preview apparently
+    /downloadable font: kern: Too large subtable/, // firefox only, keycloak only, https://github.com/keycloak/keycloak/issues/29486
+    /downloadable font: Table discarded/, // firefox only, keycloak only
+    /Target page, context or browser has been closed/, // Playwright specific warning after browser is closed
 ];
 
 export function throwOnConsole(page: Page) {
