1cfcefaf397f6f5b7130565f77316ff8e5a4d5f9
Theo Sanderson <theo@theo.io>
2024-05-21 14:14:10 +0100

    update

--- website/src/components/SearchPage/useQueryAsState.js
+++ website/src/components/SearchPage/useQueryAsState.js
@@ -1,28 +1,36 @@
 import { useState, useEffect } from 'react';
 
+const MAX_URL_LENGTH = 2000; // Set a maximum URL length threshold
+
 export default function useQueryAsState(defaultDict) {
     const [valueDict, setValueDict] = useState(defaultDict);
+    const [useUrlStorage, setUseUrlStorage] = useState(true);
+
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
-        
         const newDict = {};
         for (const [key, value] of urlParams) {
             newDict[key] = value;
         }
         setValueDict(newDict);
-    }
-    , []);
+    }, []);
+
     useEffect(() => {
-        const urlParams = new URLSearchParams();
-        for (const [key, value] of Object.entries(valueDict)) {
-            urlParams.set(key, value);
+        if (useUrlStorage) {
+            const urlParams = new URLSearchParams();
+            for (const [key, value] of Object.entries(valueDict)) {
+                urlParams.set(key, value);
+            }
+            const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + urlParams.toString();
+            
+            if (newurl.length > MAX_URL_LENGTH) {
+                setUseUrlStorage(false);
+                window.history.replaceState({ path: window.location.pathname }, '', window.location.pathname);
+            } else {
+                window.history.replaceState({ path: newurl }, '', newurl);
+            }
         }
-        const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + urlParams.toString();
-        window.history.replaceState({path:newurl},'',newurl);
-    }
-    , [valueDict]);
+    }, [valueDict, useUrlStorage]);
 
-
-    return [valueDict, setValueDict]
-
+    return [valueDict, setValueDict];
 }
