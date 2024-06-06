df9c57ddce1e6ec796345b5d975fec52da99c7c9
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-06-06 17:43:53 +0200

    Fix weird error I introduced when merging changes

--- ingest/scripts/group_segments.py
+++ ingest/scripts/group_segments.py
@@ -3,11 +3,8 @@
 import hashlib
 import json
 import logging
-import pathlib
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Final
-
 import click
 import pandas as pd
 import yaml
@@ -25,7 +22,7 @@ logging.basicConfig(
 class Config:
     compound_country_field: str
     fasta_id_field: str
-    segment_specific_fields: list[str]  # What does this field mean?
+    segment_specific_fields: list[str]
     nucleotide_sequences: list[str]
     segmented: bool
 
@@ -88,32 +85,31 @@ def main(
     logger.setLevel(log_level)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
-
-    full_config = yaml.safe_load(pathlib.Path(config_file).read_text(encoding="utf-8"))
-    relevant_config = {key: full_config[key] for key in Config.__annotations__}
-    config = Config(**relevant_config)
-    logger.info(config)
-
-    if not config.segmented:
-        raise ValueError({"ERROR: You are running a function that requires segmented data"})
-
+    with open(config_file) as file:
+        full_config = yaml.safe_load(file)
+        relevant_config = {key: full_config[key] for key in Config.__annotations__}
+        config = Config(**relevant_config)
+    logger.debug(config)
     logger.info(f"Reading metadata from {input_metadata}")
     metadata_df: pd.DataFrame = pd.read_json(input_metadata, dtype=str, orient="index")
     metadata_df.reset_index(inplace=True)
     metadata_df.rename(columns={"index": "submissionId"}, inplace=True)
     number_of_segmented_records = len(metadata_df)
 
-    segments = config.nucleotide_sequences
-    segments = []
+    if not config.segmented:
+        raise ValueError({"ERROR: tried to get segment for non-segmented virus"})
+
+    segments: list[str] = config.nucleotide_sequences if config.nucleotide_sequences else []
     # Group sequences according to isolate, collection date and isolate specific values
-    # These are the fields that are expected to be identical across all segments for a given isolate
-    shared_fields: Final = (
+    isolate_specific_cols = [
+        "specimen_collector_sample_id",
+        "sample_collection_date",
+    ] + list(
         set(metadata_df.columns)
         - set(config.segment_specific_fields)
-        - {"hash", "submissionId", "segment"}
+        - set(["hash", "submissionId", "segment"])
     )
-    grouped = metadata_df.groupby(list(shared_fields))
-    logger.info(f"Fields required to be identical for grouping: {shared_fields}")
+    grouped = metadata_df.groupby(isolate_specific_cols)
     # Add joint_accession value: concatenated list of NCBI accession values
     metadata_df["joint_accession"] = ""
 
@@ -125,7 +121,7 @@ def main(
     for g in grouped.groups:  # noqa: PLR1702
         args = g
         isolate = args[0]
-        isolate_group = grouped.get_group(args)
+        isolate_group = grouped.get_group((args))
         # If isolate is not given do not group segments
         if isolate:
             if len(isolate_group) > len(config.nucleotide_sequences):
@@ -163,55 +159,48 @@ def main(
                 processed_seq,
                 single=True,
             )
-        logging.info(
-            f"Total of {len(metadata_df["joint_accession"].unique())} joint sequences after joining"
+    logging.info(
+        f"Total of {len(metadata_df["joint_accession"].unique())} joint sequences after joining"
+    )
+    if number_of_segmented_records // len(config.nucleotide_sequences) > len(
+        metadata_df["joint_accession"].unique()
+    ):
+        raise ValueError(
+            {
+                "ERROR: After join there are less records than expected if all records have"
+                " data for all segments - stopping as this indicates a join error!"
+            }
         )
-        if number_of_segmented_records // len(config.nucleotide_sequences) > len(
-            metadata_df["joint_accession"].unique()
-        ):
-            raise ValueError(
-                {
-                    "ERROR: After join there are less records than expected if all records have"
-                    " data for all segments - stopping as this indicates a join error!"
-                }
-            )
-
-        # Add segment specific metadata for the segments
-        metadata: list[dict[str, str]] = []
-
-        for acc in metadata_df["joint_accession"].unique():
-            table = metadata_df.loc[metadata_df["joint_accession"] == acc]
-            new_row = {}
-            for col in metadata_df.columns:
-                if col not in config.segment_specific_fields and col not in {
-                    "hash",
-                    "submissionId",
-                    "segment",
-                }:
-                    new_row[col] = table[col].unique()[0]
-            for field in config.segment_specific_fields:
-                try:
-                    for segment in segments:
-                        if len(table.loc[table["segment"] == segment]) > 0:
-                            new_row[field + "_" + segment] = table.loc[
-                                table["segment"] == segment, field
-                            ].iloc[0]
-                        else:
-                            new_row[field + "_" + segment] = ""
-                except Exception as e:
-                    logger.warn(
-                        f"Unable to find field: {field}, for table: {table}. Exception: {e}"
-                    )
-            new_row["hash"] = hashlib.md5("".join(table["hash"]).encode()).hexdigest()  # noqa: S324
-            new_row["submissionId"] = new_row["joint_accession"]
-            metadata.append(new_row)
-
-        meta_dict = {rec["joint_accession"]: rec for rec in metadata}
-
-        Path(output_metadata).write_text(json.dumps(meta_dict, indent=4), encoding="utf-8")
-        Path(output_seq).write_text(json.dumps(processed_seq, indent=4), encoding="utf-8")
-
-        logging.info(f"Saved metadata for {len(metadata)} sequences")
+    ## Add segment_specific_fields metadata for the segments
+    metadata: list[dict[str, str]] = []
+    for acc in metadata_df["joint_accession"].unique():
+        table = metadata_df.loc[metadata_df["joint_accession"] == acc]
+        new_row = {}
+        for col in metadata_df.columns:
+            if col not in config.segment_specific_fields and col not in [
+                "hash",
+                "submissionId",
+                "segment",
+            ]:
+                new_row[col] = table[col].unique()[0]
+        for field in config.segment_specific_fields:
+            try:
+                for segment in segments:
+                    if len(table.loc[table["segment"] == segment]) > 0:
+                        new_row[field + "_" + segment] = table.loc[
+                            table["segment"] == segment, field
+                        ].iloc[0]
+                    else:
+                        new_row[field + "_" + segment] = ""
+            except Exception as e:
+                logger.warn(f"Unable to find field: {field}, for table: {table}. Exception: {e}")
+        new_row["hash"] = hashlib.md5("".join(table["hash"]).encode()).hexdigest()
+        new_row["submissionId"] = new_row["joint_accession"]
+        metadata.append(new_row)
+    meta_dict = {rec["joint_accession"]: rec for rec in metadata}
+    Path(output_metadata).write_text(json.dumps(meta_dict, indent=4), encoding="utf-8")
+    Path(output_seq).write_text(json.dumps(processed_seq, indent=4), encoding="utf-8")
+    logging.info(f"Saved metadata for {len(metadata)} sequences")
 
 
 if __name__ == "__main__":
