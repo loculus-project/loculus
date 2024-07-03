1407d99f9fdcb5e12780454f9933ff6f2c8168f3
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-07-03 14:49:16 +0200

    Add back missing spaces

--- website/src/components/SearchPage/DisplaySearchDocs.tsx
+++ website/src/components/SearchPage/DisplaySearchDocs.tsx
@@ -36,11 +36,11 @@ const DisplaySearchDocs: FC = () => {
                         <h4 className='font-bold text-l mb-4 text-primary-700'>Nucleotide Mutations and Insertions</h4>
                         <p className='mb-2'>
                             For a single-segmented organism, nucleotide mutations have the format{' '}
-                            <b>&lt;position&gt;&lt;base&gt;</b> or
-                            <b>&lt;base_ref&gt;&lt;position&gt;&lt;base&gt;</b>. A <b>&lt;base&gt;</b> can be one of the
-                            four nucleotides <b>A</b>, <b>T</b>, <b>C</b>, and <b>G</b>. It can also be <b>-</b> for
-                            deletion and <b>N</b> for unknown. For example if the reference sequence is <b>A</b> at
-                            position <b>23</b> both: <b>23T</b> and <b>A23T</b> will yield the same results.
+                            <b>&lt;position&gt;&lt;base&gt;</b> or <b>&lt;base_ref&gt;&lt;position&gt;&lt;base&gt;</b>.
+                            A <b>&lt;base&gt;</b> can be one of the four nucleotides <b>A</b>, <b>T</b>, <b>C</b>, and{' '}
+                            <b>G</b>. It can also be <b>-</b> for deletion and <b>N</b> for unknown. For example if the
+                            reference sequence is <b>A</b> at position <b>23</b> both: <b>23T</b> and <b>A23T</b> will
+                            yield the same results.
                         </p>
                         <p className='mb-2'>
                             If your organism is multi-segmented you must append the name of the segment to the start of
@@ -56,7 +56,7 @@ const DisplaySearchDocs: FC = () => {
                     <div className='mb-4'>
                         <h4 className='font-bold text-l mb-4 text-primary-700'>Amino Acid Mutations and Insertions</h4>
                         <p className='mb-2'>
-                            An amino acid mutation has the format <b>&lt;gene&gt;:&lt;position&gt;&lt;base&gt;</b> or
+                            An amino acid mutation has the format <b>&lt;gene&gt;:&lt;position&gt;&lt;base&gt;</b> or{' '}
                             <b>&lt;gene&gt;:&lt;base_ref&gt;&lt;position&gt;&lt;base&gt;</b>. A <b>&lt;base&gt;</b> can
                             be one of the 20 amino acid codes. It can also be <b>-</b> for deletion and <b>X</b> for
                             unknown. Example: <b>E:57Q</b>.
@@ -71,10 +71,9 @@ const DisplaySearchDocs: FC = () => {
                         <h4 className='font-bold text-l mb-4 text-primary-700'>Insertion Wildcards</h4>
                         <p className='mb-2'>
                             Loculus supports insertion queries that contain wildcards <b>?</b>. For example{' '}
-                            <b>ins_S:214:?EP?</b>
-                            will match all cases where segment <b>S</b> has an insertion of <b>EP</b> between the
-                            positions 214 and 215 but also an insertion of other AAs which include the <b>EP</b>, e.g.
-                            the insertion <b>EPE</b> will be matched.
+                            <b>ins_S:214:?EP?</b> will match all cases where segment <b>S</b> has an insertion of{' '}
+                            <b>EP</b> between the positions 214 and 215 but also an insertion of other AAs which include
+                            the <b>EP</b>, e.g. the insertion <b>EPE</b> will be matched.
                         </p>
                         <p className='mb-2'>
                             You can also use wildcards to match any insertion at a given position. For example{' '}
