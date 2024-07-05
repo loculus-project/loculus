c838262c9d962b2b53f78c4951fd31bb9a26c26e
Theo Sanderson <theo@sndrsn.co.uk>
2024-07-05 19:06:59 +0100

    Update NeedAGroup.tsx

--- website/src/components/common/NeedAGroup.tsx
+++ website/src/components/common/NeedAGroup.tsx
@@ -6,14 +6,14 @@ export const NeedAGroup = () => (
         <DashiconsGroups className='w-12 h-12 inline-block mr-2' />
         <div>
             <p>
-                Sequences can only be submitted to the database by users who are part of a <i>group</i>.
+                Sequences can only be submitted to the database by users who are part of a <i>submitting group</i>.
             </p>
             <p className='mt-3'>
                 To submit to the database, please either{' '}
                 <a href={routes.createGroup()} className='underline'>
                     create a submitting group
                 </a>{' '}
-                (a submitting group with one member is not a problem!) or ask a submitting group administrator to add
+                or ask a group administrator to add
                 you to an existing group.
             </p>
         </div>
