dc1d69978593d07cf0db6e5df7dce23b89ef56d6
Theo Sanderson <theo@theo.io>
2024-07-06 00:59:14 +0100

    format

--- website/src/components/SearchPage/DownloadDialog/DownloadDialog.tsx
+++ website/src/components/SearchPage/DownloadDialog/DownloadDialog.tsx
@@ -1,5 +1,6 @@
-import { type FC, useState } from 'react';
 import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { type FC, useState } from 'react';
+
 import { ActiveDownloadFilters } from './ActiveDownloadFilters.tsx';
 import { DownloadButton } from './DownloadButton.tsx';
 import { DownloadForm } from './DownloadForm.tsx';
@@ -33,27 +34,29 @@ export const DownloadDialog: FC<DownloadDialogProps> = ({
             <button className='outlineButton' onClick={openDialog}>
                 Download
             </button>
-            <Dialog open={isOpen} onClose={closeDialog} className="relative z-10">
-                <div className="fixed inset-0 bg-black bg-opacity-25" />
-                <div className="fixed inset-0 overflow-y-auto">
-                    <div className="flex min-h-full items-center justify-center p-4 text-center">
-                        <DialogPanel className="w-full max-w-5xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl">
-                            <DialogTitle
-                                as="h3"
-                                className="text-2xl font-bold leading-6 text-gray-900 mb-4"
-                            >
+            <Dialog open={isOpen} onClose={closeDialog} className='relative z-10'>
+                <div className='fixed inset-0 bg-black bg-opacity-25' />
+                <div className='fixed inset-0 overflow-y-auto'>
+                    <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                        <DialogPanel className='w-full max-w-5xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl'>
+                            <DialogTitle as='h3' className='text-2xl font-bold leading-6 text-gray-900 mb-4'>
                                 Download
                             </DialogTitle>
                             <button
-                                className="absolute right-2 top-2 text-gray-400 hover:text-gray-500"
+                                className='absolute right-2 top-2 text-gray-400 hover:text-gray-500'
                                 onClick={closeDialog}
                             >
-                                <span className="sr-only">Close</span>
-                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                <span className='sr-only'>Close</span>
+                                <svg className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                                    <path
+                                        strokeLinecap='round'
+                                        strokeLinejoin='round'
+                                        strokeWidth={2}
+                                        d='M6 18L18 6M6 6l12 12'
+                                    />
                                 </svg>
                             </button>
-                            <div className="mt-2">
+                            <div className='mt-2'>
                                 <ActiveDownloadFilters
                                     lapisSearchParameters={lapisSearchParameters}
                                     hiddenFieldValues={hiddenFieldValues}
@@ -73,7 +76,12 @@ export const DownloadDialog: FC<DownloadDialogProps> = ({
                                         />
                                         <span className='text-sm'>
                                             I agree to the {/* TODO(862) */}
-                                            <a href={routes.datauseTermsPage()} className='underline' target='_blank' rel="noopener noreferrer">
+                                            <a
+                                                href={routes.datauseTermsPage()}
+                                                className='underline'
+                                                target='_blank'
+                                                rel='noopener noreferrer'
+                                            >
                                                 data use terms
                                             </a>
                                             .
