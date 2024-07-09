1ca6c5a5cfb8c4fddcb13bdc9f1cdb638e2aaf3e
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-09 12:44:37 +0200

    chore(website): migrate deprecated Transition.Child to TransitionChild

--- website/src/components/SequenceDetailsPage/ReferenceSequenceLinkButton.tsx
+++ website/src/components/SequenceDetailsPage/ReferenceSequenceLinkButton.tsx
@@ -1,4 +1,4 @@
-import { Dialog, Transition, DialogPanel, DialogTitle } from '@headlessui/react';
+import { Dialog, Transition, TransitionChild, DialogPanel, DialogTitle } from '@headlessui/react';
 import React, { Fragment } from 'react';
 
 import { type ReferenceAccession } from '../../types/referencesGenomes';
@@ -36,7 +36,7 @@ const ReferenceSequenceLinkButton: React.FC<Props> = ({ reference }) => {
             </button>
             <Transition appear show={isOpen} as={Fragment}>
                 <Dialog as='div' className='relative z-10' onClose={closeDialog}>
-                    <Transition.Child
+                    <TransitionChild
                         as={Fragment}
                         enter='ease-out duration-300'
                         enterFrom='opacity-0'
@@ -46,11 +46,11 @@ const ReferenceSequenceLinkButton: React.FC<Props> = ({ reference }) => {
                         leaveTo='opacity-0'
                     >
                         <div className='fixed inset-0 bg-black bg-opacity-25' />
-                    </Transition.Child>
+                    </TransitionChild>
 
                     <div className='fixed inset-0 overflow-y-auto'>
                         <div className='flex min-h-full items-center justify-center p-4 text-center'>
-                            <Transition.Child
+                            <TransitionChild
                                 as={Fragment}
                                 enter='ease-out duration-300'
                                 enterFrom='opacity-0 scale-95'
@@ -96,7 +96,7 @@ const ReferenceSequenceLinkButton: React.FC<Props> = ({ reference }) => {
                                         )}
                                     </div>
                                 </DialogPanel>
-                            </Transition.Child>
+                            </TransitionChild>
                         </div>
                     </div>
                 </Dialog>
