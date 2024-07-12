24605ec9ee5c2916dbb231ee8baa79ceb14a775b
Theo Sanderson <theo@theo.io>
2024-07-12 16:43:09 +0200

    format

--- website/src/components/SearchPage/fields/TextField.tsx
+++ website/src/components/SearchPage/fields/TextField.tsx
@@ -112,9 +112,7 @@ export const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, Text
                 rows={hasFocus || (fieldValue !== undefined && fieldValue.toString().split('\n').length > 1) ? 4 : 1}
                 className={`rounded-md block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${className}`}
                 placeholder=''
-            >
-                
-            </textarea>
+            ></textarea>
 
             <label
                 htmlFor={id}
