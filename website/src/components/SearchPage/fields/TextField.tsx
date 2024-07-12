f0771cc1874bf85b1cc10ba48c7106b45c7de58f
Theo Sanderson <theo@theo.io>
2024-07-12 16:42:15 +0200

    fix

--- website/src/components/SearchPage/fields/TextField.tsx
+++ website/src/components/SearchPage/fields/TextField.tsx
@@ -113,7 +113,7 @@ export const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, Text
                 className={`rounded-md block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${className}`}
                 placeholder=''
             >
-                {props.fieldValue}
+                
             </textarea>
 
             <label
