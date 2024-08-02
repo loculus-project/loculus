e0a65bfd25beddcf7c2209b7b7d1d15e104bfef5
Theo Sanderson <theo@sndrsn.co.uk>
2024-08-03 00:50:16 +0100

    Update V1__init.sql

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -197,6 +197,7 @@ create table seqset_records (
 create table seqset_to_records (
     seqset_record_id bigserial not null,
     seqset_id text not null,
+    test text not null,
     seqset_version int8 not null,
 
     primary key (seqset_record_id, seqset_id, seqset_version),
@@ -216,3 +217,4 @@ create table audit_log (
     timestamp timestamp not null default now(),
     description text not null
 );
+
