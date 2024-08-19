7d22f7620eb0fa3a9484e64c351e90a27cb120d6
Theo Sanderson <theo@sndrsn.co.uk>
2024-08-19 23:57:22 +0100

    initial refactor

--- website/src/routes/navigationItems.ts
+++ website/src/routes/navigationItems.ts
@@ -1,46 +1,43 @@
 import { bottomNavigationItems } from './bottomNavigationItems.ts';
 import { routes } from './routes.ts';
+
 export const navigationItems = {
     top: topNavigationItems,
     bottom: bottomNavigationItems,
 };
 
-function topNavigationItems(organism: string | undefined, isLoggedIn: boolean, loginUrl: string | undefined) {
-    if (organism === undefined) {
-        return [
-            {
-                text: 'Browse',
-                path: routes.organismSelectorPage('search'),
-            },
-            {
-                text: 'Submit',
-                path: routes.organismSelectorPage('submission'),
-            },
-            {
-                text: 'SeqSets',
-                path: routes.seqSetsPage(),
-            },
-            ...(isLoggedIn
-                ? [{ text: 'My account', path: routes.userOverviewPage() }]
-                : [{ text: 'Login', path: loginUrl! }]),
-        ];
-    }
-
+function getCommonItems(organism: string | undefined) {
     return [
         {
             text: 'Browse',
-            path: routes.searchPage(organism),
+            path: organism ? routes.searchPage(organism) : routes.organismSelectorPage('search'),
         },
         {
             text: 'Submit',
-            path: routes.submissionPageWithoutGroup(organism),
+            path: organism ? routes.submissionPageWithoutGroup(organism) : routes.organismSelectorPage('submission'),
         },
         {
             text: 'SeqSets',
             path: routes.seqSetsPage(),
         },
-        ...(isLoggedIn
-            ? [{ text: 'My account', path: routes.userOverviewPage(organism) }]
-            : [{ text: 'Login', path: loginUrl! }]),
     ];
+}
+
+function getAccountItem(isLoggedIn: boolean, loginUrl: string | undefined, organism: string | undefined) {
+    return isLoggedIn
+        ? {
+              text: 'My account',
+              path: organism ? routes.userOverviewPage(organism) : routes.userOverviewPage(),
+          }
+        : {
+              text: 'Login',
+              path: loginUrl!,
+          };
+}
+
+function topNavigationItems(organism: string | undefined, isLoggedIn: boolean, loginUrl: string | undefined) {
+    const commonItems = getCommonItems(organism);
+    const accountItem = getAccountItem(isLoggedIn, loginUrl, organism);
+
+    return [...commonItems, accountItem];
 }
