d3f3f3b9923848b741edc205f2677373abc568d3
Theo Sanderson <theo@sndrsn.co.uk>
2024-08-23 16:34:06 +0100

    distinct loculus color scheme

---
+++ website/colors.js
@@ -0,0 +1,17 @@
+export const mainTailwindColor = {
+    '50': '#f3f6fb',
+    '100': '#e4e9f5',
+    '200': '#cfdaee',
+    '300': '#aec1e2',
+    '400': '#88a1d2',
+    '500': '#6b84c6',
+    '600': '#586bb8',
+    '700': '#4d5ba8',
+    '800': '#3e467e',
+    '900': '#3a416e',
+    '950': '#272b44',
+  };
+
+module.exports = {
+    mainTailwindColor,
+};
