9c69d333182a7ddb82d4a58d1cd29e23a8ad7a24
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-09-04 18:17:26 +0200

    Add get-released to public endpoints

--- backend/src/main/kotlin/org/loculus/backend/config/SecurityConfig.kt
+++ backend/src/main/kotlin/org/loculus/backend/config/SecurityConfig.kt
@@ -57,6 +57,7 @@ class SecurityConfig {
         "/get-seqset-records",
         "/get-seqset-cited-by-publication",
         "/get-author",
+        "/*/get-released-data",
     )
 
     private val debugEndpoints = arrayOf(
