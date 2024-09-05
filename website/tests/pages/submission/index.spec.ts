ec2c3c76685c9a7faaf48a50a49f0b6e4d714c69
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-09-05 22:00:09 +0200

    some formatting fixes

--- website/tests/pages/submission/index.spec.ts
+++ website/tests/pages/submission/index.spec.ts
@@ -1,6 +1,4 @@
 import { routes } from '../../../src/routes/routes.ts';
-import { restrictedDataUseTermsType } from '../../../src/types/backend.ts';
-import { dateTimeInMonths } from '../../../src/utils/DateTimeInMonths.tsx';
 import { baseUrl, dummyOrganism, expect, test, testSequenceCount } from '../../e2e.fixture.ts';
 
 test.describe('The submit page', () => {
@@ -27,28 +25,9 @@ test.describe('The submit page', () => {
             waitUntil: 'load',
         });
 
-        const discardButton = submitPage.page.getByRole('button', { name: 'valid sequences' });
-        await discardButton.click({ timeout: 60000 });
-    });
-
-    test('should set data use terms', async ({ submitPage, loginAsTestUserTwo }) => {
-        const { groupId } = await loginAsTestUserTwo();
-        await submitPage.goto(groupId);
-
-        await Promise.all([submitPage.uploadSequenceData(), submitPage.uploadMetadata()]);
-        await submitPage.selectRestrictedDataUseTerms();
-        await submitPage.confirmationINSDCTerms.click();
-        await submitPage.confirmationNoPII.click();
-        await submitPage.submitButton.click();
-
-        await submitPage.page.waitForURL(`${baseUrl}${routes.userSequenceReviewPage(dummyOrganism.key, groupId)}`);
-        const releaseButton = await submitPage.page.getByRole('button', { name: 'Release' });
-        await expect(releaseButton).toBeVisible();
-        await releaseButton.click();
-        await submitPage.page.waitForURL(
-            `${baseUrl}${routes.mySequencesPage(dummyOrganism.key, groupId)}?dataUseTerms=RESTRICTED`,
-        );
-        submitPage.page.getByText(`Search returned ${testSequenceCount} sequence`);
+        const discardButton = submitPage.page.getByRole('button', { name: 'Discard sequences', exact: false });
+        await expect(discardButton).toBeVisible();
+        await discardButton.click();
     });
 
     test('should upload compressed files and submit', async ({ submitPage, loginAsTestUser }) => {
@@ -62,5 +41,25 @@ test.describe('The submit page', () => {
         await submitPage.submitButton.click();
 
         await submitPage.page.waitForURL(`${baseUrl}${routes.userSequenceReviewPage(dummyOrganism.key, groupId)}`);
+    });
+
+    test('should set data use terms', async ({ submitPage, loginAsTestUserTwo }) => {
+        const { groupId } = await loginAsTestUserTwo();
+        await submitPage.goto(groupId);
+
+        await Promise.all([submitPage.uploadSequenceData(), submitPage.uploadMetadata()]);
+        await submitPage.selectRestrictedDataUseTerms();
+        await submitPage.confirmationINSDCTerms.click();
+        await submitPage.confirmationNoPII.click();
+        await submitPage.submitButton.click();
+
+        await submitPage.page.waitForURL(`${baseUrl}${routes.userSequenceReviewPage(dummyOrganism.key, groupId)}`);
+        const releaseButton = submitPage.page.getByRole('button', { name: 'Release', exact: false });
+        await expect(releaseButton).toBeVisible();
+        await releaseButton.click();
+        await submitPage.page.waitForURL(
+            `${baseUrl}${routes.mySequencesPage(dummyOrganism.key, groupId)}?dataUseTerms=RESTRICTED`,
+        );
+        submitPage.page.getByText(`Search returned ${testSequenceCount} sequence`);
     });
 });
