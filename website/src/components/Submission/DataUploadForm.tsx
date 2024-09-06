6dcc4cfa4876ff4f8f53364eeebe96d96c1b9b31
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-09-06 16:39:11 +0200

    Update DataUploadForm.tsx

--- website/src/components/Submission/DataUploadForm.tsx
+++ website/src/components/Submission/DataUploadForm.tsx
@@ -398,7 +398,7 @@ const InnerDataUploadForm = ({
                             .
                         </p>
 
-                        {organism.startsWith('not-aligned-organism') && action === 'submit' && (
+                        {(organism.startsWith('not-aligned-organism') || organism.startsWith('dummy')) && action === 'submit' && (
                             <DevExampleData
                                 setExampleEntries={setExampleEntries}
                                 exampleEntries={exampleEntries}
