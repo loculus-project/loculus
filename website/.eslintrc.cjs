c85de378dae507f3a3b1aa2db6a85ef46e17caae
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-09-09 23:23:16 +0200

    Exclude playwright report and dist to avoid astro check from flooding terminal
    See https://github.com/withastro/roadmap/discussions/930

--- website/.eslintrc.cjs
+++ website/.eslintrc.cjs
@@ -25,7 +25,7 @@ module.exports = {
             version: 'detect', // React version. "detect" automatically picks the version you have installed.
         },
     },
-    ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.cjs', 'astro.config.mjs', 'colors.cjs'],
+    ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.cjs', 'astro.config.mjs', 'colors.cjs', 'playwright-report'],
     overrides: [
         {
             // Define the configuration for `.astro` file.
