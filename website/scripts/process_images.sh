4df831f8d4295b17320773260c21d2ee9789e63a
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-09-09 10:10:26 +0200

    feat(website): resize images to max-size used in website (#2725)
    Add script `process_images.sh` that allows recompressing in the future if new organisms are added.
    
    Implementing this recommendation from Google's PageSpeedInsights: https://pagespeed.web.dev/analysis/https-pathoplexus-org/gnxkqyl8t4?form_factor=mobile

---
+++ website/scripts/process_images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <image_directory>"
+    exit 1
+fi
+
+# Directory containing images
+IMAGE_DIR="$1"
+
+# Loop over all jpg files in the directory
+for img in "$IMAGE_DIR"/*.jpg; do
+    # Extract the filename without extension
+    filename=$(basename "$img" .jpg)
+
+    # Skip files that already have '_small' in their filename
+    if [[ $filename != *"_small" ]]; then
+        # Resize, compress the JPEG and save it
+        magick "$img" -resize 200x\> -strip -quality 85 "$IMAGE_DIR/${filename}_small.jpg"
+    fi
+done
