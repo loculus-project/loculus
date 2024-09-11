f74dff0838e8381426617e52c86b71889930b432
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-09-11 15:20:48 +0200

    save last snapshot to file

--- kubernetes/loculus/silo_import_job.sh
+++ kubernetes/loculus/silo_import_job.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 set -e
-
-lastSnapshot=""
+last_snapshot=""
 
 # Parse command-line arguments
 usage() {
-  echo "Usage: $0 [--lastSnapshot=UNIXTIMESTAMP]" >&2
+  echo "Usage: $0 [--last-snapshot=UNIXTIMESTAMP]"
   exit 1
 }
 
 for arg in "$@"; do
   case $arg in
-    --lastSnapshot=*)
-      lastSnapshot="${arg#*=}"
+    --last-snapshot=*)
+      last_snapshot="${arg#*=}"
       shift
       ;;
     *)
@@ -31,85 +30,86 @@ old_input_data_dir="$input_data_dir"/$(ls -1 "$input_data_dir" | sort -n | grep 
 
 new_input_data="$new_input_data_dir/data.ndjson.zst"
 new_input_header="$new_input_data_dir/header.txt"
+new_snapshot_time="$new_input_data_dir/snapshot_time.txt"
+current_snapshot_time="$input_data_dir/snapshot_time.txt"
+
 old_input_data="$old_input_data_dir/data.ndjson.zst"
 new_input_touchfile="$new_input_data_dir/processing"
 old_input_touchfile="$old_input_data_dir/processing"
 silo_input_data="$input_data_dir/data.ndjson.zst"
 
 delete_all_input () {
-  echo "Deleting all input data" >&2
+  echo "Deleting all input data"
   rm -f "$silo_input_data"
-  rm -rf "$new_input_data_dir" >&2
+  rm -rf "$new_input_data_dir"
   echo
 }
 
 download_data() {
   mkdir -p "$new_input_data_dir"
-  echo  "created $new_input_data_dir" >&2
+  echo  "created $new_input_data_dir"
   
   # Set flag to be cleared when processing succeeds to avoid getting stuck with no output data
   touch "$new_input_touchfile"
 
   released_data_endpoint="$BACKEND_BASE_URL/get-released-data?compression=zstd"
-  echo "calling $released_data_endpoint" >&2
+  echo "calling $released_data_endpoint"
   
   set +e
-  curl -o "$new_input_data" --fail-with-body "$released_data_endpoint"  -H "If-Modified-Since: $lastSnapshot" -D "$new_input_header" -w "%{http_code}"
+  response=$(curl -o "$new_input_data" --fail-with-body "$released_data_endpoint"  -H "If-Modified-Since: $last_snapshot" -D "$new_input_header" -w "%{http_code}")
   exit_code=$?
   set -e
-
   http_code="${response: -3}"
-
+  echo "http code: $http_code"
   if [ "$http_code" -eq 304 ]; then
-    echo "State in Loculus backend has not changed: HTTP 304 Not Modified." >&2
+    echo "State in Loculus backend has not changed: HTTP 304 Not Modified."
     rm -rf "$new_input_data_dir"
-    echo "$lastSnapshot"
     exit 0
+  fi
   if [ $exit_code -ne 0 ]; then
-    echo "Curl command failed with exit code $exit_code, cleaning up and exiting." >&2
+    echo "Curl command failed with exit code $exit_code, cleaning up and exiting."
     rm -rf "$new_input_data_dir"
-    echo "$lastSnapshot"
     exit $exit_code
   fi
-  
+
   last_modified=$(grep '^last-modified:' "$new_input_header" | awk '{print $2}')
-  echo "$last_modified"
+  echo "Last-modified from header: $last_modified"
+  echo "$last_modified" >> "$new_snapshot_time"
 
-  echo "downloaded sequences" >&2
+  echo "downloaded sequences"
   ls -l "$new_input_data_dir"
-  echo >&2
+  echo
 
-  echo "checking for old input data dir $old_input_data_dir" >&2
+  echo "checking for old input data dir $old_input_data_dir"
   if [[ -f "$old_input_data" ]]; then
     if [[ -f "$old_input_touchfile" ]]; then
-      echo "Old input data dir was not processed successfully" >&2
-      echo "Skipping hash check, deleting old input data dir" >&2
+      echo "Old input data dir was not processed successfully"
+      echo "Skipping hash check, deleting old input data dir"
       rm -rf "$old_input_data_dir"
     else
-      echo "Old input data dir was processed successfully" >&2
+      echo "Old input data dir was processed successfully"
       old_hash=$(md5sum < "$old_input_data" | awk '{print $1}')
       new_hash=$(md5sum < "$new_input_data" | awk '{print $1}')
-      echo "old hash: $old_hash" >&2
-      echo "new hash: $new_hash" >&2
+      echo "old hash: $old_hash"
+      echo "new hash: $new_hash"
       if [ "$new_hash" = "$old_hash" ]; then
-        echo "Hashes are equal, skipping preprocessing" >&2
-        echo "Deleting new input data dir $new_input_data_dir" >&2
+        echo "Hashes are equal, skipping preprocessing"
+        echo "Deleting new input data dir $new_input_data_dir"
         rm -rf "$new_input_data_dir"
-        echo "$lastSnapshot"
         exit 0
       else
-        echo "Hashes are unequal, deleting old input data dir" >&2
+        echo "Hashes are unequal, deleting old input data dir"
         rm -rf "$old_input_data_dir:?}"
       fi
     fi
   else
-    echo "No old input data dir found" >&2
+    echo "No old input data dir found"
   fi
-  echo >&2
+  echo
 }
 
 preprocessing() {
-  echo "Starting preprocessing" >&2
+  echo "Starting preprocessing"
 
   rm -f "$silo_input_data"
 
@@ -123,25 +123,27 @@ preprocessing() {
   set -e
 
   if [ $exit_code -ne 0 ]; then
-    echo "SiloApi command failed with exit code $exit_code, cleaning up and exiting." >&2
+    echo "SiloApi command failed with exit code $exit_code, cleaning up and exiting."
     delete_all_input # Delete input so that we don't skip preprocessing next time due to hash equality
     exit $exit_code
   else
-    echo "SiloApi command succeeded" >&2
-    echo "Removing touchfile $new_input_touchfile to indicate successful processing" >&2
+    echo "SiloApi command succeeded"
+    echo "Removing touchfile $new_input_touchfile to indicate successful processing"
     rm -f "$new_input_touchfile"
   fi
 
-  echo "preprocessing for $current_timestamp done" >&2
-  echo >&2
+  # Only set new snapshot time when siloApi also succeeds
+  cp "$new_snapshot_time" "$current_snapshot_time"
+  echo "preprocessing for $current_timestamp done"
+  echo
 }
 
 # Potential race condition: silo might not release non-current dir if it's still being used
 cleanup_output_data() {
   for dir_type in "input" "output"; do
     dir="/preprocessing/$dir_type"
-    echo "Removing all but the most recent $dir_type directory in $dir" >&2
-    cd $dir || { echo "$lastSnapshot"; exit 1; }
+    echo "Removing all but the most recent $dir_type directory in $dir"
+    cd $dir || exit
 
     if [ -n "$(ls -d -- */ 2>/dev/null)" ]; then
       directories=$(ls -dt -- */)
@@ -149,38 +151,37 @@ cleanup_output_data() {
       dir_num_to_keep_plus_one=$((dir_num_to_keep + 1))
       if [ "$(echo "$directories" | wc -l)" -gt $dir_num_to_keep ]; then
         dirs_to_keep=$(echo "$directories" | head -n $dir_num_to_keep)
-        echo "$directories" | tail -n "+$dir_num_to_keep_plus_one" | xargs rm -r >&2
-        echo "Kept: $dirs_to_keep" >&2
+        echo "$directories" | tail -n "+$dir_num_to_keep_plus_one" | xargs rm -r
+        echo "Kept: $dirs_to_keep"
       else
-        echo "No directories to delete." >&2
+        echo "No directories to delete."
       fi
     else
-      echo "No directories found." >&2
+      echo "No directories found."
     fi
 
-    cd - > /dev/null || { echo "$lastSnapshot"; exit 1; }
-    echo >&2
+    cd - > /dev/null || exit
+    echo
   done
 }
 
 main() {
-  echo "----------------------------------------" >&2
-  echo "Script started at: $(date)" >&2
+  echo "----------------------------------------"
+  echo "Script started at: $(date)"
 
-  echo "Current content of input data dir: $input_data_dir" >&2
+  echo "Current content of input data dir: $input_data_dir"
   ls -l $input_data_dir
-  echo "Current content of output data dir: /preprocessing/output" >&2
+  echo "Current content of output data dir: /preprocessing/output"
   ls -l /preprocessing/output
-  echo >&2
+  echo
 
   # cleanup at start in case we fail later
   cleanup_output_data
-  last_modified=$(download_data)
+  download_data
   preprocessing
-  echo "$last_modified"
 
-  echo "done" >&2
-  echo "----------------------------------------" >&2
+  echo "done"
+  echo "----------------------------------------"
 }
 
 main
--- kubernetes/loculus/silo_import_wrapper.sh
+++ kubernetes/loculus/silo_import_wrapper.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-lastSnapshot=0
+input_data_dir="/preprocessing/input"
+current_snapshot_time="$input_data_dir/snapshot_time.txt"
+
+if [ -f "$current_snapshot_time" ]; then
+    last_snapshot_time=$(cat "$FILE")
+else
+    last_snapshot_time=0
+fi
+
+echo "Data in SILO corresponds to data in Loculus at time: $current_snapshot_time"
 while true
 do
-    lastSnapshot=$(bash /silo_import_job.sh --lastSnapshot=$lastSnapshot)
+    bash /silo_import_job.sh --last-snapshot="$last_snapshot_time"
     sleep 30
 done
