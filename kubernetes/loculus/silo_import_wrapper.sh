18ba74d4e46a7d1398f16a1b4f8c6058baf9fb4f
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-09-11 15:45:13 +0200

    Update with loculus data anyways if longer than 1hour since last update

--- kubernetes/loculus/silo_import_wrapper.sh
+++ kubernetes/loculus/silo_import_wrapper.sh
@@ -3,15 +3,23 @@
 input_data_dir="/preprocessing/input"
 current_snapshot_time="$input_data_dir/snapshot_time.txt"
 
-if [ -f "$current_snapshot_time" ]; then
-    last_snapshot_time=$(cat "$FILE")
-else
-    last_snapshot_time=0
-fi
-
-echo "Data in SILO corresponds to data in Loculus at time: $current_snapshot_time"
 while true
 do
-    bash /silo_import_job.sh --last-snapshot="$last_snapshot_time"
+    if [ -f "$current_snapshot_time" ]; then
+        last_snapshot_time=$(cat "$current_snapshot_time" | tr -d '[:space:]')
+    else
+        last_snapshot_time=0
+    fi
+    echo "Data in SILO corresponds to data in Loculus at time: $last_snapshot_time"
+    
+    # Check if the difference is greater than or equal to 3600 seconds (1 hour)
+    current_time=$(date +%s)
+    time_diff=$((current_time - last_snapshot_time))
+    if [ "$time_diff" -ge 3600 ]; then
+        echo "Data in SILO is over 1h older than Loculus, ask for all data regardless of last-modified-since tag."
+        bash silo_import_job.sh --last-snapshot=0
+    else
+        bash silo_import_job.sh --last-snapshot="$last_snapshot_time"
+    fi
     sleep 30
 done
