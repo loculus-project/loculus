a72afeb0ebc52a14a625918cb1e272b821ca5e57
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-09-11 08:58:27 +0200

    Remove unused functions

--- backend/src/main/kotlin/org/loculus/backend/service/submission/UpdateTrackerTable.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/UpdateTrackerTable.kt
@@ -1,6 +1,3 @@
-import kotlinx.datetime.LocalDateTime
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 
 const val UPDATE_TRACKER_TABLE_NAME = "table_update_tracker"
@@ -9,14 +6,4 @@ object UpdateTrackerTable : Table(UPDATE_TRACKER_TABLE_NAME) {
     val lastTimeUpdatedDbColumn =
         datetime("last_time_updated").nullable()
     val tableNameColumn = varchar("table_name", 255)
-
-    val islatestUpdate = lastTimeUpdatedDbColumn eq latestUpdate()
-
-    private fun latestUpdate(): Expression<LocalDateTime?> {
-        val subQueryTable = alias("subQueryTable")
-        return wrapAsExpression(
-            subQueryTable
-                .select(subQueryTable[lastTimeUpdatedDbColumn].max()),
-        )
-    }
 }
