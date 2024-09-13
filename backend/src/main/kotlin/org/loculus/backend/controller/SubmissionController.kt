06f877abdd1a2d0eed20075a1914c0e4f8791a7b
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-09-13 10:08:34 +0200

    remove unnecessary imports

--- backend/src/main/kotlin/org/loculus/backend/controller/SubmissionController.kt
+++ backend/src/main/kotlin/org/loculus/backend/controller/SubmissionController.kt
@@ -43,8 +43,6 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.transaction.annotation.Isolation
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
