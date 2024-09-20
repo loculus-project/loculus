d38457bbb47877e4bef6902007a81b2e5597013c
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-09-20 14:52:42 +0200

    chore: make start_dev.sh maintainable by breaking lines (#2853)

--- backend/start_dev.sh
+++ backend/start_dev.sh
@@ -1,2 +1,17 @@
 #! /bin/sh
-./gradlew bootRun --args="--spring.datasource.url=jdbc:postgresql://localhost:5432/loculus --spring.datasource.username=postgres --spring.datasource.password=unsecure --loculus.config.path=../website/tests/config/backend_config.json --loculus.debug-mode=true --spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:8083/realms/loculus/protocol/openid-connect/certs --keycloak.user=backend --keycloak.password=backend --keycloak.realm=loculus --keycloak.client=backend-client --keycloak.url=http://localhost:8083"
+
+args=$(printf "%s " \
+  "--spring.datasource.url=jdbc:postgresql://localhost:5432/loculus" \
+  "--spring.datasource.username=postgres" \
+  "--spring.datasource.password=unsecure" \
+  "--loculus.config.path=../website/tests/config/backend_config.json" \
+  "--loculus.debug-mode=true" \
+  "--spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:8083/realms/loculus/protocol/openid-connect/certs" \
+  "--keycloak.user=backend" \
+  "--keycloak.password=backend" \
+  "--keycloak.realm=loculus" \
+  "--keycloak.client=backend-client" \
+  "--keycloak.url=http://localhost:8083"
+)
+
+./gradlew bootRun --args="$args"
