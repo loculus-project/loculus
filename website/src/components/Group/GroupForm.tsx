c61e04636f1f9997ce12f0a0c19605e1bdd3cb6a
Felix Hennig <mail@felixhennig.com>
2024-09-30 16:15:55 +0100

    cleanup field mapping stuff

--- website/src/components/Group/GroupForm.tsx
+++ website/src/components/Group/GroupForm.tsx
@@ -5,11 +5,13 @@ import {
     AddressLineTwoInput,
     CityInput,
     CountryInput,
+    CountryInputNoOptionChosen,
     EmailContactInput,
     GroupNameInput,
     InstitutionNameInput,
     PostalCodeInput,
     StateInput,
+    groupFromFormData,
 } from './Inputs';
 import useClientFlag from '../../hooks/isClient';
 import type { NewGroup } from '../../types/backend';
@@ -32,8 +34,6 @@ export type GroupSubmitError = {
 };
 export type GroupSubmitResult = GroupSubmitSuccess | GroupSubmitError;
 
-const chooseCountry = 'Choose a country...';
-
 export const GroupForm: FC<GroupFormProps> = ({ title, buttonText, defaultGroupData, onSubmit }) => {
     const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
@@ -42,27 +42,12 @@ export const GroupForm: FC<GroupFormProps> = ({ title, buttonText, defaultGroupD
 
         const formData = new FormData(e.currentTarget);
 
-        const groupName = formData.get(fieldMapping.groupName.id) as string;
-        const institution = formData.get(fieldMapping.institution.id) as string;
-        const contactEmail = formData.get(fieldMapping.contactEmail.id) as string;
-        const country = formData.get(fieldMapping.country.id) as string;
-        const line1 = formData.get(fieldMapping.line1.id) as string;
-        const line2 = formData.get(fieldMapping.line2.id) as string;
-        const city = formData.get(fieldMapping.city.id) as string;
-        const state = formData.get(fieldMapping.state.id) as string;
-        const postalCode = formData.get(fieldMapping.postalCode.id) as string;
+        const group = groupFromFormData(formData);
 
-        if (country === chooseCountry) {
+        if (group.address.country === CountryInputNoOptionChosen) {
             setErrorMessage('Please choose a country');
             return false;
         }
-
-        const group: NewGroup = {
-            groupName,
-            institution,
-            contactEmail,
-            address: { line1, line2, city, postalCode, state, country },
-        };
 
         const result = await onSubmit(group);
 
@@ -115,42 +100,3 @@ export const GroupForm: FC<GroupFormProps> = ({ title, buttonText, defaultGroupD
         </div>
     );
 };
-
-const fieldMapping = {
-    groupName: {
-        id: 'group-name',
-        required: true,
-    },
-    institution: {
-        id: 'institution-name',
-        required: true,
-    },
-    contactEmail: {
-        id: 'email',
-        required: true,
-    },
-    country: {
-        id: 'country',
-        required: true,
-    },
-    line1: {
-        id: 'address-line-1',
-        required: true,
-    },
-    line2: {
-        id: 'address-line-2',
-        required: false,
-    },
-    city: {
-        id: 'city',
-        required: true,
-    },
-    state: {
-        id: 'state',
-        required: false,
-    },
-    postalCode: {
-        id: 'postal-code',
-        required: true,
-    },
-} as const;
