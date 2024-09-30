510feb6a4319b49ae6675f03bfbadac31db6a5bb
Felix Hennig <mail@felixhennig.com>
2024-09-30 13:28:05 +0100

    a bit of cleanup

--- website/src/components/User/GroupCreationForm.tsx
+++ website/src/components/User/GroupCreationForm.tsx
@@ -2,10 +2,10 @@ import { type FC } from 'react';
 
 import { useGroupCreation } from '../../hooks/useGroupOperations.ts';
 import { routes } from '../../routes/routes.ts';
-import { type ClientConfig } from '../../types/runtimeConfig.ts';
-import { withQueryProvider } from '../common/withQueryProvider.tsx';
-import { GroupForm, type GroupSubmitError, type GroupSubmitSuccess } from '../Group/GroupForm.tsx';
 import type { NewGroup } from '../../types/backend.ts';
+import { type ClientConfig } from '../../types/runtimeConfig.ts';
+import { GroupForm, type GroupSubmitError, type GroupSubmitSuccess } from '../Group/GroupForm.tsx';
+import { withQueryProvider } from '../common/withQueryProvider.tsx';
 
 interface GroupManagerProps {
     clientConfig: ClientConfig;
@@ -25,22 +25,16 @@ const InnerGroupCreationForm: FC<GroupManagerProps> = ({ clientConfig, accessTok
             return {
                 succeeded: true,
                 nextPageHref: routes.groupOverviewPage(result.group.groupId),
-            } as GroupSubmitSuccess
+            } as GroupSubmitSuccess;
         } else {
             return {
                 succeeded: false,
                 errorMessage: result.errorMessage,
-            } as GroupSubmitError
+            } as GroupSubmitError;
         }
     };
 
-    return (
-        <GroupForm
-            title='Create a new submitting group'
-            buttonText='Create group'
-            onSubmit={handleCreateGroup}
-        />
-    );
+    return <GroupForm title='Create a new submitting group' buttonText='Create group' onSubmit={handleCreateGroup} />;
 };
 
 export const GroupCreationForm = withQueryProvider(InnerGroupCreationForm);
