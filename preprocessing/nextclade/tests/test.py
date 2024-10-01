86b9b07fcd8aa71935780ebbd850684a91550e2b
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-10-01 15:36:00 +0200

    Add expected output

--- preprocessing/nextclade/tests/test.py
+++ preprocessing/nextclade/tests/test.py
@@ -1,13 +1,13 @@
 import unittest
-from unittest import mock
 
 import pandas as pd
-from loculus_preprocessing.prepro import process_all
-from loculus_preprocessing.datatypes import UnprocessedEntry, UnprocessedData
+
 from loculus_preprocessing.config import get_config
+from loculus_preprocessing.datatypes import ProcessedEntry, UnprocessedData, UnprocessedEntry
+from loculus_preprocessing.prepro import process_all
 
 test_metadata_file = "tests/test_metadata.tsv"
-test_config_file = "tests/config.yaml"
+test_config_file = "tests/test_config.yaml"
 
 def read_in_test_metadata(file: str) -> pd.DataFrame:
     df = pd.read_csv(file, sep="\t", dtype=str, keep_default_na=False)
@@ -23,15 +23,27 @@ def read_in_test_metadata(file: str) -> pd.DataFrame:
         unprocessed.append(unprocessed_entry)
     return unprocessed
 
+def map_accession_to_submissionId(test_metadata_file: str):
+    df = pd.read_csv(test_metadata_file, sep="\t", dtype=str, keep_default_na=False)
+    metadata_list: list[dict[str, str]] = df.to_dict(orient="records")
+    map = {}
+    for pos, metadata in enumerate(metadata_list):
+        map[("LOC_" + str(pos) + ".1")] = metadata["submissionId"]
+
+
 class PreprocessingTests(unittest.TestCase):
     def test_process_all(self):
         unprocessed = read_in_test_metadata(test_metadata_file)
-        print(unprocessed)
-        config = get_config()
-        print(config)
+        map = map_accession_to_submissionId(test_metadata_file)
+        config = get_config(test_config_file)
         dataset_dir = "temp"
-        processed = process_all(unprocessed, dataset_dir, config)
-        
+        processed: list[ProcessedEntry] = process_all(unprocessed, dataset_dir, config)
+        for entry in processed:
+            print(entry.accession)
+            print(entry.data.metadata)
+            print(entry.errors)
+            print(entry.warnings)
+            print("\n")
 
 if __name__ == "__main__":
     unittest.main()
