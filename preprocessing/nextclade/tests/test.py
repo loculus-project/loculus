c2bfd9a55d5f753652de7c0636e7c9a637dc89b7
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-10-01 17:27:39 +0200

    format tests

--- preprocessing/nextclade/tests/test.py
+++ preprocessing/nextclade/tests/test.py
@@ -11,7 +11,9 @@ test_metadata_file = "tests/test_metadata.tsv"
 test_config_file = "tests/test_config.yaml"
 expected_output_file = "tests/expected_output.json"
 
-expected_output = json.load(open(expected_output_file, encoding="utf-8"))
+with open(expected_output_file, encoding="utf-8") as file:
+    expected_output = json.load(file)
+
 
 def read_in_test_metadata(file: str) -> pd.DataFrame:
     df = pd.read_csv(file, sep="\t", dtype=str, keep_default_na=False)
@@ -21,13 +23,16 @@ def read_in_test_metadata(file: str) -> pd.DataFrame:
         unprocessed_entry = UnprocessedEntry(
             accessionVersion=("LOC_" + str(pos) + ".1"),
             data=UnprocessedData(
-                submitter="test_submitter", metadata=metadata, unalignedNucleotideSequences={"main": ""}
+                submitter="test_submitter",
+                metadata=metadata,
+                unalignedNucleotideSequences={"main": ""},
             ),
         )
         unprocessed.append(unprocessed_entry)
     return unprocessed
 
-def map_accession_to_submissionId(test_metadata_file: str):
+
+def map_accession_to_submission_id(test_metadata_file: str) -> dict[str, str]:
     df = pd.read_csv(test_metadata_file, sep="\t", dtype=str, keep_default_na=False)
     metadata_list: list[dict[str, str]] = df.to_dict(orient="records")
     map = {}
@@ -37,23 +42,31 @@ def map_accession_to_submissionId(test_metadata_file: str):
 
 
 class PreprocessingTests(unittest.TestCase):
-    def test_process_all(self):
+    def test_process_all() -> None:
         unprocessed = read_in_test_metadata(test_metadata_file)
-        map_accessions_to_submissions = map_accession_to_submissionId(test_metadata_file)
+        map_accessions_to_submissions = map_accession_to_submission_id(test_metadata_file)
         config = get_config(test_config_file)
-        dataset_dir = "temp"
-        processed: list[ProcessedEntry] = process_all(unprocessed, dataset_dir, config)
+        processed: list[ProcessedEntry] = process_all(unprocessed, "temp", config)
         for entry in processed:
             submission_id = map_accessions_to_submissions[entry.accession]
             expected_output_entry = expected_output[submission_id]
-            if {error.message for error in entry.errors} != set(expected_output_entry["errors"]):
-                print(submission_id)
-                print({error.message for error in entry.errors})
-                print(set(expected_output_entry["errors"]))
-            if {error.message for error in entry.warnings} != set(expected_output_entry["warnings"]):
-                print(submission_id)
-                print({warning.message for warning in entry.warnings})
-                print(set(expected_output_entry["warnings"]))
+            error_messages = {error.message for error in entry.errors}
+            expected_error_messages = set(expected_output_entry["errors"])
+            if error_messages != expected_error_messages:
+                message = (
+                    f"Error messages: {error_messages} do not match expected "
+                    f"error messages: {expected_error_messages}."
+                )
+                raise AssertionError(message)
+            warning_messages = {warning.message for warning in entry.warnings}
+            expected_warning_messages = set(expected_output_entry["warnings"])
+            if warning_messages != expected_warning_messages:
+                message = (
+                    f"Error messages: {warning_messages} do not match expected "
+                    f"error messages: {expected_warning_messages}."
+                )
+                raise AssertionError(message)
+
 
 if __name__ == "__main__":
     unittest.main()
