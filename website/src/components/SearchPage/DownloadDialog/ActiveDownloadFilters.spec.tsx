3f5b47aa57408196512614fdda15ff8837d7ca9c
Felix Hennig <mail@felixhennig.com>
2024-10-02 16:23:08 +0100

    Further subdivide tests

--- website/src/components/SearchPage/DownloadDialog/ActiveDownloadFilters.spec.tsx
+++ website/src/components/SearchPage/DownloadDialog/ActiveDownloadFilters.spec.tsx
@@ -1,52 +1,56 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { ActiveDownloadFilters } from "./ActiveDownloadFilters";
 
-describe('Test ActiveDownloadFilters', () => {
-    test('renders empty filters as null', () => {
-        const { container } = render(<ActiveDownloadFilters downloadParameters={{
-            type: 'filter',
-            lapisSearchParameters: {},
-            hiddenFieldValues: {}
-        }} />);
-        expect(container).toBeEmptyDOMElement();
-    })
+describe('ActiveDownloadFilters', () => {
+    describe('with LAPIS filters', () => {
+        it('renders empty filters as null', () => {
+            const { container } = render(<ActiveDownloadFilters downloadParameters={{
+                type: 'filter',
+                lapisSearchParameters: {},
+                hiddenFieldValues: {}
+            }} />);
+            expect(container).toBeEmptyDOMElement();
+        })
 
-    test('renders an empty selection as null', () => {
-        const { container } = render(<ActiveDownloadFilters downloadParameters={{
-            type: 'select',
-            selectedSequences: [],
-        }} />);
-        expect(container).toBeEmptyDOMElement();
-    })
+        it('renders filters correctly', () => {
+            render(<ActiveDownloadFilters downloadParameters={{
+                type: 'filter',
+                lapisSearchParameters: { field1: 'value1', nucleotideMutations: 'A123T,G234C' },
+                hiddenFieldValues: {}
+            }} />);
+            expect(screen.queryByText(/Active filters/)).toBeInTheDocument();
+            expect(screen.queryByText('field1: value1')).toBeInTheDocument();
+            expect(screen.queryByText(/A123T,G234C/)).toBeInTheDocument();
+        });
+    });
 
-    test('renders a single selected sequence correctly', () => {
-        render(<ActiveDownloadFilters downloadParameters={{
-            type: 'select',
-            selectedSequences: ['SEQID1'],
-        }} />);
-        expect(screen.queryByText(/Active filters/)).toBeInTheDocument();
-        expect(screen.getByText('1 sequence selected')).toBeInTheDocument();
-    })
-
-    test('renders a two selected sequences correctly', () => {
-        render(<ActiveDownloadFilters downloadParameters={{
-            type: 'select',
-            selectedSequences: ['SEQID1', 'SEQID2'],
-        }} />);
-        expect(screen.queryByText(/Active filters/)).toBeInTheDocument();
-        expect(screen.getByText('2 sequences selected')).toBeInTheDocument();
-    })
-
-    test('renders filters correctly', () => {
-        render(<ActiveDownloadFilters downloadParameters={{
-            type: 'filter',
-            lapisSearchParameters: { field1: 'value1', nucleotideMutations: 'A123T,G234C' },
-            hiddenFieldValues: {}
-        }} />);
-        expect(screen.queryByText(/Active filters/)).toBeInTheDocument();
-        expect(screen.queryByText('field1: value1')).toBeInTheDocument();
-        expect(screen.queryByText(/A123T,G234C/)).toBeInTheDocument();
+    describe('with selected sequences', () => {
+        it('renders an empty selection as null', () => {
+            const { container } = render(<ActiveDownloadFilters downloadParameters={{
+                type: 'select',
+                selectedSequences: [],
+            }} />);
+            expect(container).toBeEmptyDOMElement();
+        })
+    
+        it('renders a single selected sequence correctly', () => {
+            render(<ActiveDownloadFilters downloadParameters={{
+                type: 'select',
+                selectedSequences: ['SEQID1'],
+            }} />);
+            expect(screen.queryByText(/Active filters/)).toBeInTheDocument();
+            expect(screen.getByText('1 sequence selected')).toBeInTheDocument();
+        })
+    
+        it('renders a two selected sequences correctly', () => {
+            render(<ActiveDownloadFilters downloadParameters={{
+                type: 'select',
+                selectedSequences: ['SEQID1', 'SEQID2'],
+            }} />);
+            expect(screen.queryByText(/Active filters/)).toBeInTheDocument();
+            expect(screen.getByText('2 sequences selected')).toBeInTheDocument();
+        })
     });
 });
