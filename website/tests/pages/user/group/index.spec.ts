e7afbd6612bf592ee2099e9a5877cfee8cc07e92
Felix Hennig <mail@felixhennig.com>
2024-10-14 13:14:03 +0200

    Add playwright group edit test

--- website/tests/pages/user/group/index.spec.ts
+++ website/tests/pages/user/group/index.spec.ts
@@ -1,6 +1,6 @@
 import { v4 } from 'uuid';
 
-import { test, testUser } from '../../../e2e.fixture';
+import { expect, test, testUser } from '../../../e2e.fixture';
 
 test.describe('The group page', () => {
     test('should see all users of the group, add a user and remove it afterwards', async ({
@@ -24,7 +24,49 @@ test.describe('The group page', () => {
         await groupPage.removeUserFromGroup(testUser);
 
         await groupPage.verifyUserIsNotPresent(testUser);
+    });
 
-        // TODO: Edit a group and verify the changes are shown
+    test('should edit group info and see changes afterwards', async ({ groupPage, loginAsTestUser }) => {
+        await loginAsTestUser();
+
+        await groupPage.goToUserPage();
+        await groupPage.goToGroupCreationPage();
+
+        const groupName = v4();
+        await groupPage.createGroup(groupName);
+        const newName = v4();
+        const newInstitution = v4();
+        const newEmail = `${v4()}@example.com`;
+        const newCountry = 2;
+        const newLine1 = v4();
+        const newLine2 = v4();
+        const newCity = v4();
+        const newState = v4();
+        const newPostalCode = v4();
+
+        await groupPage.goToGroupEditPage();
+        await groupPage.editGroupName(newName);
+        await groupPage.editInstitution(newInstitution);
+        await groupPage.editContactEmail(newEmail);
+        await groupPage.editCountry(newCountry);
+        await groupPage.editAddressLine1(newLine1);
+        await groupPage.editAddressLine2(newLine2);
+        await groupPage.editCity(newCity);
+        await groupPage.editState(newState);
+        await groupPage.editPostalCode(newPostalCode);
+        await groupPage.finishEditingGroup();
+
+        await expect(groupPage.page.getByRole('heading', { name: newName })).toBeVisible();
+
+        const tableLocator = groupPage.page.locator('table');
+
+        await expect(tableLocator).toContainText(newInstitution);
+        await expect(tableLocator).toContainText(newEmail);
+        await expect(tableLocator).toContainText(newLine1);
+        await expect(tableLocator).toContainText(newLine2);
+        await expect(tableLocator).toContainText(newCity);
+        await expect(tableLocator).toContainText(newState);
+        await expect(tableLocator).toContainText(newPostalCode);
+        await expect(tableLocator).toContainText('Albania');
     });
 });
