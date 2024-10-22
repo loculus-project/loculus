86db5db0d352d66fd9b0a7a4ef0b1ece4abef58d
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-10-22 10:48:48 +0200

    Test adding a second prepro with a different image (identified by dockerTag)

--- preprocessing/nextclade/src/loculus_preprocessing/__main__.py
+++ preprocessing/nextclade/src/loculus_preprocessing/__main__.py
@@ -11,6 +11,7 @@ def cli_entry() -> None:
 
     logging.getLogger().setLevel(config.log_level)
 
+    logging.info("This is the new config!!!")
     logging.info(f"Using config: {config}")
 
     run(config)
