462cbbbf0d9b5d85717d5b9ce13c9a8ed752d6c7
Felix Hennig <fhennig@users.noreply.github.com>
2024-10-24 22:46:49 +0200

    chore: Bump spring boot to 3.3.4 (#3064)

--- backend/src/test/kotlin/org/loculus/backend/SpringBootTestWithoutDatabase.kt
+++ backend/src/test/kotlin/org/loculus/backend/SpringBootTestWithoutDatabase.kt
@@ -1,5 +1,6 @@
 package org.loculus.backend
 
+import io.mockk.every
 import io.mockk.mockk
 import org.loculus.backend.controller.PublicJwtKeyConfig
 import org.springframework.boot.test.context.SpringBootTest
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
 import org.springframework.test.context.ActiveProfiles
 import javax.sql.DataSource
 
@@ -24,5 +26,11 @@ annotation class SpringBootTestWithoutDatabase
 class TestConfig {
     @Bean
     @Primary
-    fun dataSource(): DataSource = mockk()
+    fun dataSource(): DataSource = mockk<DataSource>()
+        .also {
+            // fix for: https://github.com/spring-projects/spring-boot/issues/42323
+            every {
+                it.isWrapperFor(eq(AbstractRoutingDataSource::class.java))
+            } returns false
+        }
 }
