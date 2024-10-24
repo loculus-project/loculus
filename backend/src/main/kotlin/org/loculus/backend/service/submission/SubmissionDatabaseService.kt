85363f56b9e2da6d82c3f19be57875424ababed9
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-10-25 01:05:20 +0200

    fix(backend): lock rows that are being streamed to pipeline

--- backend/src/main/kotlin/org/loculus/backend/service/submission/SubmissionDatabaseService.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/SubmissionDatabaseService.kt
@@ -32,6 +32,9 @@ import org.jetbrains.exposed.sql.statements.StatementType
 import org.jetbrains.exposed.sql.stringParam
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
+import org.jetbrains.exposed.sql.vendors.ForUpdateOption
+import org.jetbrains.exposed.sql.vendors.ForUpdateOption.PostgreSQL.ForUpdate
+import org.jetbrains.exposed.sql.vendors.ForUpdateOption.PostgreSQL.MODE
 import org.loculus.backend.api.AccessionVersion
 import org.loculus.backend.api.AccessionVersionInterface
 import org.loculus.backend.api.AccessionVersionOriginalMetadata
@@ -157,6 +160,7 @@ open class SubmissionDatabaseService(
             }
             .orderBy(table.accessionColumn)
             .limit(numberOfSequenceEntries)
+            .forUpdate(ForUpdate(mode = MODE.SKIP_LOCKED))
             .fetchSize(streamBatchSize)
             .asSequence()
             .chunked(streamBatchSize)
