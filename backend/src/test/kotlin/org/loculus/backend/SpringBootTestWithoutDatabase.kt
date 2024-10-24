5b88d2cc40e44a7a48add09790edcad62e642f72
Felix Hennig <mail@felixhennig.com>
2024-10-24 15:00:14 +0200

    Add fix

--- backend/src/test/kotlin/org/loculus/backend/SpringBootTestWithoutDatabase.kt
+++ backend/src/test/kotlin/org/loculus/backend/SpringBootTestWithoutDatabase.kt
@@ -1,5 +1,6 @@
 package org.loculus.backend
 
+import io.mockk.every
 import io.mockk.mockk
 import org.loculus.backend.controller.PublicJwtKeyConfig
 import org.springframework.boot.test.context.SpringBootTest
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.AbstractDataSource
 import org.springframework.test.context.ActiveProfiles
 import javax.sql.DataSource
 
@@ -24,5 +26,9 @@ annotation class SpringBootTestWithoutDatabase
 class TestConfig {
     @Bean
     @Primary
-    fun dataSource(): DataSource = mockk()
+    fun dataSource(): DataSource = mockk<DataSource>()
+        .also {
+            // fix for: https://github.com/loculus-project/loculus/pull/3064
+            every { it.isWrapperFor(eq(AbstractDataSource::class.java)) } returns false
+        }
 }
