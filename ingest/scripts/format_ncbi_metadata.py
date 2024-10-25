13a58461d4a090f6c699640d4f16a1e6002c9d2b
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-10-25 17:32:15 +0200

    Format authors cleanup (#3068)
    * Clean up defaults for author-formatting
    
    * make dictionary not list of dictionaries
    
    * fix tests

--- ingest/scripts/format_ncbi_metadata.py
+++ ingest/scripts/format_ncbi_metadata.py
@@ -18,15 +18,11 @@ logging.basicConfig(
 
 
 @dataclass
-class Config:
-    simple_mappings: dict[str, str]
-    location_mappings: dict[str, str]
-    submitter_mappings: dict[str, str]
-    isolate_mappings: dict[str, str]
-    virus_mappings: dict[str, str]
-    host_mappings: dict[str, str]
+class NCBIMappings:
+    string_to_string_mappings: dict[str, str]
+    string_to_list_mappings: dict[str, str]
+    string_to_dict_mappings: dict[str, dict[str, str]]
     unknown_mappings: list[str]
-    parse_list: list[str]
 
 
 def convert_to_title_case(name: str) -> str:
@@ -113,34 +109,24 @@ def reformat_authors_from_genbank_to_loculus(
     return formatted_authors
 
 
-def extract_fields(row, config: Config) -> dict:
+def extract_fields(row, ncbi_mappings: NCBIMappings) -> dict:
     try:
         extracted = {}
-        extracted.update({key: row.get(value) for key, value in config.simple_mappings.items()})
-        location = row.get("location", {})
         extracted.update(
-            {key: location.get(value) for key, value in config.location_mappings.items()}
+            {key: row.get(value) for key, value in ncbi_mappings.string_to_string_mappings.items()}
         )
-        submitter = row.get("submitter", {})
         extracted.update(
-            {key: submitter.get(value) for key, value in config.submitter_mappings.items()}
+            {key: row.get(value) for key, value in ncbi_mappings.string_to_list_mappings.items()}
         )
-        isolate = row.get("isolate", {})
-        extracted.update(
-            {key: isolate.get(value) for key, value in config.isolate_mappings.items()}
-        )
-
-        host_lineage = row.get("host", {})
-        extracted.update(
-            {key: host_lineage.get(value) for key, value in config.host_mappings.items()}
-        )
-
-        virus_lineage = row.get("virus", {})
-        extracted.update(
-            {key: virus_lineage.get(value) for key, value in config.virus_mappings.items()}
-        )
-
-        extracted.update(dict.fromkeys(config.unknown_mappings))
+        for field in ncbi_mappings.string_to_list_mappings:
+            if extracted[field]:
+                extracted[field] = ",".join(extracted[field])
+            else:
+                extracted[field] = ""
+        for field, sub_dict in ncbi_mappings.string_to_dict_mappings.items():
+            dict_as_string = row.get(field, {})
+            extracted.update({key: dict_as_string.get(value) for key, value in sub_dict.items()})
+        extracted.update(dict.fromkeys(ncbi_mappings.unknown_mappings))
 
     except KeyError as e:
         print(f"Missing key: {e}")
@@ -148,22 +134,17 @@ def extract_fields(row, config: Config) -> dict:
     return extracted
 
 
-def jsonl_to_tsv(jsonl_file: str, tsv_file: str, config: Config) -> None:
+def jsonl_to_tsv(jsonl_file: str, tsv_file: str, ncbi_mappings: NCBIMappings) -> None:
     extracted_rows: list[dict[str, str]] = []
     with (
         open(jsonl_file, encoding="utf-8") as infile,
     ):
         for line in infile:
             row = json.loads(line.strip())
-            extracted = extract_fields(row, config)
+            extracted = extract_fields(row, ncbi_mappings)
             extracted["ncbiSubmitterNames"] = reformat_authors_from_genbank_to_loculus(
                 extracted["ncbiSubmitterNames"], extracted["genbankAccession"]
             )
-            for field in config.parse_list:
-                if extracted[field]:
-                    extracted[field] = ",".join(extracted[field])
-                else:
-                    extracted[field] = ""
             extracted_rows.append(extracted)
     df = pd.DataFrame(extracted_rows)
     df.to_csv(
@@ -190,9 +171,11 @@ def main(config_file: str, input: str, output: str, log_level: str) -> None:
 
     with open(config_file, encoding="utf-8") as file:
         full_config = yaml.safe_load(file)
-        relevant_config = {key: full_config[key] for key in Config.__annotations__}
-        config = Config(**relevant_config)
-    jsonl_to_tsv(input, output, config=config)
+        ncbi_mappings_data = full_config["ncbi_mappings"]
+        relevant_config = {key: ncbi_mappings_data[key] for key in NCBIMappings.__annotations__}
+        ncbi_mappings = NCBIMappings(**relevant_config)
+
+    jsonl_to_tsv(input, output, ncbi_mappings=ncbi_mappings)
 
 
 if __name__ == "__main__":
