1973ae6dd274208d7fb73b967b888010f82a11cb
Theo Sanderson <theo@theo.io>
2024-10-29 23:14:59 +0000

    disable webkit for now

--- integration-tests/playwright.config.ts
+++ integration-tests/playwright.config.ts
@@ -43,11 +43,12 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+/*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */
 
     /* Test against mobile viewports. */
     // {
