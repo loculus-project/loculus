06ec62cfb82e614ee956043d891531a0a13e0858
Theo Sanderson <theo@sndrsn.co.uk>
2024-10-31 15:28:13 +0000

    Update ReleasedDataModel.kt

--- backend/src/main/kotlin/org/loculus/backend/model/ReleasedDataModel.kt
+++ backend/src/main/kotlin/org/loculus/backend/model/ReleasedDataModel.kt
@@ -108,12 +108,11 @@ open class ReleasedDataModel(
                 ("versionStatus" to TextNode(versionStatus.name)),
                 ("dataUseTerms" to TextNode(currentDataUseTerms.type.name)),
                 ("dataUseTermsRestrictedUntil" to restrictedDataUseTermsUntil),
-            ) +
-            (
-                rawProcessedData.versionComment?.let {
-                    mapOf("versionComment" to TextNode(it))
-                } ?: emptyMap()
-                ).let {
+            ) + if (!rawProcessedData.processedData.metadata.containsKey("versionComment")) {
+                mapOf("versionComment" to TextNode(rawProcessedData.versionComment))
+            } else {
+                emptyMap()
+            }.let {
                 when (backendConfig.dataUseTermsUrls) {
                     null -> it
                     else -> {
