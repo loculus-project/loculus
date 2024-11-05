206691cc8b701a2eaf5cf1d034d3c3ab2a1f0877
Felix Hennig <mail@felixhennig.com>
2024-11-05 09:42:35 +0100

    formatting

--- website/src/components/SearchPage/DownloadDialog/DownloadDialog.spec.tsx
+++ website/src/components/SearchPage/DownloadDialog/DownloadDialog.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { beforeAll, describe, expect, test, vi } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import { DownloadDialog } from './DownloadDialog.tsx';
 import type { DownloadParameters } from './DownloadParameters.tsx';
