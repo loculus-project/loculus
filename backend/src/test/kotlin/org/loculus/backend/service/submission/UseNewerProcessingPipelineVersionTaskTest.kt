f59b6d0c207c95524bf7c75ca6ae5bd68411747e
Felix Hennig <mail@felixhennig.com>
2024-11-06 14:16:58 +0100

    bump

--- backend/src/test/kotlin/org/loculus/backend/service/submission/UseNewerProcessingPipelineVersionTaskTest.kt
+++ backend/src/test/kotlin/org/loculus/backend/service/submission/UseNewerProcessingPipelineVersionTaskTest.kt
@@ -26,6 +26,7 @@ class UseNewerProcessingPipelineVersionTaskTest(
         val processedData = accessionVersions.map {
             PreparedProcessedData.successfullyProcessed(it.accession, it.version)
         }
+        
         val processedDataWithError = processedData.toMutableList()
         processedDataWithError[1] = PreparedProcessedData.withErrors(processedDataWithError[1].accession)
 
