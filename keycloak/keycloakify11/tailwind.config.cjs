5b51bf77eeeb3d17c3e76823d611dd98a63ccfbd
Felix Hennig <mail@felixhennig.com>
2024-11-21 11:51:31 +0100

    switch to CommonJS syntax for the tailwind config

--- keycloak/keycloakify11/tailwind.config.cjs
+++ keycloak/keycloakify11/tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,5 +8,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
-
+};
