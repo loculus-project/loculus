16cf5f06f8d3f8493971582aa284c225b56b6090
Felix Hennig <mail@felixhennig.com>
2024-11-21 13:32:32 +0100

    Change Sign In text; minor style changes

--- keycloak/keycloakify11/src/login/i18n.ts
+++ keycloak/keycloakify11/src/login/i18n.ts
@@ -2,7 +2,14 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder
+    .withThemeName<ThemeName>()
+    .withCustomTranslations({
+        en: {
+            loginAccountTitle: "Sign In"
+        }
+    })
+    .build();
 
 type I18n = typeof ofTypeI18n;
 
