94d52eda92d7649412e8c1016b01280829a53bf7
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-11-21 22:46:38 +0100

    Fix export back to how it is in starter

--- keycloak/keycloakify/src/kc.gen.tsx
+++ keycloak/keycloakify/src/kc.gen.tsx
@@ -19,7 +19,7 @@ export const kcEnvNames: KcEnvName[] = [];
 
 export const kcEnvDefaults: Record<KcEnvName, string> = {};
 
-type KcContext = import("./login/KcContext").KcContext;
+export type KcContext = import("./login/KcContext").KcContext;
 
 declare global {
     interface Window {
