5f4e5cf6a12af0922e8f0db99c6422ea4e31f968
Felix Hennig <mail@felixhennig.com>
2024-11-21 18:08:08 +0100

    Set theme name

--- keycloak/keycloakify/vite.config.ts
+++ keycloak/keycloakify/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     plugins: [
         react(),
         keycloakify({
-            accountThemeImplementation: "none"
+            accountThemeImplementation: "none",
+            themeName: "loculus"
         })
     ]
 });
