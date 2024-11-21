0a160077d5ecf9e6dd68ce958faca10d94608732
Felix Hennig <mail@felixhennig.com>
2024-11-21 14:29:06 +0100

    Add loculus colors

--- keycloak/keycloakify11/tailwind.config.cjs
+++ keycloak/keycloakify11/tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("./colors.cjs");
+
+const mainTailwindColor = colors.mainTailwindColor;
+
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: { ...mainTailwindColor, DEFAULT: mainTailwindColor[400] },
+                logoSecondary: mainTailwindColor[600],
+                main: mainTailwindColor[600]
+            }
+        }
     },
     plugins: []
 };
