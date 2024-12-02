60bac13985146e0338ac355b2164339f21f939d8
Theo Sanderson <theo@sndrsn.co.uk>
2024-12-02 23:38:28 +0000

    Update DataUploadForm.tsx

--- website/src/components/Submission/DataUploadForm.tsx
+++ website/src/components/Submission/DataUploadForm.tsx
@@ -257,7 +257,7 @@ const UploadComponent = ({
                         <button
                             onClick={() => setMyFile(null)}
                             data-testid={`discard_${name}`}
-                            className='text-xs break-word text-gray-700 py-1.5 px-4 border border-gray-300 rounded-md hover:bg-gray-50'
+                            className='text-xs break-words text-gray-700 py-1.5 px-4 border border-gray-300 rounded-md hover:bg-gray-50'
                         >
                             Discard file
                         </button>
