4baf1b84c0c5dd07fa65e54076ef900c65fbd5c4
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-12-02 09:35:08 +0100

    ci: test storybook build in ci, migrate storybook to 8.4.6

--- keycloak/keycloakify/.storybook/main.ts
+++ keycloak/keycloakify/.storybook/main.ts
@@ -2,11 +2,17 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [],
     framework: {
         name: "@storybook/react-vite",
         options: {}
     },
-    staticDirs: ["../public"]
+
+    staticDirs: ["../public"],
+
+    docs: {},
+
+    typescript: {
+        reactDocgen: "react-docgen-typescript"
+    }
 };
 export default config;
