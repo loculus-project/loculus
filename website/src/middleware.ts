2505c0cabec66d442dda48460c39c6e8a907da87
Theo Sanderson <theo@sndrsn.co.uk>
2024-12-03 17:05:09 +0000

    do not merge: test-disabling-config-middleware

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,15 +1,12 @@
 import { sequence } from 'astro:middleware';
 
-import { getWebsiteConfig } from './config.ts';
 import { authMiddleware } from './middleware/authMiddleware.ts';
 import { catchErrorMiddleware } from './middleware/catchErrorMiddleware.ts';
 import { organismValidatorMiddleware } from './middleware/organismValidatorMiddleware.ts';
 import { submissionPagesDisablingMiddleware } from './middleware/submissionPagesDisablingMiddleware.ts';
 
 const middlewares = [catchErrorMiddleware, organismValidatorMiddleware, authMiddleware];
-if (!getWebsiteConfig().enableSubmissionPages) {
-    middlewares.push(submissionPagesDisablingMiddleware);
-}
+
 
 // Astro middleware
 export const onRequest = sequence(...middlewares);
