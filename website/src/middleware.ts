62b6c4240b9624fdfd17790a8da2abaee270201e
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-12-03 18:08:30 +0100

    fix(website): work around astro bug executing middleware at build time
    See https://github.com/withastro/astro/issues/12527
    
    Discussed here: https://loculus.slack.com/archives/C05G172HL6L/p1733245136034479

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,13 +1,13 @@
 import { sequence } from 'astro:middleware';
 
-import { getWebsiteConfig } from './config.ts';
+import { safeGetWebsiteConfig } from './config.ts';
 import { authMiddleware } from './middleware/authMiddleware.ts';
 import { catchErrorMiddleware } from './middleware/catchErrorMiddleware.ts';
 import { organismValidatorMiddleware } from './middleware/organismValidatorMiddleware.ts';
 import { submissionPagesDisablingMiddleware } from './middleware/submissionPagesDisablingMiddleware.ts';
 
 const middlewares = [catchErrorMiddleware, organismValidatorMiddleware, authMiddleware];
-if (!getWebsiteConfig().enableSubmissionPages) {
+if (!(safeGetWebsiteConfig()?.enableSubmissionPages ?? false)) {
     middlewares.push(submissionPagesDisablingMiddleware);
 }
 
