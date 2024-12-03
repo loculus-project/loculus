d4d48bb3eac3bde9e928ef42809aaa08b0939a93
Felix Hennig <mail@felixhennig.com>
2024-12-03 17:16:12 +0100

    progress

--- website/src/components/SearchPage/EditDataUseTermsModal.tsx
+++ website/src/components/SearchPage/EditDataUseTermsModal.tsx
@@ -1,43 +1,50 @@
 import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 import { BaseDialog } from './BaseDialog';
-import { lapisClientHooks } from '../../services/serviceHooks';
+import { backendClientHooks, lapisClientHooks } from '../../services/serviceHooks';
 import { DATA_USE_TERMS_FIELD, DATA_USE_TERMS_RESTRICTED_UNTIL_FIELD } from '../../settings';
-import type { SequenceFilter } from './DownloadDialog/SequenceFilters';
 import { ActiveDownloadFilters } from './DownloadDialog/ActiveDownloadFilters';
+import type { SequenceFilter } from './DownloadDialog/SequenceFilters';
+import type { ClientConfig } from '../../types/runtimeConfig';
+import { createAuthorizationHeader } from '../../utils/createAuthorizationHeader';
+import { stringifyMaybeAxiosError } from '../../utils/stringifyMaybeAxiosError';
 
 interface EditDataUseTermsModalProps {
     lapisUrl: string;
+    clientConfig: ClientConfig;
+    accessToken: string;
     sequenceFilter: SequenceFilter;
 }
 
 type LoadingState = {
-    type: 'loading'
-}
+    type: 'loading';
+};
 
 type ErrorState = {
-    type: 'error',
-    error: any // TODO
-}
+    type: 'error';
+    error: any; // TODO
+};
 
 type ResultType = 'allOpen' | 'mixed' | 'allRestricted';
 
 type LoadedState = {
-    type: 'loaded',
-    resultType: ResultType,
-    totalCount: number,
-    openCount: number,
-    restrictedCount: number,
-    openAccessions: string[],
-    earliestRestrictedUntil: Date | null
-}
+    type: 'loaded';
+    resultType: ResultType;
+    totalCount: number;
+    openCount: number;
+    restrictedCount: number;
+    openAccessions: string[];
+    earliestRestrictedUntil: Date | null;
+};
 
 function getLoadedState(rows: Record<string, any>[]): LoadedState {
     const openAccessions: string[] = [];
-    var earliestRestrictedUntil: Date | null = null;
+    let earliestRestrictedUntil: Date | null = null;
 
     rows.forEach((row) => {
-        if (row[DATA_USE_TERMS_FIELD] !== 'RESTRICTED') { // TODO maybe don't hardcode this here?
+        if (row[DATA_USE_TERMS_FIELD] !== 'RESTRICTED') {
+            // TODO maybe don't hardcode this here?
             openAccessions.push(row.accession);
         } else {
             const date = new Date(row[DATA_USE_TERMS_RESTRICTED_UNTIL_FIELD]);
@@ -50,7 +57,8 @@ function getLoadedState(rows: Record<string, any>[]): LoadedState {
     const totalCount = rows.length;
     const openCount = openAccessions.length;
     const restrictedCount = totalCount - openCount;
-    var resultType: ResultType = openCount == totalCount ? 'allOpen' : (restrictedCount === totalCount ? 'allRestricted' : 'mixed');
+    const resultType: ResultType =
+        openCount === totalCount ? 'allOpen' : restrictedCount === totalCount ? 'allRestricted' : 'mixed';
 
     return {
         type: 'loaded',
@@ -59,20 +67,23 @@ function getLoadedState(rows: Record<string, any>[]): LoadedState {
         openCount,
         restrictedCount,
         openAccessions,
-        earliestRestrictedUntil
-    }
+        earliestRestrictedUntil,
+    };
 }
 
 type DataState = LoadingState | ErrorState | LoadedState;
 
-
-export const EditDataUseTermsModal: React.FC<EditDataUseTermsModalProps> = ({ lapisUrl, sequenceFilter }) => {
+export const EditDataUseTermsModal: React.FC<EditDataUseTermsModalProps> = ({
+    lapisUrl,
+    clientConfig,
+    accessToken,
+    sequenceFilter,
+}) => {
     const [isOpen, setIsOpen] = useState(false);
     const openDialog = () => setIsOpen(true);
     const closeDialog = () => setIsOpen(false);
 
-    const hooks = lapisClientHooks(lapisUrl).zodiosHooks;
-    const detailsHook = hooks.useDetails({}, {});
+    const detailsHook = lapisClientHooks(lapisUrl).zodiosHooks.useDetails({}, {});
 
     useEffect(() => {
         detailsHook.mutate({
@@ -82,21 +93,22 @@ export const EditDataUseTermsModal: React.FC<EditDataUseTermsModalProps> = ({ la
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [sequenceFilter]);
 
-    const [state, setState] = useState<DataState>({type: 'loading'});
+    const [state, setState] = useState<DataState>({ type: 'loading' });
 
     useEffect(() => {
         if (detailsHook.isLoading) {
             return;
         }
-        if (detailsHook.isError && state.type !== 'error') {
-            setState({type: 'error', error: detailsHook.error});
+        if (detailsHook.error !== null && state.type !== 'error') {
+            setState({ type: 'error', error: detailsHook.error });
             return;
-        } 
-        if (detailsHook.isSuccess) {
+        }
+        if (detailsHook.data) {
             const newState = getLoadedState(detailsHook.data.data);
             setState(newState);
         }
-    }, [detailsHook.data?.data, detailsHook.isError, detailsHook.isLoading]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [detailsHook.data, detailsHook.error, detailsHook.isLoading]);
 
     return (
         <>
@@ -107,52 +119,60 @@ export const EditDataUseTermsModal: React.FC<EditDataUseTermsModalProps> = ({ la
                 <ActiveDownloadFilters downloadParameters={sequenceFilter} />
                 {state.type === 'loading' && 'loading'}
                 {state.type === 'error' && `error: ${state.error}`}
-                {state.type === 'loaded' && (<EditControl state={state} />) }
+                {state.type === 'loaded' && (
+                    <EditControl clientConfig={clientConfig} accessToken={accessToken} state={state} />
+                )}
             </BaseDialog>
         </>
     );
 };
 
 interface EditControlProps {
-    state: LoadedState
+    clientConfig: ClientConfig;
+    accessToken: string;
+    state: LoadedState;
 }
 
-const EditControl: React.FC<EditControlProps> = ({
-    state
-}) => {
+const EditControl: React.FC<EditControlProps> = ({ clientConfig, accessToken, state }) => {
+    // TODO use this
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const setDataUseTermsHook = backendClientHooks(clientConfig).useSetDataUseTerms(
+        { headers: createAuthorizationHeader(accessToken) },
+        {
+            onError: (error) =>
+                toast.error('Failed to edit terms of use: ' + stringifyMaybeAxiosError(error), {
+                    position: 'top-center',
+                    autoClose: false,
+                }),
+            onSuccess: () => location.reload(),
+        },
+    );
+
     switch (state.resultType) {
         case 'allOpen':
-            return (
-                <p>
-                    All selected sequences are already open, nothing to edit.
-                </p>
-            );
+            return <p>All selected sequences are already open, nothing to edit.</p>;
         case 'mixed':
             return (
                 <>
                     <p>
-                        {state.openCount} open and {state.restrictedCount} restricted sequences selected.
-                        You can release all the {state.restrictedCount} restricted sequences as open.
-                        If you want to pick a date for the restricted sequences, please narrow your selection down to 
-                        just restricted sequences. You can use the filters to do so.
+                        {state.openCount} open and {state.restrictedCount} restricted sequences selected. You can
+                        release all the {state.restrictedCount} restricted sequences as open. If you want to pick a date
+                        for the restricted sequences, please narrow your selection down to just restricted sequences.
+                        You can use the filters to do so.
                     </p>
-                    <p className='italic'>
-                        TODO: Add the button to release here
-                    </p>
+                    <p className='italic'>TODO: Add the button to release here</p>
                 </>
             );
         case 'allRestricted':
             return (
                 <>
                     <p>
-                        {state.restrictedCount} restricted sequences selected.
-                        The earliest date is {String(state.earliestRestrictedUntil)}.
-                        You can update all sequences with a date from now until that date.
+                        {state.restrictedCount} restricted sequences selected. The earliest date is{' '}
+                        {String(state.earliestRestrictedUntil)}. You can update all sequences with a date from now until
+                        that date.
                     </p>
-                    <p className='italic'>
-                        TODO add calendar here to select date, or button to release them all.
-                    </p>
+                    <p className='italic'>TODO add calendar here to select date, or button to release them all.</p>
                 </>
             );
     }
-}
+};
