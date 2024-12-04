ade1f3480bd94252ce6a42c4b39f67c884ad0d9a
Felix Hennig <mail@felixhennig.com>
2024-12-04 15:56:24 +0100

    remove ':' from active filters for consistency

--- website/src/components/SearchPage/DownloadDialog/ActiveDownloadFilters.tsx
+++ website/src/components/SearchPage/DownloadDialog/ActiveDownloadFilters.tsx
@@ -11,7 +11,7 @@ export const ActiveDownloadFilters: FC<ActiveDownloadFiltersProps> = ({ download
 
     return (
         <div className='mb-4'>
-            <h4 className='font-bold mb-2'>Active filters:</h4>
+            <h4 className='font-bold mb-2'>Active filters</h4>
             <div className='flex flex-row flex-wrap gap-4'>
                 {[...downloadParameters.toDisplayStrings()].map(([key, desc]) => (
                     <div key={key} className='border-black border rounded-full px-2 py-1 text-sm'>
