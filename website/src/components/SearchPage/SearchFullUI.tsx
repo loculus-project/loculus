307b6dfda53ddb3c33b350e248f375dada9133ed
Theo Sanderson <theo@theo.io>
2024-12-05 19:01:27 +0000

    update

--- website/src/components/SearchPage/SearchFullUI.tsx
+++ website/src/components/SearchPage/SearchFullUI.tsx
@@ -374,10 +374,9 @@ export const InnerSearchFullUI = ({
                         </div>
                     </div>
 
-                    <div className='overflow-x-auto'>
-                        <Table
-                            schema={schema}
-                            data={
+                    <Table
+                        schema={schema}
+                        data={
                             detailsHook.data?.data !== undefined
                                 ? (detailsHook.data.data as TableSequenceData[])
                                 : (oldData ?? initialData)
@@ -396,7 +395,6 @@ export const InnerSearchFullUI = ({
                         setOrderDirection={setOrderDirection}
                         columnsToShow={columnsToShow}
                     />
-                    </div>
 
                     <div className='mt-4 flex justify-center'>
                         {totalSequences !== undefined && (
