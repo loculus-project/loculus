51e170f079467f17b114211ed524128a14a64246
Felix Hennig <mail@felixhennig.com>
2024-12-06 23:19:59 +0100

    Trying out a new ActiveFilters design

--- website/src/components/common/ActiveFilters.tsx
+++ website/src/components/common/ActiveFilters.tsx
@@ -12,9 +12,9 @@ export const ActiveFilters: FC<ActiveFiltersProps> = ({ sequenceFilter: download
     return (
         <div className='mb-4'>
             <h4 className='font-bold mb-2'>Active filters</h4>
-            <div className='flex flex-row flex-wrap gap-4'>
+            <div className='flex flex-row flex-wrap gap-3'>
                 {[...downloadParameters.toDisplayStrings()].map(([key, desc]) => (
-                    <div key={key} className='border-black border rounded-full px-2 py-1 text-sm'>
+                    <div key={key} className='border-primary-600 rounded-sm border border-l-primary-600 bg-gray-100 text-primary-900 border-l-8 px-3 py-1 text-sm font-semibold'>
                         {desc}
                     </div>
                 ))}
