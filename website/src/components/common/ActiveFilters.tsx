32f1128d76d2d156b525c74882ee09cacdc9bf68
Felix Hennig <fhennig@users.noreply.github.com>
2024-12-10 16:53:02 +0100

    style: Trying out a new active-filter style (#3397)
    * Trying out a new ActiveFilters design
    
    * Use display names in tags
    
    * fix tests
    
    * forgot to format
    
    * fixed tests
    
    * Better filter display for individual sequence selection
    
    * add color to select radios
    
    * change layout

--- website/src/components/common/ActiveFilters.tsx
+++ website/src/components/common/ActiveFilters.tsx
@@ -12,10 +12,14 @@ export const ActiveFilters: FC<ActiveFiltersProps> = ({ sequenceFilter: download
     return (
         <div className='mb-4'>
             <h4 className='font-bold mb-2'>Active filters</h4>
-            <div className='flex flex-row flex-wrap gap-4'>
-                {[...downloadParameters.toDisplayStrings()].map(([key, desc]) => (
-                    <div key={key} className='border-black border rounded-full px-2 py-1 text-sm'>
-                        {desc}
+            <div className='flex flex-row flex-wrap gap-3'>
+                {[...downloadParameters.toDisplayStrings()].map(([key, [label, value]]) => (
+                    <div
+                        key={key}
+                        className='border-primary-600 rounded-sm border border-l-primary-600 bg-gray-100 border-l-8 px-3 py-1 text-sm'
+                    >
+                        <span className='text-primary-900 font-light pr-1'>{label}:</span>
+                        <span className='text-primary-900 font-semibold'>{value}</span>
                     </div>
                 ))}
             </div>
