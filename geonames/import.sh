c43395701b7aaef446773913203a2351cb23aba4
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-12-23 23:37:00 +0100

    fix

--- geonames/import.sh
+++ geonames/import.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -I https://download.geonames.org/export/dump/allCountries.zip
+curl -I https://download.geonames.org/export/dump/allCountries.zip > results/allCountries.zip
 unzip -o results/allCountries.zip -d results
 tsv-filter --str-eq 7:A results/allCountries.txt > results/adm.tsv
 tsv-select -f 1-3,5-6,8-13 results/adm.tsv > uploads/input.tsv
