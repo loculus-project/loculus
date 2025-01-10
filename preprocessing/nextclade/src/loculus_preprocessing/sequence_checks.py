06698efcf40d17ccbdb525b5c575571099009db3
Theo Sanderson <theo@sndrsn.co.uk>
2025-01-10 11:56:55 +0000

    Update sequence_checks.py

--- preprocessing/nextclade/src/loculus_preprocessing/sequence_checks.py
+++ preprocessing/nextclade/src/loculus_preprocessing/sequence_checks.py
@@ -47,7 +47,7 @@ def errors_if_non_iupac(
                         ],
                         message=(
                             f"Found non-IUPAC symbols in the {segment} sequence: "
-                            + ", ".join(non_iupac_symbols)
+                            + ", ".join(non_iupac_symbols) + (". Gap characters (-) are not allowed in raw sequences." if "-" in non_iupac_symbols else "")
                         ),
                     )
                 )
