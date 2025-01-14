9e9122e382d9b00d9f9b8b5f34469e5b9343e3b1
Theo Sanderson <theo@sndrsn.co.uk>
2025-01-14 16:29:30 +0000

    add type decl

--- integration-tests/tests/fixtures/auth.fixture.ts
+++ integration-tests/tests/fixtures/auth.fixture.ts
@@ -1,4 +1,5 @@
 import { test as base, Page } from '@playwright/test';
+
 import { v4 as uuidv4 } from 'uuid';
 import { AuthPage } from '../pages/auth.page';
 import { TestAccount } from '../types/auth.types';
@@ -10,7 +11,7 @@ type TestFixtures = {
 
 export const test = base.extend<TestFixtures>({
   testAccount: async ({}, use) => {
-    const testAccount = {
+    const testAccount : TestAccount = {
       username: `test_user_${uuidv4().slice(0, 8)}`,
       password: 'password',
       email: `test_${uuidv4().slice(0, 8)}@test.com`,
