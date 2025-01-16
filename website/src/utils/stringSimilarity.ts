03b0e5eab4e0971ba68615a6bea6fa9c978ed229
Felix Hennig <mail@felixhennig.com>
2025-01-16 09:45:55 +0100

    formatting

--- website/src/utils/stringSimilarity.ts
+++ website/src/utils/stringSimilarity.ts
@@ -23,31 +23,35 @@
  * @param {boolean} [caseSensitive=false] Optional. Whether you want to consider case in string matching. Default false;
  * @returns Number between 0 and 1, with 0 being a low match score.
  */
-export const stringSimilarity = (str1: string, str2: string, substringLength: number = 2, caseSensitive: boolean = false) => {
-	if (!caseSensitive) {
-		str1 = str1.toLowerCase();
-		str2 = str2.toLowerCase();
-	}
+export const stringSimilarity = (
+    str1: string,
+    str2: string,
+    substringLength: number = 2,
+    caseSensitive: boolean = false,
+) => {
+    if (!caseSensitive) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+    }
 
-	if (str1.length < substringLength || str2.length < substringLength)
-		return 0;
+    if (str1.length < substringLength || str2.length < substringLength) return 0;
 
-	const map = new Map();
-	for (let i = 0; i < str1.length - (substringLength - 1); i++) {
-		const substr1 = str1.substr(i, substringLength);
-		map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
-	}
+    const map = new Map<string, number>();
+    for (let i = 0; i < str1.length - (substringLength - 1); i++) {
+        const substr1 = str1.substring(i, substringLength);
+        map.set(substr1, (map.get(substr1) ?? 0) + 1);
+    }
 
-	let match = 0;
-	for (let j = 0; j < str2.length - (substringLength - 1); j++) {
-		const substr2 = str2.substr(j, substringLength);
-		const count = map.has(substr2) ? map.get(substr2) : 0;
-		if (count > 0) {
-			map.set(substr2, count - 1);
-			match++;
-		}
-	}
+    let match = 0;
+    for (let j = 0; j < str2.length - (substringLength - 1); j++) {
+        const substr2 = str2.substring(j, substringLength);
+        const count = map.get(substr2) ?? 0;
+        if (count > 0) {
+            map.set(substr2, count - 1);
+            match++;
+        }
+    }
 
-	return (match * 2) / (str1.length + str2.length - ((substringLength - 1) * 2));
+    return (match * 2) / (str1.length + str2.length - (substringLength - 1) * 2);
 };
 export default stringSimilarity;
