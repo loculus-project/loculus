e8f4f31983cc82e207dd16d548fb6c19ee6af88c
Felix Hennig <mail@felixhennig.com>
2025-01-17 15:29:00 +0100

    Always show tooltip

--- website/src/components/Submission/FileUpload/ColumnMappingModal.tsx
+++ website/src/components/Submission/FileUpload/ColumnMappingModal.tsx
@@ -197,21 +197,19 @@ export const ColumnSelectorRow: FC<ColumnSelectorRowProps> = ({
             <span className={usedOptions.includes(field.name) ? 'text-gray-400' : ''}>
                 {field.displayName ?? field.name}
             </span>
-            {(field.definition ?? field.guidance) && (
-                <Tooltip
-                    id={`${header}-${field.name}-tooltip`}
-                    place='right'
-                    positionStrategy='fixed'
-                    className='z-20 max-w-80 space-y-2'
-                    delayShow={200}
-                >
-                    <p>
-                        <span className='font-mono font-semibold text-gray-300'>{field.name}</span>
-                    </p>
-                    {field.definition && <p>{field.definition}</p>}
-                    {field.guidance && <p>{field.guidance}</p>}
-                </Tooltip>
-            )}
+            <Tooltip
+                id={`${header}-${field.name}-tooltip`}
+                place='right'
+                positionStrategy='fixed'
+                className='z-20 max-w-80 space-y-2'
+                delayShow={200}
+            >
+                <p>
+                    <span className='font-mono font-semibold text-gray-300'>{field.name}</span>
+                </p>
+                {field.definition && <p>{field.definition}</p>}
+                {field.guidance && <p>{field.guidance}</p>}
+            </Tooltip>
         </ListboxOption>
     );
 
