8c65cdaf1f200911f306c49c9d96a4b69db3d484
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-01-21 18:53:34 +0100

    format

--- website/src/components/Submission/DataUploadForm.tsx
+++ website/src/components/Submission/DataUploadForm.tsx
@@ -273,7 +273,8 @@ const InnerDataUploadForm = ({
 
                         <p className='text-gray-400 text-xs mt-3'>
                             Files can optionally be compressed, with the appropriate extension (<i>.zst</i>, <i>.gz</i>,{' '}
-                            <i>.zip</i>), <i>.xz</i> is also supported specifically for FASTA files. For more information please refer to the{' '}
+                            <i>.zip</i>), <i>.xz</i> is also supported specifically for FASTA files. For more
+                            information please refer to the{' '}
                             <a href={dataUploadDocsUrl} className='text-primary-700 opacity-90'>
                                 help pages
                             </a>
