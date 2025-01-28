b93c85b8cddd0bee397c8e658e1742813593484f
GitHub Action <action@github.com>
2025-01-28 14:35:11 +0000

    Update schema documentation based on migration changes

--- backend/docs/db/schema.sql
+++ backend/docs/db/schema.sql
@@ -609,7 +609,7 @@ ALTER TABLE ONLY public.audit_log
 --
 
 ALTER TABLE ONLY public.current_processing_pipeline
-    ADD CONSTRAINT current_processing_pipeline_pkey PRIMARY KEY (organism, version);
+    ADD CONSTRAINT current_processing_pipeline_pkey PRIMARY KEY (organism);
 
 
 --
