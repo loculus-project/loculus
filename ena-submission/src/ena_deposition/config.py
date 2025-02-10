5d1007ab364fe3a7bdb18bd714805ef702c1c2b8
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-02-10 17:29:44 +0100

    Update ena-submission/src/ena_deposition/config.py

--- ena-submission/src/ena_deposition/config.py
+++ ena-submission/src/ena_deposition/config.py
@@ -51,7 +51,7 @@ def secure_ena_connection(config: Config):
         config.test = True
         logging.info("Submitting to ENA dev environment")
         config.ena_submission_url = "https://wwwdev.ebi.ac.uk/ena/submit/drop-box/submit"
-        config.github_url = "https://raw.githubusercontent.com/pathoplexus/ena-submission/cronjob_test/test/approved_ena_submission_list.json"
+        config.github_url = "https://pathoplexus.github.io/ena-submission/test/approved_ena_submission_list.json"
         config.ena_reports_service_url = "https://wwwdev.ebi.ac.uk/ena/submit/report"
 
     if submit_to_ena_prod:
