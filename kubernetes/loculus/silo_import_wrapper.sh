29f2e5061ae316eabf92a7910ecb4233b4a25093
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-02-17 16:39:14 +0100

    feat(deployment): improve observability of silo preprocessing errors by exiting the process if failed

--- kubernetes/loculus/silo_import_wrapper.sh
+++ kubernetes/loculus/silo_import_wrapper.sh
@@ -14,17 +14,16 @@ get_value_from_file() {
     fi
 }
 
-while true
-do
+while true; do
     echo "Checking for new data in SILO"
     last_etag=$(get_value_from_file "$current_etag_path")
     echo "Last download of released data had etag: $last_etag"
 
     last_hard_refresh_time=$(get_value_from_file "$last_hard_refresh_time_path")
     echo "Last hard refresh was at: $last_hard_refresh_time"
-    
+
     # Check if the difference is greater than or equal to 3600 seconds (1 hour)
-    # We only use cache 
+    # We only use cache
     current_time=$(date +%s)
     echo "Current timestamp: $current_time"
     time_diff=$((current_time - last_hard_refresh_time))
@@ -34,13 +33,21 @@ do
         exit_code=$?
         if [ "$exit_code" -ne 0 ]; then
             echo "Error: Hard refresh failed with exit code $exit_code"
+            exit $exit_code
         else
             echo "Hard refresh completed successfully"
-            echo "$current_time" > "$last_hard_refresh_time_path"
+            echo "$current_time" >"$last_hard_refresh_time_path"
         fi
     else
         echo "Last hard refresh was less than 1 hour ago. Passing last etag time to import job: $last_etag"
         bash /silo_import_job.sh --last-etag="$last_etag" --backend-base-url="$BACKEND_BASE_URL"
+        exit_code=$?
+        if [ "$exit_code" -ne 0 ]; then
+            echo "Error: Soft refresh failed with exit code $exit_code"
+            exit $exit_code
+        else
+            echo "Soft refresh completed successfully"
+        fi
     fi
     echo "Sleeping for 30 seconds"
     sleep 30
