c7a3c8ead6d390fe9e6d95548c241c80c30c5b11
Felix Hennig <mail@felixhennig.com>
2025-02-20 15:44:42 +0100

    Improve error messages

--- website/src/components/Edit/EditableSequenceEntry.ts
+++ website/src/components/Edit/EditableSequenceEntry.ts
@@ -9,6 +9,11 @@ export interface ProcessedInsertions {
     aminoAcidInsertions: Record<string, string[]>;
 }
 
+/**
+ * A data class underlying the form input.
+ * The sequence metadata and sequence data in this class can be retrieved as a Record<string, string>
+ * or as Files (TSV, Fasta), depending on which API endpoint the data needs to be submitted to.
+ */
 export class EditableSequenceEntry {
     public readonly editedMetadata: Row[];
     public readonly setEditedMetadata: Dispatch<SetStateAction<Row[]>>;
@@ -18,7 +23,9 @@ export class EditableSequenceEntry {
 
     /**
      * @param initialData If given, pre-populate the form with the information from this object. 'segmentNames' will be ignored.
-     * @param segmentNames If no 'initialData' is given, use the segmentNames to display according sequence edit fields.
+     * @param segmentNames If no 'initialData' is given, use the segmentNames to pre-populate empty sequences with according keys.
+     *     This is useful, because the form will render one input field for existing segment, so if no segments are there,
+     *     there won't be any input fields.
      */
     constructor(initialData?: SequenceEntryToEdit, segmentNames?: string[]) {
         const [_editedMetadata, _setEditedMetadata] = useState(initialData ? mapMetadataToRow(initialData) : []);
@@ -43,7 +50,15 @@ export class EditableSequenceEntry {
               };
     }
 
-    public getMetadataTsv(submissionId: string, accession?: string): File {
+    /**
+     * Return the Metadata information as a TSV. If no information is present, 'undefined' is returned.
+     * @param submissionId The submission ID to put into the TSV.
+     * @param accession optional. If an accession is already assigned to this sequence, it should be given.
+     */
+    public getMetadataTsv(submissionId: string, accession?: string): File | undefined {
+        // if no values are set at all, return undefined
+        if (!this.editedMetadata.some((row) => row.value !== '')) return undefined;
+
         const tableVals = [...this.editedMetadata, { key: SUBMISSION_ID_FIELD, value: submissionId }];
 
         if (accession) {
@@ -52,6 +67,8 @@ export class EditableSequenceEntry {
                 value: accession,
             });
         }
+
+        // TODO - use a library for TSV building, because raw string interpolation doesn't escape stuff.
 
         const header = tableVals.map((row) => row.key).join('\t');
 
@@ -66,12 +83,17 @@ export class EditableSequenceEntry {
         return this.editedMetadata.reduce((prev, row) => ({ ...prev, [row.key]: row.value }), {});
     }
 
-    public getSequenceFasta(submissionId: string) {
+    public getSequenceFasta(submissionId: string): File | undefined {
+        // if no values are set at all, return undefined
+        if (!this.editedSequences.some((row) => row.value !== '')) return undefined;
+
         const sequences = this.editedSequences;
         const fastaContent =
             sequences.length === 1
                 ? `>${submissionId}\n${sequences[0].value}`
                 : sequences.map((sequence) => `>${submissionId}_${sequence.key}\n${sequence.value}`).join('\n');
+
+        // TODO return undefined if sequence is empty.
 
         return new File([fastaContent], 'sequences.fasta', { type: 'text/plain' });
     }
