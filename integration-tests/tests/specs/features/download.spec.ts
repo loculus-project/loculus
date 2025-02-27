f7c0bc8704b8892f056dc34b486e1711c1faddf8
Theo Sanderson <theo@theo.io>
2025-02-27 14:19:56 +0000

    try a POST test

--- integration-tests/tests/specs/features/download.spec.ts
+++ integration-tests/tests/specs/features/download.spec.ts
@@ -1,4 +1,6 @@
+import { approxMaxAcceptableUrlLength } from "../../../../website/src/routes/routes";
 const { test, expect } = require('@playwright/test');
+
 const fs = require('fs');
 
 test('Download metadata and check number of cols', async ({ page }) => {
@@ -8,6 +10,43 @@ test('Download metadata and check number of cols', async ({ page }) => {
 
   await page.goto('/');
   await page.getByRole('link', { name: 'Crimean-Congo Hemorrhagic Fever Virus' }).click();
+  
+  await page.getByRole('button', { name: 'Download all entries' }).click();
+  await page.getByLabel('I agree to the data use terms.').check();
+
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByTestId('start-download').click();
+  const download = await downloadPromise;
+
+  const downloadPath = await download.path();
+  expect(downloadPath).toBeTruthy();
+
+  const fileContent = fs.readFileSync(downloadPath, 'utf8');
+  const lines = fileContent.split('\n');
+  const firstLine = lines[0];
+  const fields = firstLine.split('\t');
+
+  expect(fields).toHaveLength(8);
+  console.log(`Found ${fields.length} fields in the first line of the TSV`);
+});
+
+
+test('Download metadata with POST and check number of cols', async ({ page }) => {
+  console.log('Waiting for 20 seconds before starting the test...');
+  await page.waitForTimeout(20000);
+  console.log('Continuing with test after delay');
+
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Crimean-Congo Hemorrhagic Fever Virus' }).click();
+
+
+  const loculusId = await page.textContent(/LOC_[A-Z0-9]+\.[0-9]+/);
+  console.log(`Found loculus ID: ${loculusId}`);
+
+
+  const query = `${loculusId}\n${'A'.repeat(2000)}`;
+  await page.getByLabelText('Accession').type(query);
+
   
   await page.getByRole('button', { name: 'Download all entries' }).click();
   await page.getByLabel('I agree to the data use terms.').check();
