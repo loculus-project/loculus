e99eddcc62710b3761ceb42fd36336b3fc152adc
Felix Hennig <mail@felixhennig.com>
2025-03-03 14:44:27 +0000

    test fixes

--- backend/src/test/kotlin/org/loculus/backend/controller/submission/SubmitEndpointTest.kt
+++ backend/src/test/kotlin/org/loculus/backend/controller/submission/SubmitEndpointTest.kt
@@ -161,7 +161,7 @@ class SubmitEndpointTest(
     }
 
     @Test
-    fun `GIVEN fasta data with unknown segment THEN data is accepted to let the preprocessing pipeline verify it`() {
+    fun `  GIVEN fasta data with unknown segment THEN data is not accepted`() {
         submissionControllerClient.submit(
             SubmitFiles.metadataFileWith(
                 content = """
@@ -178,9 +178,7 @@ class SubmitEndpointTest(
             organism = OTHER_ORGANISM,
             groupId = groupId,
         )
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
-            .andExpect(jsonPath("\$.length()").value(1))
+            .andExpect(status().isBadRequest)
     }
 
     @ParameterizedTest(name = "GIVEN {0} THEN throws error \"{5}\"")
