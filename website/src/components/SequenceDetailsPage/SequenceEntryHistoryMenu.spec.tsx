e24c31c983623c76a59cdf23a4d4323628ea981b
Felix Hennig <mail@felixhennig.com>
2025-03-05 09:19:58 +0000

    fill in tests

--- website/src/components/SequenceDetailsPage/SequenceEntryHistoryMenu.spec.tsx
+++ website/src/components/SequenceDetailsPage/SequenceEntryHistoryMenu.spec.tsx
@@ -1,16 +1,18 @@
-import { describe } from "vitest";
-import type { SequenceEntryHistory, SequenceEntryHistoryEntry } from "../../types/lapis";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, test } from 'vitest';
 
+import { SequenceEntryHistoryMenu } from './SequenceEntryHistoryMenu';
+import type { SequenceEntryHistory, SequenceEntryHistoryEntry } from '../../types/lapis';
 
 describe('SequenceEntryHistoryMenu', () => {
-
     const baseEntry: SequenceEntryHistoryEntry = {
-        submittedAtTimestamp: "",
-        accession: "",
+        submittedAtTimestamp: '',
+        accession: '',
         version: 0,
-        accessionVersion: "",
-        versionStatus: "REVOKED",
-        isRevocation: false
+        accessionVersion: '',
+        versionStatus: 'REVOKED',
+        isRevocation: false,
     };
 
     const historyRevised: SequenceEntryHistory = [
@@ -18,15 +20,26 @@ describe('SequenceEntryHistoryMenu', () => {
         { ...baseEntry, accessionVersion: 'FOO.2', versionStatus: 'LATEST_VERSION', isRevocation: false },
     ];
 
-    // TODO add some testing
+    test('latest version is labeled correctly', async () => {
+        render(<SequenceEntryHistoryMenu sequenceEntryHistory={historyRevised} accessionVersion='FOO.2' />);
+        const button = screen.getByText('All versions');
+        await userEvent.hover(button);
 
+        expect(screen.getByRole('link', { name: 'FOO.1 Previous version' })).toBeVisible();
+        expect(screen.getByRole('link', { name: 'FOO.2 Latest version' })).toBeVisible();
+    });
 
     const historyRevoke: SequenceEntryHistory = [
         { ...baseEntry, accessionVersion: 'BAR.1', versionStatus: 'REVISED', isRevocation: false },
         { ...baseEntry, accessionVersion: 'BAR.2', versionStatus: 'LATEST_VERSION', isRevocation: true },
     ];
 
-    // TODO add some testing
+    test('revoked version is labeled correctly', async () => {
+        render(<SequenceEntryHistoryMenu sequenceEntryHistory={historyRevoke} accessionVersion='FOO.2' />);
+        const button = screen.getByText('All versions');
+        await userEvent.hover(button);
 
-
+        expect(screen.getByRole('link', { name: 'BAR.1 Previous version' })).toBeVisible();
+        expect(screen.getByRole('link', { name: 'BAR.2 Sequence revoked' })).toBeVisible();
+    });
 });
