4e96ce68bb6dd6382f9775d4e65564a49eaaeb77
Felix Hennig <mail@felixhennig.com>
2025-03-10 15:42:38 +0000

    format

--- integration-tests/tests/pages/singlesubmission.page.ts
+++ integration-tests/tests/pages/singlesubmission.page.ts
@@ -1,75 +1,74 @@
 import { Page } from '@playwright/test';
 
 export class SingleSequenceSubmissionPage {
-  private page: Page;
+    private page: Page;
 
-  constructor(page: Page) {
-    this.page = page;
-  }
+    constructor(page: Page) {
+        this.page = page;
+    }
 
+    async navigateToSubmissionPage() {
+        await this.page.getByRole('link', { name: 'Submit' }).click();
+        await this.page.getByRole('link', { name: 'Ebola Sudan' }).click();
+        await this.page.getByRole('link', { name: 'Submit Upload new sequences.' }).click();
+        await this.page.getByRole('link', { name: 'Submit single sequence' }).click();
+    }
 
-  async navigateToSubmissionPage() {
-    await this.page.getByRole('link', { name: 'Submit' }).click();
-    await this.page.getByRole('link', { name: 'Ebola Sudan' }).click();
-    await this.page.getByRole('link', { name: 'Submit Upload new sequences.' }).click();
-    await this.page.getByRole('link', { name: 'Submit single sequence' }).click();
-  }
+    async fillSubmissionForm({
+        submissionId,
+        collectionCountry,
+        collectionDate,
+        authorAffiliations,
+    }: {
+        submissionId: string;
+        collectionCountry: string;
+        collectionDate: string;
+        authorAffiliations: string;
+    }) {
+        await this.page.getByLabel('Submission ID:').fill(submissionId);
+        await this.page.getByLabel('Collection country:').fill(collectionCountry);
+        await this.page.getByLabel('Collection date:').fill(collectionDate);
+        await this.page.getByLabel('Author affiliations:').fill(authorAffiliations);
+    }
 
-  async fillSubmissionForm({
-    submissionId,
-    collectionCountry,
-    collectionDate,
-    authorAffiliations,
-  }: {
-    submissionId: string;
-    collectionCountry: string;
-    collectionDate: string;
-    authorAffiliations: string;
-  }) {
-    await this.page.getByLabel('Submission ID:').fill(submissionId);
-    await this.page.getByLabel('Collection country:').fill(collectionCountry);
-    await this.page.getByLabel('Collection date:').fill(collectionDate);
-    await this.page.getByLabel('Author affiliations:').fill(authorAffiliations);
-  }
+    async fillSequenceData(sequenceData: Record<string, string>) {
+        Object.entries(sequenceData).forEach(async ([key, value]) => {
+            await this.page.getByLabel(`${key}:`).fill(value);
+        });
+    }
 
-async fillSequenceData(sequenceData: Record<string, string>) {
-  Object.entries(sequenceData).forEach(async ([key, value]) => {
-    await this.page.getByLabel(`${key}:`).fill(value);
-  })
-}
+    async acceptTerms() {
+        await this.page.getByText('I confirm that the data').click();
+        await this.page.getByText('I confirm I have not and will').click();
+    }
 
-async acceptTerms() {
-    await this.page.getByText('I confirm that the data').click();
-    await this.page.getByText('I confirm I have not and will').click();
-  }
+    async submitSequence() {
+        await this.page.getByRole('button', { name: 'Submit sequences' }).click();
+    }
 
- 
-  async submitSequence() {
-    await this.page.getByRole('button', { name: 'Submit sequences' }).click();
-  }
-
-  async completeSubmission({
-    submissionId,
-    collectionCountry,
-    collectionDate,
-    authorAffiliations,
-  }: {
-    submissionId: string;
-    collectionCountry: string;
-    collectionDate: string;
-    authorAffiliations: string;
-  }, 
-  sequenceData: Record<string, string>
-) {
-    await this.navigateToSubmissionPage();
-    await this.fillSubmissionForm({
-      submissionId,
-      collectionCountry,
-      collectionDate,
-      authorAffiliations,
-    });
-    await this.fillSequenceData(sequenceData);
-    await this.acceptTerms();
-    await this.submitSequence();
-  }
+    async completeSubmission(
+        {
+            submissionId,
+            collectionCountry,
+            collectionDate,
+            authorAffiliations,
+        }: {
+            submissionId: string;
+            collectionCountry: string;
+            collectionDate: string;
+            authorAffiliations: string;
+        },
+        sequenceData: Record<string, string>,
+    ) {
+        await this.navigateToSubmissionPage();
+        await this.fillSubmissionForm({
+            submissionId,
+            collectionCountry,
+            collectionDate,
+            authorAffiliations,
+        });
+        await this.fillSequenceData(sequenceData);
+        await this.acceptTerms();
+        await this.submitSequence();
+    }
 }
