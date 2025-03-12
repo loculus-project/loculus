23d7fa43051694a6be951309bc435706bd982855
Theo Sanderson <theo@sndrsn.co.uk>
2025-03-12 16:53:15 +0000

    revert removal of sequence form!

--- website/src/components/Edit/EditPage.tsx
+++ website/src/components/Edit/EditPage.tsx
@@ -108,6 +108,12 @@ const InnerEditPage: FC<EditPageProps> = ({
                     />
                 </tbody>
             </table>
+            {submissionDataTypes.consensusSequences && (
+                <div className='mt-4 space-y-4'>
+                    <SequencesForm editableSequences={editableSequences} setEditableSequences={setEditableSequences} />
+                </div>
+            )}
+           
 
             <div className='flex items-center gap-4 mt-4'>
                 <button
