6da309877aa8cd069f4ff975ddeb4bb7f646c91e
Theo Sanderson <theo@sndrsn.co.uk>
2025-03-14 00:36:37 +0000

    Remove unnecessary type casting in useUrlParamState

--- website/src/hooks/useUrlParamState.ts
+++ website/src/hooks/useUrlParamState.ts
@@ -28,12 +28,12 @@ function useUrlParamState<T>(
     function parseUrlValue(urlValue: string, type: ParamType): T {
         switch (type) {
             case 'boolean':
-                return (urlValue === 'true') as unknown as T;
+                return (urlValue === 'true')  as T;
             case 'nullable-string':
-                return (urlValue || null) as unknown as T;
+                return (urlValue || null) as T;
             case 'string':
             default:
-                return urlValue as unknown as T;
+                return urlValue as T;
         }
     }
 
