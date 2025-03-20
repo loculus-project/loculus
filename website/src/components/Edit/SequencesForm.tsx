ddfd157ed8f09ea88fd43440381cf12a2ca14e1f
Felix Hennig <mail@felixhennig.com>
2025-03-20 17:00:27 +0100

    ...

--- website/src/components/Edit/SequencesForm.tsx
+++ website/src/components/Edit/SequencesForm.tsx
@@ -3,38 +3,59 @@ import { type Dispatch, type FC, type SetStateAction } from 'react';
 import type { KeyValuePair, Row } from './InputField';
 import { mapErrorsAndWarnings, type SequenceEntryToEdit } from '../../types/backend.ts';
 import { FileUploadComponent } from '../Submission/FileUpload/FileUploadComponent.tsx';
-import { PLAIN_SEGMENT_KIND } from '../Submission/FileUpload/fileProcessing.ts';
+import { PLAIN_SEGMENT_KIND, VirtualFile } from '../Submission/FileUpload/fileProcessing.ts';
 
 export class EditableSequences {
     private constructor(public readonly rows: Row[]) {}
 
-    static fromInitialData(initialData: SequenceEntryToEdit): EditableSequences {
-        return new EditableSequences(
-            Object.entries(initialData.originalData.unalignedNucleotideSequences).map(([key, value]) => ({
+    private static emptyRows(names: string[]): Row[] {
+        return names.map((name) => ({
+            key: name,
+            initialValue: '',
+            value: '',
+            errors: [],
+            warnings: [],
+        }));
+    }
+
+    /**
+     * @param initialData The sequence entry to edit, from which the initial sequence data is taken.
+     * @param segmentNames All segment names for the organism of the sequence. This is used to include empty segments.
+     */
+    static fromInitialData(initialData: SequenceEntryToEdit, segmentNames: string[]): EditableSequences {
+        const emptyRows = this.emptyRows(segmentNames);
+        const existingDataRows = Object.entries(initialData.originalData.unalignedNucleotideSequences).map(
+            ([key, value]) => ({
                 key,
                 initialValue: value.toString(),
                 value: value.toString(),
                 ...mapErrorsAndWarnings(initialData, key, 'NucleotideSequence'),
-            })),
+            }),
         );
+        const mergedRows: Row[] = [];
+        // merge in this way to retain the order of segment names as they were given.
+        emptyRows.forEach((row) => {
+            const existingRow = existingDataRows.find((r) => r.key === row.key);
+            if (existingRow) {
+                mergedRows.push(existingRow);
+            } else {
+                mergedRows.push(row);
+            }
+        });
+        return new EditableSequences(mergedRows);
     }
 
+    /**
+     * Create an empty {@link EditableSequences} object from segment names.
+     * Each segment will be empty initially.
+     */
     static fromSequenceNames(segmentNames: string[]): EditableSequences {
-        return new EditableSequences(
-            segmentNames.map((name) => ({
-                key: name,
-                initialValue: '',
-                value: '',
-                errors: [],
-                warnings: [],
-            })),
-        );
+        return new EditableSequences(this.emptyRows(segmentNames));
     }
 
-    static empty(): EditableSequences {
-        return new EditableSequences([]);
-    }
-
+    /**
+     * Create a new {@link EditableSequences} object with the given row value updated.
+     */
     update(editedRow: KeyValuePair & {}): EditableSequences {
         return new EditableSequences(
             this.rows.map((prevRow) =>
@@ -99,6 +120,12 @@ export const SequencesForm: FC<SequenceFormProps> = ({ editableSequences, setEdi
                             ariaLabel={`${field.key} Segment File`}
                             fileKind={PLAIN_SEGMENT_KIND}
                             small={true}
+                            initialValue={
+                                field.initialValue.length > 0
+                                    ? new VirtualFile(field.initialValue, 'Existing data')
+                                    : undefined
+                            }
+                            showUndo={true}
                         />
                     </div>
                 ))}
