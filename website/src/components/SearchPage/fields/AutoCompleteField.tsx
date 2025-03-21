0e3cd9052b6950cdbfa22cea0273de296dd95d87
Felix Hennig <mail@felixhennig.com>
2025-03-21 17:18:54 +0100

    Add integration test

--- website/src/components/SearchPage/fields/AutoCompleteField.tsx
+++ website/src/components/SearchPage/fields/AutoCompleteField.tsx
@@ -116,7 +116,7 @@ export const AutoCompleteField = ({
                             setQuery('');
                             setSomeFieldValues([field.name, '']);
                         }}
-                        aria-label='Clear'
+                        aria-label={`Clear ${field.displayName ?? field.name}`}
                     >
                         <svg className='w-5 h-5 text-gray-400' fill='currentColor' viewBox='0 0 20 20'>
                             <path
