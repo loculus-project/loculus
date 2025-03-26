51971e12c4c94286abe990c60e36fe3013171269
Felix Hennig <fhennig@users.noreply.github.com>
2025-03-26 18:40:54 +0000

    fix: Edit form does not incorrectly drop sequence data anymore. (#3892)
    * Update tests and fix
    
    * fix format
    
    * Error when file doesn't contain segment data

--- website/src/components/Edit/SequencesForm.spec.tsx
+++ website/src/components/Edit/SequencesForm.spec.tsx
@@ -3,19 +3,36 @@ import { describe, expect, test } from 'vitest';
 import { EditableSequences } from './SequencesForm';
 import { defaultReviewData } from '../../../vitest.setup';
 
-describe('InputForm', () => {
+describe('SequencesForm', () => {
     test('Empty editable sequences (with names only) produces `undefined`', () => {
         const emptyEditableSequences = EditableSequences.fromSequenceNames(['foo', 'bar']);
         expect(emptyEditableSequences.getSequenceFasta('subId')).toBeUndefined();
     });
 
-    test('GIVEN only a single segment has data THEN the fasta has only that segment', async () => {
+    test('GIVEN a multi-segmented organism and only a single segment has data THEN the fasta has only that segment', async () => {
         let editableSequences = EditableSequences.fromSequenceNames(['foo', 'bar']);
         editableSequences = editableSequences.update({ key: 'foo', value: 'ATCG' });
         const fasta = editableSequences.getSequenceFasta('subId');
         expect(fasta).not.toBeUndefined();
         const fastaText = await fasta!.text();
-        expect(fastaText).toBe('>subId_foo\nATCG');
+        expect.soft(fastaText).toBe('>subId_foo\nATCG');
+
+        const sequenceRecord = Object.entries(editableSequences.getSequenceRecord());
+        expect(sequenceRecord.length).toBe(1);
+        expect(sequenceRecord[0]).toStrictEqual(['foo', 'ATCG']);
+    });
+
+    test('GIVEN a single-segmented organism with segment data THEN the fasta header does not contain the segment name', async () => {
+        let editableSequences = EditableSequences.fromSequenceNames(['foo']);
+        editableSequences = editableSequences.update({ key: 'foo', value: 'ATCG' });
+        const fasta = editableSequences.getSequenceFasta('subId');
+        expect(fasta).not.toBeUndefined();
+        const fastaText = await fasta!.text();
+        expect.soft(fastaText).toBe('>subId\nATCG');
+
+        const sequenceRecord = Object.entries(editableSequences.getSequenceRecord());
+        expect(sequenceRecord.length).toBe(1);
+        expect(sequenceRecord[0]).toStrictEqual(['foo', 'ATCG']);
     });
 
     test('GIVEN initial data with an empty segment THEN the fasta does not contain the empty segment', async () => {
@@ -27,6 +44,23 @@ describe('InputForm', () => {
         const fasta = editableSequences.getSequenceFasta('subId');
         expect(fasta).not.toBeUndefined();
         const fastaText = await fasta!.text();
-        expect(fastaText).toBe('>subId_originalSequenceName\nATCG');
+        expect.soft(fastaText).toBe('>subId_originalSequenceName\nATCG');
+
+        const sequenceRecord = Object.entries(editableSequences.getSequenceRecord());
+        expect(sequenceRecord.length).toBe(1);
+        expect(sequenceRecord[0]).toStrictEqual(['originalSequenceName', 'ATCG']);
+    });
+
+    test('GIVEN initial segment data that is then deleted as an edit THEN the edit record does not contain the segment key', () => {
+        let editableSequences = EditableSequences.fromInitialData(defaultReviewData, [
+            'originalSequenceName',
+            'anotherSequenceName',
+        ]);
+        editableSequences = editableSequences.update({ key: 'originalSequenceName', value: '' });
+        const fasta = editableSequences.getSequenceFasta('subId');
+        expect(fasta).toBeUndefined();
+
+        const sequenceRecord = Object.entries(editableSequences.getSequenceRecord());
+        expect(sequenceRecord.length).toBe(0);
     });
 });
