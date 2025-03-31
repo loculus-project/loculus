92879adff801a807764a1d98038d7df2f7a265c2
Chaoran Chen <mail@chaoran-chen.de>
2025-03-31 23:16:38 +0200

    feat(backend): Add /files/request-uploads API (#3913)
    Co-authored-by: Felix Hennig <mail@felixhennig.com>

---
+++ backend/src/test/kotlin/org/loculus/backend/controller/files/FilesClient.kt
@@ -0,0 +1,18 @@
+package org.loculus.backend.controller.files
+
+import org.loculus.backend.controller.jwtForDefaultUser
+import org.loculus.backend.controller.withAuth
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+
+class FilesClient(private val mockMvc: MockMvc) {
+
+    fun requestUploads(groupId: Int?, numberFiles: Int?, jwt: String = jwtForDefaultUser): ResultActions {
+        val request = post("/files/request-upload")
+            .withAuth(jwt)
+        groupId?.let { request.param("groupId", it.toString()) }
+        numberFiles?.let { request.param("numberFiles", it.toString()) }
+        return mockMvc.perform(request)
+    }
+}
