9b759ba675d267aa6b987b45b05581784f66053b
Felix Hennig <fhennig@users.noreply.github.com>
2025-04-01 17:02:47 +0100

    config: add definition of input/output files (#3923)
    resolves #3907

--- website/src/types/config.ts
+++ website/src/types/config.ts
@@ -105,9 +105,21 @@ export type GroupedMetadataFilter = {
     header?: string;
 };
 
+export const fileField = z.object({
+    name: z.string(),
+    multipleFiles: z.boolean(),
+});
+
+export const submissionFiles = z.object({
+    enabled: z.boolean(),
+    fields: z.array(fileField).optional(),
+});
+
 export const submissionDataTypesSchema = z.object({
     consensusSequences: z.boolean(),
+    files: submissionFiles.optional(),
 });
+
 export type SubmissionDataTypes = z.infer<typeof submissionDataTypesSchema>;
 
 export const schema = z.object({
