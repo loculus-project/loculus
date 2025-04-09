48569a7f93ff0f48ac7b3f7af6b562b80a414812
Felix Hennig <mail@felixhennig.com>
2025-04-09 19:07:55 +0200

    Add getFileUrl

--- backend/src/main/kotlin/org/loculus/backend/controller/FilesController.kt
+++ backend/src/main/kotlin/org/loculus/backend/controller/FilesController.kt
@@ -3,13 +3,19 @@ package org.loculus.backend.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import jakarta.servlet.http.HttpServletResponse
 import org.loculus.backend.api.FileIdAndUrl
 import org.loculus.backend.auth.AuthenticatedUser
 import org.loculus.backend.auth.HiddenParam
+import org.loculus.backend.service.files.FileId
 import org.loculus.backend.service.files.FilesDatabaseService
 import org.loculus.backend.service.files.S3Service
 import org.loculus.backend.service.groupmanagement.GroupManagementPreconditionValidator
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -24,7 +30,6 @@ class FilesController(
     private val s3Service: S3Service,
     private val groupManagementPreconditionValidator: GroupManagementPreconditionValidator,
 ) {
-
     @Operation(description = "Requests S3 presigned URLs to upload files")
     @PostMapping("/request-upload")
     fun requestUploads(
@@ -43,5 +48,17 @@ class FilesController(
             response.add(FileIdAndUrl(fileId, presignedUploadUrl))
         }
         return response
+    }
+
+    @GetMapping("/{groupId}/{fileId}")
+    fun getFileUrl(
+        @HiddenParam authenticatedUser: AuthenticatedUser,
+        @PathVariable groupId: Int,
+        @PathVariable fileId: FileId,
+        response: HttpServletResponse,
+    ): ResponseEntity<Void> {
+        val presignedUrl = s3Service.createUrlToReadPrivateFile(fileId, groupId)
+        response.sendRedirect(presignedUrl)
+        return ResponseEntity.status(HttpStatus.FOUND).build() // 302 Redirect
     }
 }
