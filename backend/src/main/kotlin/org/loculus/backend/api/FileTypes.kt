0ae5408938f9ff1efd60608fae006f78f6c56352
Felix Hennig <mail@felixhennig.com>
2025-04-15 11:41:05 +0200

    Render JSON

--- backend/src/main/kotlin/org/loculus/backend/api/FileTypes.kt
+++ backend/src/main/kotlin/org/loculus/backend/api/FileTypes.kt
@@ -17,4 +17,5 @@ data class FileIdAndUrl(
 
 data class FileIdAndName(val fileId: FileId, val name: String)
 
+@Seriali
 data class FileIdAndNameAndUrl(val fileId: FileId, val name: String, val url: String)
