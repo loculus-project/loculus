6ca4949f24aef2c365b6e20c94893275e32d4128
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-04-29 09:36:39 +0200

    feat(ingest): clean up

--- ingest/scripts/parse_alignment_output.py
+++ ingest/scripts/parse_alignment_output.py
@@ -9,24 +9,6 @@ import click
 import pandas as pd
 
 
-def parse_files(input_files: list[str], output_file: str):
-    all_dfs = [pd.read_csv(file, sep="\t") for file in input_files]
-    for i in range(len(input_files)):
-        all_dfs[i]["segment"] = input_files[i].split("_")[-1].split(".")[0]
-    df_combined = pd.concat(
-        all_dfs, ignore_index=True
-    )
-
-    df_combined = df_combined.dropna(subset=["alignmentScore"])
-    df_sorted = df_combined.sort_values(by=["seqName", "alignmentScore"], ascending=[True, False])
-
-    # Keep the top (highest score) row per seqName
-    df_top = df_sorted.drop_duplicates(subset="seqName", keep="first")
-
-    df_top = df_top[["seqName", "segment"]]
-    df_top.to_csv(output_file, sep="\t", index=False)
-
-
 @click.command()
 @click.option(
     "--alignment-results",
@@ -36,8 +18,19 @@ def parse_files(input_files: list[str], output_file: str):
 )
 @click.option("--output", required=True, type=click.Path(exists=False))
 def main(alignment_results: list[str], output: str) -> None:
+    all_dfs = [pd.read_csv(file, sep="\t") for file in alignment_results]
+    for i in range(len(alignment_results)):
+        all_dfs[i]["segment"] = alignment_results[i].split("_")[-1].split(".")[0]
+    df_combined = pd.concat(all_dfs, ignore_index=True)
 
-    parse_files(alignment_results, output)
+    df_combined = df_combined.dropna(subset=["alignmentScore"])
+    df_sorted = df_combined.sort_values(by=["seqName", "alignmentScore"], ascending=[True, False])
+
+    # Keep the top (highest score) row per seqName
+    df_top = df_sorted.drop_duplicates(subset="seqName", keep="first")
+
+    df_top = df_top[["seqName", "segment"]]
+    df_top.to_csv(output, sep="\t", index=False)
 
 
 if __name__ == "__main__":
