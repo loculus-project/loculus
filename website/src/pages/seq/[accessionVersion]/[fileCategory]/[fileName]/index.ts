accf75a2dc783fc528b401f9936975a8c2a3e6e5
Felix Hennig <mail@felixhennig.com>
2025-04-30 14:12:47 +0200

    fix some URLs

--- website/src/pages/seq/[accessionVersion]/[fileCategory]/[fileName]/index.ts
+++ website/src/pages/seq/[accessionVersion]/[fileCategory]/[fileName]/index.ts
@@ -1,12 +1,14 @@
 import type { APIRoute } from 'astro';
 
 import { getRuntimeConfig } from '../../../../../config';
+import { parseAccessionVersionFromString } from '../../../../../utils/extractAccessionVersion';
 
 export const GET: APIRoute = ({ params }) => {
     const runtimeConfig = getRuntimeConfig();
     const { accessionVersion, fileCategory, fileName } = params;
+    const { accession, version } = parseAccessionVersionFromString(accessionVersion!);
 
-    const redirectUrl = `${runtimeConfig.public.backendUrl}/files/get/${accessionVersion}/${fileCategory}/${fileName}`;
+    const redirectUrl = `${runtimeConfig.public.backendUrl}/files/get/${accession}/${version}/${fileCategory}/${fileName}`;
     return new Response(null, {
         status: 302,
         headers: {
