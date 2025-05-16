3f37c20d62010fa7052175c636d7437113d41fa2
Chaoran Chen <mail@chaoran-chen.de>
2025-05-16 16:13:12 +0200

    feat(website): make LinkOuts usable without data use terms (#4121)
    If DUTs are disabled when clicking on a LinkOut / Tool on the search
    page, no modal to select the data use terms will be shown. Instead, the
    tool will be opened directly.

--- website/src/components/SearchPage/DownloadDialog/LinkOutMenu.spec.tsx
+++ website/src/components/SearchPage/DownloadDialog/LinkOutMenu.spec.tsx
@@ -18,37 +18,38 @@ const realDownloadUrlGenerator = new DownloadUrlGenerator('test', 'http://testur
 
 const mockSequenceFilter = FieldFilterSet.empty();
 
-describe('LinkOutMenu', () => {
-    const linkOuts = [
-        {
-            name: 'Basic',
-            url: 'http://example.com/tool?data=[unalignedNucleotideSequences]',
-            description: 'Basic tool description',
-        },
-        { name: 'Format', url: 'http://example.com/tool?data=[unalignedNucleotideSequences|json]' },
-        { name: 'Segment', url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S]' },
-        { name: 'SegmentFormat', url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S|json]' },
-        { name: 'Rich', url: 'http://example.com/tool?data=[unalignedNucleotideSequences+rich]' },
-        { name: 'RichFormat', url: 'http://example.com/tool?data=[unalignedNucleotideSequences+rich|json]' },
-        { name: 'SegmentRich', url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S+rich]' },
-        {
-            name: 'Complete',
-            url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S+rich|json]',
-            description: 'Complete tool with all options',
-        },
-        {
-            name: 'Multiple',
-            url: 'http://example.com/tool?data1=[unalignedNucleotideSequences]&data2=[metadata|json]',
-        },
-        { name: 'Invalid', url: 'http://example.com/tool?data=[invalidType]&valid=[metadata]' },
-    ];
+const linkOuts = [
+    {
+        name: 'Basic',
+        url: 'http://example.com/tool?data=[unalignedNucleotideSequences]',
+        description: 'Basic tool description',
+    },
+    { name: 'Format', url: 'http://example.com/tool?data=[unalignedNucleotideSequences|json]' },
+    { name: 'Segment', url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S]' },
+    { name: 'SegmentFormat', url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S|json]' },
+    { name: 'Rich', url: 'http://example.com/tool?data=[unalignedNucleotideSequences+rich]' },
+    { name: 'RichFormat', url: 'http://example.com/tool?data=[unalignedNucleotideSequences+rich|json]' },
+    { name: 'SegmentRich', url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S+rich]' },
+    {
+        name: 'Complete',
+        url: 'http://example.com/tool?data=[unalignedNucleotideSequences:S+rich|json]',
+        description: 'Complete tool with all options',
+    },
+    {
+        name: 'Multiple',
+        url: 'http://example.com/tool?data1=[unalignedNucleotideSequences]&data2=[metadata|json]',
+    },
+    { name: 'Invalid', url: 'http://example.com/tool?data=[invalidType]&valid=[metadata]' },
+];
 
+describe('LinkOutMenu with enabled data use terms', () => {
     test('opens modal when a tool is clicked', () => {
         render(
             <LinkOutMenu
                 downloadUrlGenerator={realDownloadUrlGenerator}
                 sequenceFilter={mockSequenceFilter}
                 linkOuts={linkOuts}
+                dataUseTermsEnabled={true}
             />,
         );
 
@@ -67,6 +68,7 @@ describe('LinkOutMenu', () => {
                 downloadUrlGenerator={realDownloadUrlGenerator}
                 sequenceFilter={mockSequenceFilter}
                 linkOuts={linkOuts}
+                dataUseTermsEnabled={true}
             />,
         );
 
@@ -91,6 +93,7 @@ describe('LinkOutMenu', () => {
                 downloadUrlGenerator={realDownloadUrlGenerator}
                 sequenceFilter={mockSequenceFilter}
                 linkOuts={linkOuts}
+                dataUseTermsEnabled={true}
             />,
         );
 
@@ -115,6 +118,7 @@ describe('LinkOutMenu', () => {
                 downloadUrlGenerator={realDownloadUrlGenerator}
                 sequenceFilter={mockSequenceFilter}
                 linkOuts={[{ name: 'Basic', url: 'http://example.com/tool?data=[unalignedNucleotideSequences]' }]}
+                dataUseTermsEnabled={true}
             />,
         );
 
@@ -124,5 +128,23 @@ describe('LinkOutMenu', () => {
 
         expect(window.open).toHaveBeenCalled();
         expect(vi.mocked(window.open).mock.calls[0][0]).not.toBeUndefined();
+    });
+});
+
+describe('LinkOutMenu with disabled data use terms', () => {
+    test('opens tool when it is clicked', () => {
+        render(
+            <LinkOutMenu
+                downloadUrlGenerator={realDownloadUrlGenerator}
+                sequenceFilter={mockSequenceFilter}
+                linkOuts={linkOuts}
+                dataUseTermsEnabled={false}
+            />,
+        );
+
+        fireEvent.click(screen.getByRole('button', { name: /Tools/ }));
+        fireEvent.click(screen.getByText('Basic'));
+
+        expect(window.open).toHaveBeenCalled();
     });
 });
