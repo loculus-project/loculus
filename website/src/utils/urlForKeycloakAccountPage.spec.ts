cfc2140909b1bb6614049067f2399950cc3aa06c
Theo Sanderson <theo@sndrsn.co.uk>
2025-05-17 20:12:10 +0100

    Handle protocol in Keycloak account URL

---
+++ website/src/utils/urlForKeycloakAccountPage.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'vitest';
+import { urlForKeycloakAccountPage } from './urlForKeycloakAccountPage';
+import type { BaseClient } from 'openid-client';
+
+function createClient(url: string): BaseClient {
+    return {
+        endSessionUrl: () => url,
+    } as unknown as BaseClient;
+}
+
+describe('urlForKeycloakAccountPage', () => {
+    test('uses https issuer', () => {
+        const client = createClient('https://kc.example.com/realms/loculus/protocol/openid-connect/logout');
+        expect(urlForKeycloakAccountPage(client)).toBe('https://kc.example.com/realms/loculus/account');
+    });
+
+    test('uses http issuer', () => {
+        const client = createClient('http://kc.example.com/realms/loculus/protocol/openid-connect/logout');
+        expect(urlForKeycloakAccountPage(client)).toBe('http://kc.example.com/realms/loculus/account');
+    });
+});
