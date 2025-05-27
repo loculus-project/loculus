816d95de8648521e6b217d10345d4f056eedfcf2
Theo Sanderson <theo@sndrsn.co.uk>
2025-05-27 13:23:13 +0100

    Update backend/src/main/resources/db/migration/V1.22__organism_index.sql
    Co-authored-by: Chaoran Chen <mail@chaoran-chen.de>

--- backend/src/main/resources/db/migration/V1.22__organism_index.sql
+++ backend/src/main/resources/db/migration/V1.22__organism_index.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS sequence_entries_organism_idx ON public.sequence_entries USING btree (organism);
+CREATE INDEX IF NOT EXISTS sequence_entries_organism_idx ON sequence_entries (organism);
