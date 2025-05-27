2d875a6a8b26bb5847af1c5b33afbe90831337dc
Theo Sanderson <theo@sndrsn.co.uk>
2025-05-27 17:30:44 +0100

    refactor(backend): disable S3 client in a different way (#4184)
    This was Codex's suggestion for how to allow disabling S3 without
    creating a Noop interface. It's no big deal either way - but it does
    seem decent to my untrained eye. I was also just interested to test
    Codex on the backend (it's a challenge because it doesn't have docker
    for the testContainers, but can still be useful).
    
    - drop `S3ServiceNoop`
    - update `S3ServiceImpl` to always exist and lazily create clients
    - adjust tests for the new behaviour
    
    @chaoran-chen  etc., np if you think this isn't a good approach
    (integration test failure is file-sharing tests)
    
    🚀 Preview: https://codex-refactor-s3-client.loculus.org
    
    ---------
    
    Co-authored-by: Chaoran Chen <mail@chaoran-chen.de>

--- backend/src/main/kotlin/org/loculus/backend/service/files/S3Service.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/files/S3Service.kt
@@ -1,8 +1,107 @@
 package org.loculus.backend.service.files
 
-interface S3Service {
-    fun createUrlToUploadPrivateFile(fileId: FileId): String
-    fun createUrlToReadPrivateFile(fileId: FileId, downloadFileName: String? = null): String
-    fun getPublicUrl(fileId: FileId): String
-    fun setFileToPublic(fileId: FileId)
+import io.minio.GetPresignedObjectUrlArgs
+import io.minio.MinioClient
+import io.minio.SetObjectTagsArgs
+import io.minio.http.Method
+import org.loculus.backend.config.S3BucketConfig
+import org.loculus.backend.config.S3Config
+import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
+
+private const val PRESIGNED_URL_EXPIRY_SECONDS = 60 * 30
+
+@Service
+class S3Service(private val s3Config: S3Config) {
+
+    private val minioClient: MinioClient by lazy { createClient(getS3BucketConfig()) }
+
+    private val internalMinioClient: MinioClient by lazy { createClient(getS3BucketConfig(), true) }
+
+    fun createUrlToUploadPrivateFile(fileId: FileId): String {
+        val config = getS3BucketConfig()
+        return getClient().getPresignedObjectUrl(
+            GetPresignedObjectUrlArgs.builder()
+                .method(Method.PUT)
+                .bucket(config.bucket)
+                .`object`(getFileIdPath(fileId))
+                .expiry(PRESIGNED_URL_EXPIRY_SECONDS, TimeUnit.SECONDS)
+                .build(),
+        )
+    }
+
+    fun createUrlToReadPrivateFile(fileId: FileId, downloadFileName: String? = null): String {
+        val config = getS3BucketConfig()
+        var args = GetPresignedObjectUrlArgs.builder()
+            .method(Method.GET)
+            .bucket(config.bucket)
+            .`object`(getFileIdPath(fileId))
+            .expiry(PRESIGNED_URL_EXPIRY_SECONDS, TimeUnit.SECONDS)
+        if (downloadFileName != null) {
+            args =
+                args.extraQueryParams(
+                    mapOf(
+                        "response-content-disposition" to "attachment; filename=\"$downloadFileName\"",
+                    ),
+                )
+        }
+        return minioClient.getPresignedObjectUrl(args.build())
+    }
+
+    /**
+     * Returns the URL of the file, which can be used for published files.
+     * (Use [createUrlToReadPrivateFile] to generate presigned URLs for not-yet-published files).
+     */
+    fun getPublicUrl(fileId: FileId): String {
+        val config = getS3BucketConfig()
+        return "${config.endpoint}/${config.bucket}/${getFileIdPath(fileId)}"
+    }
+
+    /**
+     * Sets the 'public=true' tag on the given file ID.
+     * The bucket should have a policy that files with this tag are publicly accessible.
+     */
+    fun setFileToPublic(fileId: FileId) {
+        val config = getS3BucketConfig()
+        getInternalClient().setObjectTags(
+            SetObjectTagsArgs.builder()
+                .bucket(config.bucket)
+                .`object`(getFileIdPath(fileId))
+                .tags(mapOf("public" to "true"))
+                .build(),
+        )
+    }
+
+    private fun assertIsEnabled() {
+        if (!s3Config.enabled) {
+            throw IllegalStateException("S3 is not enabled")
+        }
+    }
+
+    private fun getS3BucketConfig(): S3BucketConfig {
+        assertIsEnabled()
+        if (s3Config.bucket == null) {
+            throw RuntimeException("S3 buckets configurations are missing")
+        }
+        return s3Config.bucket
+    }
+
+    /**
+     * Use the client to generate URLs that are accessible from outside the cluster.
+     */
+    private fun getClient(): MinioClient = minioClient
+
+    /**
+     * Use the internal client to make direct requests to S3.
+     */
+    private fun getInternalClient(): MinioClient = internalMinioClient
+
+    private fun createClient(bucketConfig: S3BucketConfig, internal: Boolean = false): MinioClient =
+        MinioClient.builder()
+            .endpoint(if (internal) bucketConfig.internalEndpoint ?: bucketConfig.endpoint else bucketConfig.endpoint)
+            .region(bucketConfig.region)
+            .credentials(bucketConfig.accessKey, bucketConfig.secretKey)
+            .build()
+
+    private fun getFileIdPath(fileId: FileId): String = "files/$fileId"
 }
--- backend/src/test/kotlin/org/loculus/backend/service/files/EnabledS3ServiceTest.kt
+++ backend/src/test/kotlin/org/loculus/backend/service/files/EnabledS3ServiceTest.kt
@@ -1,9 +1,11 @@
 package org.loculus.backend.service.files
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.loculus.backend.config.BackendSpringProperty
 import org.loculus.backend.controller.EndpointTest
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.UUID
 
 @EndpointTest(
     properties = ["${BackendSpringProperty.S3_ENABLED}=true"],
@@ -11,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired
 class EnabledS3ServiceTest(@Autowired val s3Service: S3Service) {
 
     @Test
-    fun `WHEN S3 is enabled THEN get an instance of S3ServiceImpl`() {
-        assert(s3Service is S3ServiceImpl)
+    fun `WHEN calling createUrlToUploadPrivateFile THEN no error is thrown`() {
+        assertDoesNotThrow {
+            s3Service.createUrlToUploadPrivateFile(UUID.randomUUID())
+        }
     }
 }
