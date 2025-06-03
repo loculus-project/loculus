04a85cac0d35e60af3f0711ee0ae7d0f54cb834a
Theo Sanderson <theo@sndrsn.co.uk>
2025-06-03 23:16:27 +0100

    Update playwright.config.ts

--- integration-tests/playwright.config.ts
+++ integration-tests/playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: process.env.BASE_URL || 'http://localhost:3000',
+        ignoreHTTPSErrors: true,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: process.env.CI ? 'on-first-retry' : 'on',
