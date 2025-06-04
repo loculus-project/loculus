48f98d7eae37c86b08c8fc6baf706cf1d56e19f0
Theo Sanderson <theo@sndrsn.co.uk>
2025-06-04 15:32:37 +0100

    Update DataUseTermsPreconditionValidator.kt

--- backend/src/main/kotlin/org/loculus/backend/service/datauseterms/DataUseTermsPreconditionValidator.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/datauseterms/DataUseTermsPreconditionValidator.kt
@@ -36,7 +36,7 @@ class DataUseTermsPreconditionValidator(private val dateProvider: DateProvider) 
                     DataUseTermsType.fromString(it[DataUseTermsTable.dataUseTermsTypeColumn]) == DataUseTermsType.OPEN
                 }
             ) {
-                throw UnprocessableEntityException("Data use terms are already OPEN.")
+                throw UnprocessableEntityException("The data use terms have already been set to 'Open' - this should appear in the next several minutes.")
             }
         }
 
