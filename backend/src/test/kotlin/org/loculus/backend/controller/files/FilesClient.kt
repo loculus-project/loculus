cebaa0f95edaa27b7ff70f854e746651bf6f4620
Felix Hennig <fhennig@users.noreply.github.com>
2025-06-04 09:47:55 +0100

    fix(backend): authenticated file access to unpublished files (#4301)
    I found an issue, the authenticated access to unpublished files didn't
    work. I wrote a test for that: `GIVEN an unpublished file WHEN
    requesting with auth THEN the file is returned`, and implemented the
    fix.
    
    This came up while working on
    https://github.com/loculus-project/loculus/issues/3968 and is relevant
    for implementing that.
    
    🚀 Preview: Add `preview` label to enable

--- backend/src/test/kotlin/org/loculus/backend/controller/files/FilesClient.kt
+++ backend/src/test/kotlin/org/loculus/backend/controller/files/FilesClient.kt
@@ -1,12 +1,16 @@
 package org.loculus.backend.controller.files
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.loculus.backend.api.FileCategory
 import org.loculus.backend.api.FileIdAndWriteUrl
 import org.loculus.backend.controller.jwtForDefaultUser
 import org.loculus.backend.controller.withAuth
 import org.loculus.backend.service.files.FileId
+import org.loculus.backend.utils.Accession
+import org.loculus.backend.utils.Version
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import java.util.*
 
@@ -17,6 +21,24 @@ class FilesClient(private val mockMvc: MockMvc) {
             .withAuth(jwt)
         groupId?.let { request.param("groupId", it.toString()) }
         numberFiles?.let { request.param("numberFiles", it.toString()) }
+        return mockMvc.perform(request)
+    }
+
+    fun getFile(
+        accession: Accession,
+        version: Version,
+        fileCategory: FileCategory,
+        fileName: String,
+        jwt: String? = null,
+    ): ResultActions {
+        val request = get(
+            "/files/get/{accession}/{version}/{category}/{filename}",
+            accession,
+            version,
+            fileCategory,
+            fileName,
+        )
+            .withAuth(jwt)
         return mockMvc.perform(request)
     }
 }
