829b0e1d3e442b34931a87a25d08bb8b867f5ae1
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-06-05 15:56:23 +0200

    feat: ensure int comparison

--- ena-submission/scripts/get_ena_submission_list.py
+++ ena-submission/scripts/get_ena_submission_list.py
@@ -68,13 +68,14 @@ def filter_for_submission(
         if other_versions_list and int(other_versions_list[-1]) >= int(version):
             # If the latest version in the db is greater or equal than the current version, ignore
             continue
-        if accession in data_dict and data_dict[accession]["metadata"]["version"] >= version:
+        if accession in data_dict and int(data_dict[accession]["metadata"]["version"]) >= int(
+            version
+        ):
             # If the accession is already in the dict and the version is greater or equal, ignore
             continue
-        if (
-            accession in data_dict_with_external_metadata
-            and data_dict_with_external_metadata[accession]["metadata"]["version"] >= version
-        ):
+        if accession in data_dict_with_external_metadata and int(
+            data_dict_with_external_metadata[accession]["metadata"]["version"]
+        ) >= int(version):
             continue
         entry["organism"] = organism
         if any(entry["metadata"].get(field, False) for field in ena_specific_metadata):
@@ -86,7 +87,9 @@ def filter_for_submission(
             continue
         data_dict[accession] = entry
     return SubmissionResults(
-        entries_to_submit={f"{key}.{data['metadata']['version']}": data for key, data in data_dict.items()},
+        entries_to_submit={
+            f"{key}.{data['metadata']['version']}": data for key, data in data_dict.items()
+        },
         entries_with_ext_metadata_to_submit={
             f"{key}.{data['version']}": data
             for key, data in data_dict_with_external_metadata.items()
