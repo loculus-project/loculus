ef01ecf8fcbefc7b847dcef7f3615877c0b5f166
Felix Hennig <mail@felixhennig.com>
2025-06-05 14:59:38 +0200

    await

--- integration-tests/tests/specs/features/file-sharing.spec.ts
+++ integration-tests/tests/specs/features/file-sharing.spec.ts
@@ -33,9 +33,9 @@ test('submit a single sequence with two files', async ({ pageWithGroup, page }) 
 
     // check that files can be seen after processing
     const filesDialog = await reviewPage.viewFiles();
-    expect(filesDialog.getByText('hello.txt')).toBeVisible();
+    await expect(filesDialog.getByText('hello.txt')).toBeVisible();
     await checkFileContent(page, 'hello.txt', 'Hello');
-    expect(filesDialog.getByText('world.txt')).toBeVisible();
+    await expect(filesDialog.getByText('world.txt')).toBeVisible();
     await checkFileContent(page, 'world.txt', 'World');
     await reviewPage.closeFilesDialog();
 
