7f7adc09e6dbfaf133a963223b1e6811e1d4370a
Felix Hennig <mail@felixhennig.com>
2025-06-19 16:51:05 +0200

    implement it

--- website/src/components/Submission/FileUpload/ColumnMappingModal.tsx
+++ website/src/components/Submission/FileUpload/ColumnMappingModal.tsx
@@ -9,6 +9,7 @@ import { ColumnMapping } from './ColumnMapping';
 import { type ProcessedFile } from './fileProcessing';
 import type { InputField } from '../../../types/config';
 import { BaseDialog } from '../../common/BaseDialog';
+import { InputFieldTooltip } from '../InputFieldTooltip';
 
 interface ColumnMappingModalProps {
     inputFile: ProcessedFile;
@@ -30,7 +31,7 @@ export const ColumnMappingModal: FC<ColumnMappingModalProps> = ({
         setIsOpen(false);
         // This is used to not have the Tooltip on the open-button pop up again.
         setTimeout(() => {
-            if (document.activeElement === null) return;
+            if (typeof document !== 'undefined' && document.activeElement === null) return;
             (document.activeElement as HTMLElement).blur();
         }, 10);
     };
@@ -209,19 +210,7 @@ export const ColumnSelectorRow: FC<ColumnSelectorRowProps> = ({
             <span className={usedOptions.includes(field.name) ? 'text-gray-400' : ''}>
                 {field.displayName ?? field.name}
             </span>
-            <Tooltip
-                id={`${header}-${field.name}-tooltip`}
-                place='right'
-                positionStrategy='fixed'
-                className='z-20 max-w-80 space-y-2'
-                delayShow={200}
-            >
-                <p>
-                    <span className='font-mono font-semibold text-gray-300'>{field.name}</span>
-                </p>
-                {field.definition && <p>{field.definition}</p>}
-                {field.guidance && <p>{field.guidance}</p>}
-            </Tooltip>
+            <InputFieldTooltip id={`${header}-${field.name}-tooltip`} field={field} />
         </ListboxOption>
     );
 
