85dcd94c217afcafc3fb60d36653e4215d35c53f
Felix Hennig <mail@felixhennig.com>
2025-06-19 18:15:58 +0200

    format; v-offset icon

--- website/src/components/Edit/EditPage.spec.tsx
+++ website/src/components/Edit/EditPage.spec.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { sentenceCase } from 'change-case';
 import { beforeEach, describe, expect, test } from 'vitest';
 
 import { EditPage } from './EditPage.tsx';
