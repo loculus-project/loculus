12313fe7c070ef944d521d5ef026fe4b49532c62
Theo Sanderson <theo@sndrsn.co.uk>
2025-06-30 15:00:49 +0100

    chore(website): Remove remnant TODO (#4544)
    🚀 Preview: Add `preview` label to enable

--- website/src/components/SearchPage/DownloadDialog/DownloadDialog.tsx
+++ website/src/components/SearchPage/DownloadDialog/DownloadDialog.tsx
@@ -73,7 +73,7 @@ export const DownloadDialog: FC<DownloadDialogProps> = ({
                                     onChange={() => setAgreedToDataUseTerms(!agreedToDataUseTerms)}
                                 />
                                 <span className='text-sm'>
-                                    I agree to the {/* TODO(862) */}
+                                    I agree to the{' '}
                                     <a
                                         href={routes.datauseTermsPage()}
                                         className='underline'
