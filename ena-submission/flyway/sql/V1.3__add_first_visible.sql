d9a81cb2fcbbe44d9561f0467fb7a52472c20629
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-07-07 20:55:49 +0200

    Can now check all types in ENA

--- ena-submission/flyway/sql/V1.3__add_first_visible.sql
+++ ena-submission/flyway/sql/V1.3__add_first_visible.sql
@@ -1,1 +1,6 @@
 ALTER TABLE project_table ADD ena_first_publicly_visible timestamp with time zone;
+
+ALTER TABLE sample_table ADD ena_first_publicly_visible timestamp with time zone;
+
+ALTER TABLE assembly_table ADD ena_nucleotide_first_publicly_visible timestamp with time zone;
+ALTER TABLE assembly_table ADD ena_gca_first_publicly_visible timestamp with time zone;
--- ena-submission/src/ena_deposition/submission_db_helper.py
+++ ena-submission/src/ena_deposition/submission_db_helper.py
@@ -151,7 +151,7 @@ class ProjectTableEntry:
     ncbi_first_publicly_visible: datetime | None = None
 
 
-@dataclass
+@dataclass(kw_only=True)
 class SampleTableEntry:
     accession: str
     version: int
@@ -161,9 +161,11 @@ class SampleTableEntry:
     started_at: datetime | None = None
     finished_at: datetime | None = None
     result: dict[str, str] | str | None = None
+    ena_first_publicly_visible: datetime | None = None
+    ncbi_first_publicly_visible: datetime | None = None
 
 
-@dataclass
+@dataclass(kw_only=True)
 class AssemblyTableEntry:
     accession: str
     version: int
@@ -173,6 +175,10 @@ class AssemblyTableEntry:
     started_at: datetime | None = None
     finished_at: datetime | None = None
     result: str | None = None
+    ena_nucleotide_first_publicly_visible: datetime | None = None
+    ncbi_nucleotide_first_publicly_visible: datetime | None = None
+    ena_gca_first_publicly_visible: datetime | None = None
+    ncbi_gca_first_publicly_visible: datetime | None = None
 
 
 type Accession = str
