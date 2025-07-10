loculus-project/loculus

commit b634cdb67ae4167a48e8f8b2f663738e827cc955

fix(deposition): fix tests, types, switch to pytest, use webin from bioconda, add ruff to ci, improve logs & docs (#4577)
### Related/fixed issues
- resolve #4526 (Clarification: only retry get requests e.g. for status,
add option to retry post requests but only try once)
- probably fix #4417
- progress towards #4377
- fix #4111
- resolve #4419

### Summary of changes
#### CI / Testing
- Make ENA HTTP timeout configurable; raise default from 10s to 60s
(this was major test flake reason)
- Add automatic retries for ENA HTTP POST/GET requests (also help with
flakes)
- Run unittests after Docker image build as a smoke test
- Run API tests in CI as well (previously not done)
- Switch from `unittest` to `pytest` for running tests

#### Code Style / Formatting
- Add Ruff check/format in CI
- Fix all Ruff issues (excluding a few hard-to-fix/noisy rules)
- Add `format_me` action for the ena-deposition repo code

#### Dependency & Environment Management
- Get `webin-cli` via Bioconda instead of manual download
- Add type-stubs packages to the environment

#### Type Safety & Data Validation
- Use Pydantic to parse backend group-info responses  
- Add type hints to more functions and fix many (not all) type errors
- make webin log file parsing more robust, use dedicated output temp dir

#### Other
- Support reading ENA credentials from a `.env` file (in addition to env
vars)
- Improve specificity and context in log messages  
- General refactors, e.g. remove the `ENAConfig` class  
- Add **FAQ.md** capturing notes and lessons learned while reviewing the
code
- Add pre-commit running ruff to make DX smoother

### Testing
Existing integration/unit tests already cover a lot. Also we're type
checking more now. We also run unit tests in docker image now.

Some things that aren't automatically tested:
- Actual interaction with backend: we mock it right now (would be nice
to use ena in full integration tests at some point)
- Notifications

ena-submission/scripts/get_ena_submission_list.py
@@ -51,8 +51,9 @@ def filter_for_submission(
     To prevent this we need to make sure:
         - data was not submitted by the config.ingest_pipeline_submission_group
         - data is not in submission_table
-        - as an extra check we send a notification if there are sequences with ena-specific-metadata fields
-        (users can add these fields, nothing prohibits them from doing so)
+        - as an extra check we send a notification if there are sequences with
+          ena-specific-metadata fields (users can add these fields, nothing prohibits them
+          from doing so)
     """
     entries_to_submit: dict[Accession, dict[str, Any]] = {}
     entries_with_external_metadata: set[Accession] = set()

ena-submission/src/ena_deposition/notifications.py
@@ -19,32 +19,15 @@ class SlackConfig:
     last_notification_sent: datetime | None
 
 
-
 def slack_conn_init(
     slack_hook_default: str, slack_token_default: str, slack_channel_id_default: str
 ) -> SlackConfig:
-    slack_hook = os.getenv("SLACK_HOOK")
-    if not slack_hook:
-        slack_hook = slack_hook_default
-
-    slack_token = os.getenv("SLACK_TOKEN")
-    if not slack_token:
-        slack_token = slack_token_default
-
-    slack_channel_id = os.getenv("SLACK_CHANNEL_ID")
-    if not slack_channel_id:
-        slack_channel_id = slack_channel_id_default
-
-    last_notification_sent = None
-
-    params = {
-        "slack_hook": slack_hook,
-        "slack_token": slack_token,
-        "slack_channel_id": slack_channel_id,
-        "last_notification_sent": last_notification_sent,
-    }
-
-    return SlackConfig(**params)
+    return SlackConfig(
+        slack_hook=os.getenv("SLACK_HOOK", slack_hook_default),
+        slack_token=os.getenv("SLACK_TOKEN", slack_token_default),
+        slack_channel_id=os.getenv("SLACK_CHANNEL_ID", slack_channel_id_default),
+        last_notification_sent=None,
+    )
 
 
 def notify(config: SlackConfig, text: str):
@@ -58,13 +41,11 @@ def upload_file_with_comment(
 ) -> web.SlackResponse:
     """Upload file with comment to slack channel"""
     client = WebClient(token=config.slack_token)
-    output_file_zip = file_path.split(".")[0] + ".zip"
-    zip = zipfile.ZipFile(output_file_zip, "w", zipfile.ZIP_DEFLATED)
-    zip.write(file_path)
-    zip.close()
+    with zipfile.ZipFile(file_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(file_path, arcname=os.path.basename(file_path))
     return client.files_upload_v2(
-        file=output_file_zip,
-        title=file_path.split("/")[-1],
+        file=file_path,
+        title=file_path,
         channel=config.slack_channel_id,
         initial_comment=comment,
     )

.github/workflows/ena-submission-image.yaml
@@ -98,3 +98,9 @@ jobs:
           for TAG in "${TAGS[@]}"; do
             docker buildx imagetools create --tag $TAG ${{ env.DOCKER_IMAGE_NAME }}:${{ env.DIR_HASH }}
           done
+      # Run smoke test to ensure the image is functional
+      - name: Run smoke test (unit tests in docker)
+        run: |
+          docker run \
+            --rm ${{ env.DOCKER_IMAGE_NAME }}:${{ env.DIR_HASH }} \
+            pytest scripts/test_ena_submission.py

.github/workflows/ena-submission-unit-tests.yaml
@@ -5,6 +5,7 @@ on:
     paths:
       - "ena-submission/**"
       - ".github/workflows/ena-submission-unit-tests.yaml"
+      - "ruff.toml"
   push:
     branches:
       - main
@@ -13,6 +14,24 @@ concurrency:
   group: ci-${{ github.ref == 'refs/heads/main' && github.run_id || github.ref }}-ena-submission-unit-tests
   cancel-in-progress: true
 jobs:
+  checkFormat:
+    name: Check formatting
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - uses: astral-sh/ruff-action@v3
+        with:
+          args: "format --check --diff"
+          src: "ena-submission"
+  lint:
+    name: Lint code
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - uses: astral-sh/ruff-action@v3
+        with:
+          args: "check"
+          src: "ena-submission"
   unitTests:
     name: Unit Tests
     runs-on: ubuntu-latest
@@ -21,16 +40,17 @@ jobs:
       - uses: actions/checkout@v4
       - name: Set up micromamba
         uses: mamba-org/setup-micromamba@v2
-        with: 
-            environment-file: ena-submission/environment.yml
-            micromamba-version: 'latest'
-            init-shell: >-
-                bash
-            cache-environment: true
-            post-cleanup: 'all'
+        with:
+          environment-file: "ena-submission/environment.yml"
+          micromamba-version: "latest"
+          cache-environment: true
       - name: Run tests
         run: |
-            pip install .
-            python3 scripts/test_ena_submission.py
+          pip install .
+          # The integration tests are run in a separate workflow
+          # They require postgres to be running
+            pytest \
+            --ignore=./scripts/test_ena_submission_integration.py \
+            --ignore=./scripts/test_api.py
         shell: micromamba-shell {0}
         working-directory: ena-submission

.github/workflows/ena-submission-workflow-tests.yml
@@ -53,11 +53,6 @@ jobs:
             -password=unsecure \
             -locations=filesystem:./ena-submission/flyway/sql \
             migrate
-      - name: Install WEBIN CLI
-        run: |
-          WEBIN_CLI_VERSION=$(cat ena-submission/.webinrc)
-          wget -q "https://github.com/enasequence/webin-cli/releases/download/${WEBIN_CLI_VERSION}/webin-cli-${WEBIN_CLI_VERSION}.jar" \
-            -O ena-submission/webin-cli.jar
       - name: Set up micromamba
         uses: mamba-org/setup-micromamba@v2
         with: 
@@ -67,6 +62,8 @@ jobs:
       - name: Run tests
         run: |
             pip install .
-            python3 scripts/test_ena_submission_integration.py
+            # The unit tests are run in ena-submission-unit-tests.yaml
+            pytest \
+              --ignore=./scripts/test_ena_submission.py
         shell: micromamba-shell {0}
         working-directory: ena-submission

.github/workflows/format-ena-deposition-on-dispatch.yml
@@ -1,11 +1,11 @@
-name: Format website code
+name: Format ENA deposition code on label or dispatch
 on:
   workflow_dispatch:
   pull_request:
     types: [synchronize, labeled]
     paths:
-      - "website/**"
-      - ".github/workflows/format-website-on-dispatch.yml"
+      - "ena-submission/**"
+      - ".github/workflows/format-ena-deposition-on-dispatch.yml"
 jobs:
   format:
     if: github.event_name == 'workflow_dispatch' || (github.event_name == 'pull_request' && contains(github.event.pull_request.labels.*.name, 'format_me'))
@@ -18,20 +18,16 @@ jobs:
         uses: actions/checkout@v4
         with:
           ref: ${{ github.head_ref }}
-      - name: Set up Node.js
-        uses: actions/setup-node@v4
+      - name: Run ruff format
+        uses: astral-sh/ruff-action@v3
         with:
-          node-version-file: ./website/.nvmrc
-      - name: Cache .npm
-        uses: actions/cache@v4
+          args: "format"
+          src: "ena-submission"
+      - name: Run ruff check --fix
+        uses: astral-sh/ruff-action@v3
         with:
-          path: ~/.npm
-          key: ${{ runner.os }}-node-${{ hashFiles('website/**/package-lock.json') }}
-      - name: NPM install and format website
-        working-directory: website
-        run: |
-          npm install
-          npm run format
+          args: "check --fix"
+          src: "ena-submission"
       - name: Commit and Push Changes
         run: |
           git config --global user.name 'Loculus bot'

.pre-commit-config.yaml
@@ -1,0 +1,12 @@
+repos:
+- repo: https://github.com/astral-sh/ruff-pre-commit
+  # Ruff version.
+  rev: v0.12.1
+  hooks:
+    # Run the linter and fix issues.
+    - id: ruff-check
+      args: [ --fix ]
+      files: ^ena-submission/
+    # Run the formatter.
+    - id: ruff-format
+      files: ^ena-submission/

ena-submission/.gitignore
@@ -5,4 +5,5 @@ project/
 sample/
 __pycache__
 config/config.yaml
+tests.log
 webin-cli.jar

commit 6144993245ca3fda93c25e71ba68b90701fd003b

fix(deposition): make submission list upload to Slack work again
I must have broken something in #4577 - never tested things there - it's not covered by current tests.

I now tested that the zip upload actually works with a test notification.

ena-submission/scripts/get_ena_submission_list.py
@@ -1,8 +1,9 @@
 import json
 import logging
+import zipfile
 from collections.abc import Iterator
 from dataclasses import dataclass
-from pathlib import Path
+from io import BytesIO
 from typing import Any
 
 import click
@@ -12,7 +13,6 @@ from ena_deposition.notifications import (
     SlackConfig,
     notify,
     slack_conn_init,
-    upload_file_with_comment,
 )
 from ena_deposition.submission_db_helper import (
     Accession,
@@ -21,6 +21,7 @@ from ena_deposition.submission_db_helper import (
     highest_version_in_submission_table,
 )
 from psycopg2.pool import SimpleConnectionPool
+from slack_sdk import WebClient
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -115,18 +116,32 @@ def send_slack_notification_with_file(
     slack_config: SlackConfig,
     message: str,
     entries_to_submit: dict[AccessionVersion, dict[str, Any]],
-    output_file,
+    output_file_name: str,
 ) -> None:
     len_entries = len(entries_to_submit)
-    logger.info(f"Writing {len_entries} sequences to {output_file}")
-    Path(output_file).write_text(json.dumps(entries_to_submit), encoding="utf-8")
+    logger.info(f"Writing {len_entries} sequences to {output_file_name}")
+
+    json_content = json.dumps(entries_to_submit, indent=2)
+
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        zip_file.writestr(output_file_name, json_content)
+    zip_buffer.seek(0)
 
     logger.info("Sending slack notification with file")
     if not slack_config.slack_hook:
         logger.info("Could not find slack hook, cannot send message")
         return
+
+    client = WebClient(token=slack_config.slack_token)
     try:
-        response = upload_file_with_comment(slack_config, output_file, message)
+        response = client.files_upload_v2(
+            filename=output_file_name + ".zip",
+            file=zip_buffer,
+            title=output_file_name + ".zip",
+            channel=slack_config.slack_channel_id,
+            initial_comment=message,
+        )
         if not response.get("ok", False):
             raise Exception
     except Exception as e:
@@ -184,9 +199,9 @@ def get_ena_submission_list(config_file) -> None:
                 f"{config.backend_url}: {organism} - ENA Submission pipeline wants to submit "
                 f"{len(submission_results.entries_to_submit)} sequences"
             )
-            output_file = f"{organism}_{output_file_suffix}"
+            output_file_name = f"{organism}_{output_file_suffix}"
             send_slack_notification_with_file(
-                slack_config, message, submission_results.entries_to_submit, output_file
+                slack_config, message, submission_results.entries_to_submit, output_file_name
             )
         if submission_results.entries_with_ext_metadata_to_submit:
             message = (
@@ -198,12 +213,12 @@ def get_ena_submission_list(config_file) -> None:
                 " Bioprojects should be public and SRA accessions should also include bioprojects"
                 " and biosamples."
             )
-            output_file = f"{organism}_with_ena_fields_{output_file_suffix}"
+            output_file_name = f"{organism}_with_ena_fields_{output_file_suffix}"
             send_slack_notification_with_file(
                 slack_config,
                 message,
                 submission_results.entries_with_ext_metadata_to_submit,
-                output_file,
+                output_file_name,
             )
         all_entries_to_submit.update(submission_results.entries_to_submit)
 

ena-submission/src/ena_deposition/notifications.py
@@ -1,12 +1,10 @@
 import json
 import logging
 import os
-import zipfile
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 
 import requests
-from slack_sdk import WebClient, web
 
 logger = logging.getLogger(__name__)
 
@@ -34,21 +32,6 @@ def notify(config: SlackConfig, text: str):
     """Send slack notification using slack hook"""
     if config.slack_hook:
         requests.post(config.slack_hook, data=json.dumps({"text": text}), timeout=10)
-
-
-def upload_file_with_comment(
-    config: SlackConfig, file_path: str, comment: str
-) -> web.SlackResponse:
-    """Upload file with comment to slack channel"""
-    client = WebClient(token=config.slack_token)
-    with zipfile.ZipFile(file_path, "w", zipfile.ZIP_DEFLATED) as zipf:
-        zipf.write(file_path, arcname=os.path.basename(file_path))
-    return client.files_upload_v2(
-        file=file_path,
-        title=file_path,
-        channel=config.slack_channel_id,
-        initial_comment=comment,
-    )
 
 
 def send_slack_notification(