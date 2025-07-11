948b1b7caf503678d09d01181b2fec97ecbde37f
alejandra-gonzalezsanchez <alejandra.gonzalez@vallhebron.cat>
2025-07-11 15:18:25 +0200

    f

--- preprocessing/nextclade/tests/test_processing_functions.py
+++ preprocessing/nextclade/tests/test_processing_functions.py
@@ -242,7 +242,7 @@ test_case_definitions = [
             "regex_field": None,
         },
         expected_errors=[ProcessingAnnotationTestCase(
-                ["regex_field"], ["regex_field"], "The value 'EPIISL_123456' does not match the expected regex pattern: '^EPI_ISL_\d+$'."  # type: ignore[arg-type]
+                ["regex_field"], ["regex_field"], r"The value 'EPIISL_123456' does not match the expected regex pattern: '^EPI_ISL_\d+$'."
             ),],
         expected_warnings=[],
     ),
