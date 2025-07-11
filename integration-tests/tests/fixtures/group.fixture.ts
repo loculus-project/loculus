cdee764741acc4f4b6be0c53c9e761688596ae4f
Theo Sanderson <theo@theo.io>
2025-07-11 23:36:25 +0100

    update

--- integration-tests/tests/fixtures/group.fixture.ts
+++ integration-tests/tests/fixtures/group.fixture.ts
@@ -52,7 +52,9 @@ export const test = authTest.extend<GroupFixtures>({
         await use(groupId);
     },
 
-    pageWithGroup: async ({ pageWithACreatedUser}, use) => {
+    pageWithGroup: async ({ pageWithACreatedUser, groupId }, use) => {
+        console.log(`Using group ID: ${groupId}`);
+
         await use(pageWithACreatedUser);
     },
 });
