1306ee00e6dfc6b624d4ed6aad918724f6cf95ad
Theo Sanderson <theo@sndrsn.co.uk>
2025-07-14 12:20:52 +0100

    Update governance.spec.ts

--- integration-tests/tests/specs/features/governance.spec.ts
+++ integration-tests/tests/specs/features/governance.spec.ts
@@ -1,8 +1,8 @@
 import { expect } from '@playwright/test';
 import { test } from '../../fixtures/console-warnings.fixture';
 
-test.describe('Governance page', () => {
-    test('displays Governance heading', async ({ page }) => {
+test.describe('Test markdown page', () => {
+    test('visits test markdown page', async ({ page }) => {
         await page.goto('/-testpage');
         await expect(page.getByText('Governance')).toBeVisible();
     });
