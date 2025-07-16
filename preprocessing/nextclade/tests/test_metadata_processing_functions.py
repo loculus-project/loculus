1bfcc88b6def5de11a07900576e39ea902e09ebd
alejandra-gonzalezsanchez <alejandra.gonzalez@vallhebron.cat>
2025-07-16 11:36:32 +0200

    f

--- preprocessing/nextclade/tests/test_metadata_processing_functions.py
+++ preprocessing/nextclade/tests/test_metadata_processing_functions.py
@@ -389,7 +389,8 @@ test_case_definitions = [
                 ["authors"],
                 "Unsupported non-Latin character encountered: 汉 (U+6C49).",
             ),
-        ]
+        ],
+        expected_warnings=[],
     ),
     Case(
         name="non_latin_characters_authors",
