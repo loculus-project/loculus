75fdf0ab1ce1a2958319adccbf05d920fcec64af
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-07-16 08:26:15 +0200

    improve

--- ena-submission/flyway/sql/V1.4__project_id_foreign_key.sql
+++ ena-submission/flyway/sql/V1.4__project_id_foreign_key.sql
@@ -1,10 +1,15 @@
+-- All entries without a project_id were submitted by us and thus have an ena_submission_accession
+-- We also create new projects based on the group_id and organism
+
 UPDATE submission_table s
 SET project_id = p.project_id
 FROM project_table p
 WHERE s.project_id IS NULL
   AND s.group_id = p.group_id
-  AND s.organism = p.organism;
+  AND s.organism = p.organism
+  AND p.result ? 'ena_submission_accession';
 
+-- The case will fail if there are any project_ids that are not numeric
 -- SELECT project_id FROM submission_table WHERE project_id !~ '^\d+$';
 
 ALTER TABLE submission_table
