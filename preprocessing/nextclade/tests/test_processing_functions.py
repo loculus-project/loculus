ad0b7c025c21ae5d711719e87876499003d92c50
alejandra-gonzalezsanchez <alejandra.gonzalez@vallhebron.cat>
2025-07-16 11:27:41 +0200

    Add warning for ENA deposition

--- preprocessing/nextclade/tests/test_processing_functions.py
+++ preprocessing/nextclade/tests/test_processing_functions.py
@@ -416,6 +416,13 @@ test_case_definitions = [
                 "Unsupported non-Latin character encountered: 汉 (U+6C49).",
             ),
         ],
+        expected_warnings=[
+            ProcessingAnnotationTestCase(
+                ["authors"],
+                ["authors"],
+                "Latin non-ASCII characters detected; they will be converted to ASCII characters for ENA deposition.",
+            ),
+        ],
     ),
 #    Case(
 #        name="non_ascii_authors",
