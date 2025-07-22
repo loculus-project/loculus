af935e8a81cd9971e153abffa95cdb7ed970e740
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-07-22 18:32:35 +0200

    Make it configurable how often we check for prepro upgradability (fairly expensive as it does a full join etc.)

--- backend/src/main/kotlin/org/loculus/backend/config/BackendSpringConfig.kt
+++ backend/src/main/kotlin/org/loculus/backend/config/BackendSpringConfig.kt
@@ -32,6 +32,7 @@ object BackendSpringProperty {
     const val BACKEND_CONFIG_PATH = "loculus.config.path"
     const val STALE_AFTER_SECONDS = "loculus.cleanup.task.reset-stale-in-processing-after-seconds"
     const val CLEAN_UP_RUN_EVERY_SECONDS = "loculus.cleanup.task.run-every-seconds"
+    const val USE_NEWER_PIPELINE_RUN_EVERY_SECONDS = "loculus.use-newer-pipeline.task.run-every-seconds"
     const val STREAM_BATCH_SIZE = "loculus.stream.batch-size"
     const val DEBUG_MODE = "loculus.debug-mode"
     const val ENABLE_SEQSETS = "loculus.enable-seqsets"
