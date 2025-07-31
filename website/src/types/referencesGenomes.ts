b89e1bc60e38dd2314670038f145a18ba62ad618
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-07-31 11:26:03 +0200

    4784 wip

--- website/src/types/referencesGenomes.ts
+++ website/src/types/referencesGenomes.ts
@@ -19,6 +19,8 @@ export const referenceGenome = z.object({
 export type ReferenceGenome = z.infer<typeof referenceGenome>;
 
 export const suborganism = z.string();
+export type Suborganism = z.infer<typeof suborganism>;
+
 export const referenceGenomes = z
     .record(suborganism, referenceGenome)
     .refine((value) => Object.entries(value).length > 0, 'The reference genomes must not be empty.');
@@ -26,8 +28,13 @@ export type ReferenceGenomes = z.infer<typeof referenceGenomes>;
 
 export type NucleotideSegmentNames = string[];
 
-export type ReferenceGenomesSequenceNames = {
-    nucleotideSequences: NucleotideSegmentNames;
-    genes: string[];
-    insdcAccessionFull: ReferenceAccession[];
-};
+export type ReferenceGenomesSequenceNames = Record<
+    Suborganism,
+    {
+        nucleotideSequences: NucleotideSegmentNames;
+        genes: string[];
+        insdcAccessionFull: ReferenceAccession[];
+    }
+>;
+
+export const SINGLE_REFERENCE = 'singleReference';
