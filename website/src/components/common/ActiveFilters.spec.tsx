8badf9b0e4eb3d2e421b3e1dcf1848d74c07e44d
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-07-31 11:45:35 +0200

    4784 wip

--- website/src/components/common/ActiveFilters.spec.tsx
+++ website/src/components/common/ActiveFilters.spec.tsx
@@ -21,7 +21,13 @@ describe('ActiveFilters', () => {
                             new MetadataFilterSchema([]),
                             { field1: 'value1', mutations: 'A123T,G234C' },
                             {},
-                            { nucleotideSequences: ['main'], genes: [], insdcAccessionFull: [] },
+                            {
+                                [SINGLE_REFERENCE]: {
+                                    nucleotideSequences: ['main'],
+                                    genes: [],
+                                    insdcAccessionFull: [],
+                                },
+                            },
                         )
                     }
                 />,
@@ -62,7 +68,7 @@ describe('ActiveFilters', () => {
                             new MetadataFilterSchema([]),
                             { field1: 'value1' },
                             {},
-                            { nucleotideSequences: [], genes: [], insdcAccessionFull: [] },
+                            { [SINGLE_REFERENCE]: { nucleotideSequences: [], genes: [], insdcAccessionFull: [] } },
                         )
                     }
                     removeFilter={mockRemoveFilter}
