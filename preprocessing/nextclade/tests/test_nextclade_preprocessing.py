55b38fda6ff4d588cd5f15499d3ac4706493f768
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-08-05 14:27:38 +0200

    feat(ingest): add tests

--- preprocessing/nextclade/tests/test_nextclade_preprocessing.py
+++ preprocessing/nextclade/tests/test_nextclade_preprocessing.py
@@ -15,7 +15,7 @@ from factory_methods import (
     verify_processed_entry,
 )
 
-from loculus_preprocessing.config import Config, get_config
+from loculus_preprocessing.config import AlignmentRequirement, Config, get_config
 from loculus_preprocessing.datatypes import (
     AnnotationSourceType,
     ProcessedEntry,
@@ -437,6 +437,9 @@ multi_segment_case_definitions = [
             aminoAcidInsertions={},
         ),
     ),
+]
+
+multi_segment_case_definitions_all_requirement = [
     Case(
         name="with one failed alignment, one succeeded",
         input_metadata={},
@@ -482,7 +485,56 @@ multi_segment_case_definitions = [
             },
             aminoAcidInsertions={},
         ),
-    ),
+    )
+]
+
+multi_segment_case_definitions_any_requirement = [
+    Case(
+        name="with one failed alignment, one succeeded",
+        input_metadata={},
+        input_sequence={
+            "ebola-sudan": invalid_sequence(),
+            "ebola-zaire": sequence_with_mutation("ebola-zaire"),
+        },
+        accession_id="1",
+        expected_metadata={
+            "totalInsertedNucs_ebola-sudan": None,
+            "totalSnps_ebola-sudan": None,
+            "totalDeletedNucs_ebola-sudan": None,
+            "length_ebola-sudan": 53,
+            "totalInsertedNucs_ebola-zaire": 0,
+            "totalSnps_ebola-zaire": 1,
+            "totalDeletedNucs_ebola-zaire": 0,
+            "length_ebola-zaire": len(consensus_sequence("ebola-zaire")),
+        },
+        expected_errors=[],
+        expected_warnings=[
+            ProcessingAnnotationHelper(
+                ["ebola-sudan"],
+                ["ebola-sudan"],
+                "Nucleotide sequence for ebola-sudan failed to align",
+                AnnotationSourceType.NUCLEOTIDE_SEQUENCE,
+            )
+        ],
+        expected_processed_alignment=ProcessedAlignment(
+            unalignedNucleotideSequences={
+                "ebola-sudan": invalid_sequence(),
+                "ebola-zaire": sequence_with_mutation("ebola-zaire"),
+            },
+            alignedNucleotideSequences={
+                "ebola-sudan": None,
+                "ebola-zaire": sequence_with_mutation("ebola-zaire"),
+            },
+            nucleotideInsertions={"ebola-sudan": [], "ebola-zaire": []},
+            alignedAminoAcidSequences={
+                "NPEbolaSudan": None,
+                "VP35EbolaSudan": None,
+                "VP24EbolaZaire": ebola_zaire_aa(sequence_with_mutation("ebola-zaire"), "VP24"),
+                "LEbolaZaire": ebola_zaire_aa(sequence_with_mutation("ebola-zaire"), "L"),
+            },
+            aminoAcidInsertions={},
+        ),
+    )
 ]
 
 
@@ -505,10 +557,26 @@ def test_preprocessing_single_segment(test_case_def: Case):
 
 
 @pytest.mark.parametrize(
-    "test_case_def", multi_segment_case_definitions, ids=lambda tc: f"multi segment {tc.name}"
+    "test_case_def",
+    multi_segment_case_definitions + multi_segment_case_definitions_all_requirement,
+    ids=lambda tc: f"multi segment {tc.name}",
 )
-def test_preprocessing_multi_segment(test_case_def: Case):
+def test_preprocessing_multi_segment_all_requirement(test_case_def: Case):
     config = get_config(MULTI_SEGMENT_CONFIG, ignore_args=True)
+    factory_custom = ProcessedEntryFactory(all_metadata_fields=list(config.processing_spec.keys()))
+    test_case = test_case_def.create_test_case(factory_custom)
+    processed_entry = process_single_entry(test_case, config, MULTI_EBOLA_DATASET)
+    verify_processed_entry(processed_entry, test_case.expected_output, test_case.name)
+
+
+@pytest.mark.parametrize(
+    "test_case_def",
+    multi_segment_case_definitions + multi_segment_case_definitions_any_requirement,
+    ids=lambda tc: f"multi segment {tc.name}",
+)
+def test_preprocessing_multi_segment_any_requirement(test_case_def: Case):
+    config = get_config(MULTI_SEGMENT_CONFIG, ignore_args=True)
+    config.alignment_requirement = AlignmentRequirement.ANY
     factory_custom = ProcessedEntryFactory(all_metadata_fields=list(config.processing_spec.keys()))
     test_case = test_case_def.create_test_case(factory_custom)
     processed_entry = process_single_entry(test_case, config, MULTI_EBOLA_DATASET)
