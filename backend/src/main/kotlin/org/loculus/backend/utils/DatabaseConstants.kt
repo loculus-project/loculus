e766e34649aba580725c77397fa2482fff03bbe7
Theo Sanderson <theo@sndrsn.co.uk>
2025-08-06 17:43:02 +0100

    Update backend/src/main/kotlin/org/loculus/backend/utils/DatabaseConstants.kt
    Co-authored-by: Chaoran Chen <mail@chaoran-chen.de>

--- backend/src/main/kotlin/org/loculus/backend/utils/DatabaseConstants.kt
+++ backend/src/main/kotlin/org/loculus/backend/utils/DatabaseConstants.kt
@@ -9,7 +9,11 @@ fun <T> Collection<T>.processInDatabaseSafeChunks(action: (List<T>) -> Unit) {
     this.chunked(DatabaseConstants.SAFE_CHUNK_SIZE).forEach(action)
 }
 
-fun <T, R> Collection<T>.chunkedForDatabase(transform: (List<T>) -> List<R>): List<R> {
+/**
+  * PostgreSQL only supports 65,535 query parameters per query. This function chunks a list to avoid sending more parameters than allowed.
+  * @param numberQueryParametersPerEntry The number of query parameters that each entry in the list would use. The default of 6 should be safe for many requests. 
+  */
+fun <T, R> Collection<T>.chunkedForDatabase(transform: (List<T>) -> List<R>, numberQueryParametersPerEntry = 6): List<R> {
     if (isEmpty()) return emptyList()
-    return chunked(DatabaseConstants.SAFE_CHUNK_SIZE).flatMap(transform)
+    return chunked(floor((POSTGRESQL_PARAMETER_LIMIT - 100)/ numberQueryParametersPerEntry)).flatMap(transform)
 }
