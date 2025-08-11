d182c08a6a1dc39d618aa10e88f2febf44a6891a
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-08-11 16:50:48 +0200

    feat(kotlin): correctly define fields

--- backend/src/main/kotlin/org/loculus/backend/service/submission/UploadDatabaseService.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/UploadDatabaseService.kt
@@ -113,7 +113,7 @@ class UploadDatabaseService(
         uploadedSequencesBatch.chunkedForDatabase({ batch ->
             SequenceUploadAuxTable.batchInsert(batch) {
                 this[sequenceSubmissionIdColumn] = it.sampleName
-                this[metadataSubmissionIdColumn] = it.sampleName
+                this[metadataSubmissionIdColumn] = null
                 this[sequenceUploadIdColumn] = uploadId
                 this[compressedSequenceDataColumn] = compressor.compressNucleotideSequence(
                     it.sequence,
