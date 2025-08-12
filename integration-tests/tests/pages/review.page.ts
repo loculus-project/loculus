278d08519e0f6e9fc961ea9334dd11910b3d94d2
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-08-12 15:48:51 +0200

    feat: revert changes that are not required

--- integration-tests/tests/pages/review.page.ts
+++ integration-tests/tests/pages/review.page.ts
@@ -22,7 +22,7 @@ export class ReviewPage {
     async waitForZeroProcessing() {
         await expect(this.page.locator('[data-testid="review-page-control-panel"]')).toContainText(
             '0 awaiting processing',
-            { timeout: 200000 },
+            { timeout: 60000 },
         );
     }
 
