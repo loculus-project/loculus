75f9f30d3cda6952623005684116d2cbe1211a12
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-09-05 11:13:13 +0200

    feat(backend): make fileMapping optional in swagger

--- backend/src/main/kotlin/org/loculus/backend/controller/SubmissionController.kt
+++ backend/src/main/kotlin/org/loculus/backend/controller/SubmissionController.kt
@@ -104,7 +104,7 @@ open class SubmissionController(
                 " It is the date when the sequence entries will become 'OPEN'." +
                 " Format: YYYY-MM-DD",
         ) @RequestParam restrictedUntil: String?,
-        @Parameter(description = FILE_MAPPING_DESCRIPTION) @RequestPart fileMapping: String?,
+        @Parameter(description = FILE_MAPPING_DESCRIPTION) @RequestPart(required = false) fileMapping: String?,
     ): List<SubmissionIdMapping> {
         var innerDataUseTermsType = DataUseTermsType.OPEN
         if (backendConfig.dataUseTerms.enabled) {
