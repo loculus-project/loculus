e66354c71edecbbeabcab021354f8893a18a5d6d
Theo Sanderson <theo@sndrsn.co.uk>
2025-09-05 17:25:41 +0100

    fix(website): restore URL storage when query shortens (#4947)
    - allow search state to return to URL when its encoded length falls
    below the max
    - test that URL storage resumes after length shrinks
    
    Prior to this if you hit "Select all" then "Select none" stuff would no
    longer be stored in the URL. Now that will only be the case while there
    is so much that the URL can't fit the info.
    
    
    ------
    https://chatgpt.com/codex/tasks/task_e_68b997541e088325b80b74d2d010a880
    
    🚀 Preview: https://codex-add-dynamic-url-sta.loculus.org

--- website/src/components/SearchPage/useQueryAsState.spec.ts
+++ website/src/components/SearchPage/useQueryAsState.spec.ts
@@ -71,4 +71,23 @@ describe('useQueryAsState', () => {
 
         expect(replaceStateMock).toHaveBeenCalledWith({ path: '/test' }, '', '/test');
     });
+
+    test('re-enables URL storage when length drops below max', () => {
+        const longValue = 'a'.repeat(2000);
+        const { result } = renderHook(() => useQueryAsState({}));
+
+        act(() => {
+            result.current[1]({ longKey: longValue });
+        });
+
+        act(() => {
+            result.current[1]({ shortKey: 'b' });
+        });
+
+        expect(replaceStateMock).toHaveBeenLastCalledWith(
+            { path: 'http://localhost:3000/test?shortKey=b' },
+            '',
+            'http://localhost:3000/test?shortKey=b',
+        );
+    });
 });
