64c19f619c6029e5dfce2e60b208196acc1d9042
Theo Sanderson <theo@sndrsn.co.uk>
2025-09-07 23:24:29 +0300

    Update RestrictedUseWarning.tsx

--- website/src/components/common/RestrictedUseWarning.tsx
+++ website/src/components/common/RestrictedUseWarning.tsx
@@ -5,7 +5,11 @@ import ErrorBox from './ErrorBox';
 const RestrictedUseWarning: React.FC = () => {
     return (
         <ErrorBox title='Restricted-Use sequence' level='warning'>
-            This sequence is only available under the Restricted Use Terms
+            This sequence is only available under the Restricted Use Terms. If you make use of this data, you
+                    must follow the{' '}
+                    <a href={routes.datauseTermsPage()} className='underline'>
+                        terms of use
+                    </a>.
         </ErrorBox>
     );
 };
