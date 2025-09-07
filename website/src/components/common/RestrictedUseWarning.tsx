8952532921c7d1b7303803369dcd9362c25634c0
Theo Sanderson <theo@sndrsn.co.uk>
2025-09-07 23:04:14 +0300

    update

--- website/src/components/common/RestrictedUseWarning.tsx
+++ website/src/components/common/RestrictedUseWarning.tsx
@@ -5,12 +5,8 @@ import ErrorBox from './ErrorBox';
 const RestrictedUseWarning: React.FC = () => {
     return (
         <ErrorBox title='Restricted-Use sequence' level='warning'>
-            This sequence is only available under the{' '}
-            <a href='https://pathoplexus.org/about/terms-of-use/restricted-data' className='underline'>
-                Restricted Use Terms
-            </a>
-            . This prohibits publications that use this sequence as focal data during the restricted-use period, except
-            with the consent of the sequence submitters.
+            This sequence is only available under the
+             Restricted Use Terms
         </ErrorBox>
     );
 };
