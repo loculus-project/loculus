4de65cccf3786ffae60e24ad5ac19c02accfa5c1
Theo Sanderson <theo@sndrsn.co.uk>
2025-09-23 13:13:21 +0100

    continue submission (#5082)
    resolves #
    
    ### Screenshot
    
    ### PR Checklist
    - [ ] All necessary documentation has been adapted.
    - [ ] The implemented feature is covered by appropriate, automated
    tests.
    - [ ] Any manual testing that has been done is documented (i.e. what
    exactly was tested?)
    
    🚀 Preview: https://nav-exp4.loculus.org

--- website/src/components/common/NeedAGroup.tsx
+++ website/src/components/common/NeedAGroup.tsx
@@ -1,20 +1,30 @@
 import { routes } from '../../routes/routes';
 import DashiconsGroups from '~icons/dashicons/groups';
 
-export const NeedAGroup = () => (
-    <div className='mt-6 message max-w-4xl mx-auto'>
-        <DashiconsGroups className='w-12 h-12 inline-block mr-2' />
-        <div>
-            <p>
-                Sequences can only be submitted to the database by users who are part of a <i>submitting group</i>.
-            </p>
-            <p className='mt-3'>
-                To submit to the database, please either{' '}
-                <a href={routes.createGroup()} className='underline'>
-                    create a submitting group
-                </a>{' '}
-                or ask a group administrator to add you to an existing group.
-            </p>
+interface NeedAGroupProps {
+    continueSubmissionOrganism?: string;
+}
+
+export const NeedAGroup = ({ continueSubmissionOrganism }: NeedAGroupProps) => {
+    const href = continueSubmissionOrganism
+        ? routes.createGroup({ organism: continueSubmissionOrganism })
+        : routes.createGroup();
+
+    return (
+        <div className='mt-6 message max-w-4xl mx-auto'>
+            <DashiconsGroups className='w-12 h-12 inline-block mr-2' />
+            <div>
+                <p>
+                    Sequences can only be submitted to the database by users who are part of a <i>submitting group</i>.
+                </p>
+                <p className='mt-3'>
+                    To submit to the database, please either{' '}
+                    <a href={href} className='underline'>
+                        create a submitting group
+                    </a>{' '}
+                    or ask a group administrator to add you to an existing group.
+                </p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
