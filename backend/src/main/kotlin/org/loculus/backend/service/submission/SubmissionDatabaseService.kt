50b96b871997eab5c7b0c7a8e8bac3ab66dbec25
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-10-06 17:03:58 +0200

    Update backend/src/main/kotlin/org/loculus/backend/service/submission/SubmissionDatabaseService.kt

--- backend/src/main/kotlin/org/loculus/backend/service/submission/SubmissionDatabaseService.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/SubmissionDatabaseService.kt
@@ -97,8 +97,6 @@ import org.springframework.transaction.annotation.Transactional
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.time.Instant
-import java.time.temporal.ChronoUnit
 import java.util.Locale
 import javax.sql.DataSource
 
