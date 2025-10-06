55dac91116a588478c68a5cef13a3fb497f03708
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2025-10-06 09:41:01 +0200

    feat(website): review page, sequences dialog: only show sequences that exist for that entry (#5156)
    resolves #4998
    
    
    ### Screenshot
    
    Before:
    <img width="1177" height="392" alt="image"
    src="https://github.com/user-attachments/assets/d0520bc9-6fb0-4a46-a877-1809f6fc45dd"
    />
    
    After:
    <img width="1177" height="392" alt="image"
    src="https://github.com/user-attachments/assets/fb5f1506-2950-4536-8f81-56d10e3796c2"
    />
    
    
    ### PR Checklist
    ~~- [ ] All necessary documentation has been adapted.~~
    - [x] The implemented feature is covered by appropriate, automated
    tests.
    - [x] Any manual testing that has been done is documented (i.e. what
    exactly was tested?)
      - see screenshots
    
    🚀 Preview: Add `preview` label to enable

---
+++ website/src/components/ReviewPage/SequencesDialog.spec.tsx
@@ -0,0 +1,74 @@
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, test } from 'vitest';
+
+import { SequencesDialog } from './SequencesDialog.tsx';
+import { processedStatus, type SequenceEntryToEdit } from '../../types/backend.ts';
+
+describe('SequencesDialog', () => {
+    test('should only show existing sequences', async () => {
+        const { getByText, queryByText, getByRole } = render(
+            <SequencesDialog isOpen={true} onClose={() => undefined} dataToView={dataToView} />,
+        );
+
+        await waitFor(() => {
+            expect(getByText('ATTTGCC')).toBeVisible();
+        });
+
+        await userEvent.click(getByRole('button', { name: `${sequence1} (aligned)` }));
+        await waitFor(() => {
+            expect(getByText('A-T-T-T-G-C-C')).toBeVisible();
+        });
+
+        await userEvent.click(getByRole('button', { name: `${sequence1} (unaligned)` }));
+        await waitFor(() => {
+            expect(getByText('ATTTGCC')).toBeVisible();
+        });
+
+        await userEvent.click(getByRole('button', { name: `gene1` }));
+        await waitFor(() => {
+            expect(getByText('MADS*')).toBeVisible();
+        });
+
+        expect(queryByText(new RegExp(sequence2))).not.toBeInTheDocument();
+        expect(queryByText(new RegExp(gene2))).not.toBeInTheDocument();
+    });
+});
+
+const sequence1 = 'sequence1';
+const sequence2 = 'sequence2';
+const gene1 = 'gene1';
+const gene2 = 'gene2';
+
+const dataToView: SequenceEntryToEdit = {
+    submissionId: 'test',
+    accession: 'test',
+    version: 0,
+    groupId: 0,
+    originalData: {
+        metadata: {},
+        unalignedNucleotideSequences: {},
+        files: null,
+    },
+    processedData: {
+        metadata: {},
+        unalignedNucleotideSequences: {
+            [sequence1]: 'ATTTGCC',
+            [sequence2]: null,
+        },
+        alignedNucleotideSequences: {
+            [sequence1]: 'A-T-T-T-G-C-C',
+            [sequence2]: null,
+        },
+        alignedAminoAcidSequences: {
+            [gene1]: 'MADS*',
+            [gene2]: null,
+        },
+        nucleotideInsertions: {},
+        aminoAcidInsertions: {},
+        files: null,
+    },
+    status: processedStatus,
+    errors: null,
+    warnings: null,
+};
