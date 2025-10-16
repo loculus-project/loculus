b4f8346f516037e4fad5ed94d86f0e8a987a2748
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-10-16 13:35:22 +0200

    4769 wip

--- backend/src/test/kotlin/org/loculus/backend/controller/EndpointTestExtension.kt
+++ backend/src/test/kotlin/org/loculus/backend/controller/EndpointTestExtension.kt
@@ -5,6 +5,8 @@ import io.minio.MakeBucketArgs
 import io.minio.MinioClient
 import io.minio.SetBucketPolicyArgs
 import mu.KotlinLogging
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -31,12 +33,14 @@ import org.loculus.backend.service.submission.SEQUENCE_ENTRIES_TABLE_NAME
 import org.loculus.backend.service.submission.SEQUENCE_UPLOAD_AUX_TABLE_NAME
 import org.loculus.backend.service.submission.dbtables.CURRENT_PROCESSING_PIPELINE_TABLE_NAME
 import org.loculus.backend.testutil.TestEnvironment
+import org.loculus.backend.testutil.docker.DockerPostgres
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.core.annotation.AliasFor
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
+import java.io.File
 
 /**
  * The main annotation for tests. It also loads the [EndpointTestExtension], which initializes
@@ -127,7 +131,9 @@ val MINIO_TEST_BUCKET = "testbucket"
 private val log = KotlinLogging.logger { }
 
 class EndpointTestExtension :
+    BeforeAllCallback,
     BeforeEachCallback,
+    AfterEachCallback,
     TestExecutionListener {
     companion object {
         private val env = TestEnvironment
@@ -203,9 +209,18 @@ class EndpointTestExtension :
         }
     }
 
+    override fun beforeAll(context: ExtensionContext?) {
+        (env.postgres as DockerPostgres).restore(File(javaClass.classLoader.getResource("MigrationTest_pg_dump.sql").file))
+    }
+
     override fun beforeEach(context: ExtensionContext) {
         log.debug("Clearing database")
         env.postgres.exec(clearDatabaseStatement())
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        log.debug("Dumping database")
+        (env.postgres as DockerPostgres).dump(File("dump.sql"))
     }
 
     override fun testPlanExecutionFinished(testPlan: TestPlan) {
