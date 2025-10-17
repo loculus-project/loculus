ff8e6e19b2974963c49ecf0daaa45930fd6c2bb9
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-10-17 10:35:25 +0200

    4769 wip

---
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/CompressionDictionariesTable.kt
@@ -0,0 +1,21 @@
+package org.loculus.backend.service.submission
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+
+private const val COMPRESSION_DICTIONARIES_TABLE_NAME = "compression_dictionaries_table"
+
+object CompressionDictionariesTable : IntIdTable(COMPRESSION_DICTIONARIES_TABLE_NAME, "id") {
+    val hashColumn = text("hash").uniqueIndex()
+    val dictContentsColumn = text("dict_contents")
+}
+
+class CompressionDictionaryEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<CompressionDictionaryEntity>(CompressionDictionariesTable)
+
+    var hash by CompressionDictionariesTable.hashColumn
+    var dictContents by CompressionDictionariesTable.dictContentsColumn
+}
