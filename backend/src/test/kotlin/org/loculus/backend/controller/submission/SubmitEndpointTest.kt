09dae7f2cd12b3515c4cf888d45784009110edb4
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-10-18 13:32:38 +0200

    feat(backend): improve tests

--- backend/src/test/kotlin/org/loculus/backend/controller/submission/SubmitEndpointTest.kt
+++ backend/src/test/kotlin/org/loculus/backend/controller/submission/SubmitEndpointTest.kt
@@ -5,7 +5,9 @@ import kotlinx.datetime.DateTimeUnit.Companion.YEAR
 import kotlinx.datetime.minus
 import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
+import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.containsString
+import org.hamcrest.Matchers.hasEntry
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -33,6 +35,7 @@ import org.loculus.backend.utils.DateProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.web.servlet.ResultMatcher
@@ -45,6 +48,7 @@ import kotlin.time.Clock
 @EndpointTest
 class SubmitEndpointTest(
     @Autowired val submissionControllerClient: SubmissionControllerClient,
+    @Autowired val convenienceClient: SubmissionConvenienceClient,
     @Autowired val backendConfig: BackendConfig,
     @Autowired val groupManagementClient: GroupManagementControllerClient,
 ) {
@@ -151,6 +155,81 @@ class SubmitEndpointTest(
             .andExpect(jsonPath("\$[0].submissionId").value("custom0"))
             .andExpect(jsonPath("\$[0].accession", containsString(backendConfig.accessionPrefix)))
             .andExpect(jsonPath("\$[0].version").value(1))
+    }
+
+    @Test
+    fun `GIVEN valid input data THEN data is accepted and shows fastaID`() {
+        val groupId = groupManagementClient.createNewGroup().andGetGroupId()
+
+        submissionControllerClient.submit(
+            SubmitFiles.metadataFileWith(
+                content = """
+                        submissionId	firstColumn
+                        header1	someValue
+                        header2	someValue
+                """.trimIndent(),
+            ),
+            SubmitFiles.sequenceFileWith(
+                content = """
+                        >header1
+                        AC
+                        >header2
+                        GT
+                """.trimIndent(),
+            ),
+            groupId = groupId,
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("\$.length()").value(2))
+            .andExpect(jsonPath("\$[0].submissionId").value("header1"))
+            .andExpect(jsonPath("\$[0].accession", containsString(backendConfig.accessionPrefix)))
+            .andExpect(jsonPath("\$[0].version").value(1))
+
+        val unalignedNucleotideSequences = convenienceClient.extractUnprocessedData()[0]
+            .data
+            .unalignedNucleotideSequences
+
+        assertThat(unalignedNucleotideSequences, hasEntry("header1", "AC"))
+    }
+
+    @Test
+    fun `GIVEN valid input multi segment data THEN data is accepted and originalData shows fastaID`() {
+        val groupId = groupManagementClient.createNewGroup().andGetGroupId()
+
+        submissionControllerClient.submit(
+            SubmitFiles.metadataFileWith(
+                content = """
+                        submissionId	firstColumn	fastaId
+                        header1	someValue	header1_seg1, header1_seg2
+                        header2	someValue	fasta_header2_seg1
+                """.trimIndent(),
+            ),
+            SubmitFiles.sequenceFileWith(
+                content = """
+                        >header1_seg1
+                        AC
+                        >header1_seg2
+                        GT
+                        >fasta_header2_seg1
+                        TA
+                """.trimIndent(),
+            ),
+            organism = OTHER_ORGANISM,
+            groupId = groupId,
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("\$.length()").value(2))
+            .andExpect(jsonPath("\$[0].submissionId").value("header1"))
+            .andExpect(jsonPath("\$[0].accession", containsString(backendConfig.accessionPrefix)))
+            .andExpect(jsonPath("\$[0].version").value(1))
+
+        val unalignedNucleotideSequences = convenienceClient.extractUnprocessedData(organism = OTHER_ORGANISM)[0]
+            .data
+            .unalignedNucleotideSequences
+
+        assertThat(unalignedNucleotideSequences, hasEntry("header1_seg1", "AC"))
     }
 
     @Test
