759b8a8111f447f9b0a6d1859a4f06776518d185
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-10-22 08:45:44 +0200

    4769 rename table

--- backend/src/main/resources/db/migration/V1.17__add_compression_dict_table.sql
+++ backend/src/main/resources/db/migration/V1.17__add_compression_dict_table.sql
@@ -1,8 +1,8 @@
-create table compression_dictionaries_table
+create table compression_dictionaries
 (
     id            SERIAL primary key,
     hash          TEXT not null unique,
     dict_contents TEXT not null
 );
 
-create index idx_dict_table_hash on compression_dictionaries_table (hash);
+create index idx_dict_table_hash on compression_dictionaries (hash);
