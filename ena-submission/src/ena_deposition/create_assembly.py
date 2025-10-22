691e38b531b52a0d2e567e14c94c0623ef2adbe8
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-10-22 16:29:05 +0200

    feat(deposition): only block revision if formatted authors field changes

--- ena-submission/src/ena_deposition/create_assembly.py
+++ ena-submission/src/ena_deposition/create_assembly.py
@@ -393,6 +393,9 @@ def can_be_revised(config: Config, db_config: SimpleConnectionPool, entry: dict[
         for field in value.get("loculus_fields", []):
             last_entry = last_version_data[0]["metadata"].get(field)
             new_entry = entry["metadata"].get(field)
+            if field == "authors":
+                last_entry = get_authors(last_entry) if last_entry else last_entry
+                new_entry = get_authors(new_entry) if new_entry else new_entry
             if last_entry != new_entry:
                 differing_fields.append(field)
     if differing_fields:
