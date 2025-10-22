57e50b126e993a9ff66c9e4181c7d6992cd5c682
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-10-22 10:26:43 +0200

    4769 unit test

--- backend/src/test/kotlin/org/loculus/backend/controller/EndpointTestExtension.kt
+++ backend/src/test/kotlin/org/loculus/backend/controller/EndpointTestExtension.kt
@@ -29,6 +29,7 @@ import org.loculus.backend.service.submission.METADATA_UPLOAD_AUX_TABLE_NAME
 import org.loculus.backend.service.submission.SEQUENCE_ENTRIES_PREPROCESSED_DATA_TABLE_NAME
 import org.loculus.backend.service.submission.SEQUENCE_ENTRIES_TABLE_NAME
 import org.loculus.backend.service.submission.SEQUENCE_UPLOAD_AUX_TABLE_NAME
+import org.loculus.backend.service.submission.dbtables.COMPRESSION_DICTIONARIES_TABLE_NAME
 import org.loculus.backend.service.submission.dbtables.CURRENT_PROCESSING_PIPELINE_TABLE_NAME
 import org.loculus.backend.testutil.TestEnvironment
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -235,6 +236,7 @@ private fun clearDatabaseStatement(): String = """
             $DATA_USE_TERMS_TABLE_NAME,
             $CURRENT_PROCESSING_PIPELINE_TABLE_NAME,
             $FILES_TABLE_NAME,
+            $COMPRESSION_DICTIONARIES_TABLE_NAME,
             external_metadata,
             seqsets,
             seqset_records,
