70151485a214c6cbe2d28259bf6e83f92454a07c
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2025-10-23 10:38:17 +0200

    4769 no migration for now

--- backend/src/test/kotlin/org/loculus/backend/service/CompressionServiceTest.kt
+++ backend/src/test/kotlin/org/loculus/backend/service/CompressionServiceTest.kt
@@ -4,22 +4,28 @@ import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.loculus.backend.SpringBootTestWithoutDatabase
 import org.loculus.backend.api.Organism
 import org.loculus.backend.api.OriginalData
+import org.loculus.backend.config.BackendConfig
+import org.loculus.backend.controller.DEFAULT_ORGANISM
 import org.loculus.backend.service.submission.CompressionDictService
 import org.loculus.backend.service.submission.CompressionService
 import org.loculus.backend.service.submission.DictEntry
+import org.springframework.beans.factory.annotation.Autowired
 
-class CompressionServiceTest {
+@SpringBootTestWithoutDatabase
+class CompressionServiceTest(@Autowired private val backendConfig: BackendConfig) {
     private val compressionDictServiceMock = mockk<CompressionDictService>()
 
     private val compressor = CompressionService(
+        backendConfig = backendConfig,
         compressionDictService = compressionDictServiceMock,
     )
 
     @Test
     fun `Round trip compress and decompress sequences in original data`() {
-        val organism = Organism("test organism")
+        val organism = Organism(DEFAULT_ORGANISM)
 
         val dict = "NNACTGACTGACTGACTGATCGATCGATCGATCGATCGATCGATC".toByteArray()
         every { compressionDictServiceMock.getDictForUnalignedSequence(organism) } returns DictEntry(
@@ -37,7 +43,7 @@ class CompressionServiceTest {
             mapOf(segmentName to input),
         )
         val compressed = compressor.compressSequencesInOriginalData(testData, organism)
-        val decompressed = compressor.decompressSequencesInOriginalData(compressed)
+        val decompressed = compressor.decompressSequencesInOriginalData(compressed, organism)
 
         assertEquals(testData, decompressed)
     }
