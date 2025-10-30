d915fd6c4192c6b09228d881cbfb4b074b6b77ac
Theo Sanderson <theo@theo.io>
2025-10-30 15:49:10 +0000

    test(website): move download stuff from e2e to integration tests

--- website/tests/pages/search/index.spec.ts
+++ website/tests/pages/search/index.spec.ts
@@ -1,11 +1,8 @@
-import { type Page, type Download } from '@playwright/test';
 import { DateTime } from 'luxon';
 
+import { getAccessionVersionString } from '../../../src/utils/extractAccessionVersion.ts';
 import { expect, test } from '../../e2e.fixture';
-
-interface PerformDownloadOptions {
-    selectRawNucleotide?: boolean;
-}
+import { getTestSequences } from '../../util/testSequenceProvider.ts';
 
 test.describe('The search page', () => {
     test('should find no data in the future', async ({ searchPage }) => {
@@ -27,50 +24,19 @@ test.describe('The search page', () => {
         expect(rowCount).toBeGreaterThan(0);
     });
 
-    async function performDownload(page: Page, options: PerformDownloadOptions = {}): Promise<string> {
-        const { selectRawNucleotide = false } = options;
-
-        const downloadButton = page.getByRole('button', { name: 'Download' });
-        await downloadButton.click();
-
-        if (selectRawNucleotide) {
-            const rawNucleotideRadio = page.getByLabel('Raw nucleotide sequences');
-            await rawNucleotideRadio.check();
-        }
-
-        const agreeCheckbox = page.getByLabel(/I agree/);
-        await agreeCheckbox.check();
-
-        const downloadButton2 = page.getByTestId('start-download');
-
-        const downloadPromise: Promise<Download> = page.waitForEvent('download');
-
-        await downloadButton2.click();
-
-        const download: Download = await downloadPromise;
-
-        const suggestedFileName: string = download.suggestedFilename();
-        const filePath: string = '/tmp/' + String(Math.random()).slice(0, 5) + suggestedFileName;
-        await download.saveAs(filePath);
-
-        return filePath;
-    }
-
-    test('should download file when agreeing to terms', async ({ searchPage, page, browserName }) => {
-        test.skip(browserName === 'webkit', 'Download tests are skipped on WebKit');
+    test('should reset the search', async ({ searchPage }) => {
         await searchPage.goto();
 
-        const filePath = await performDownload(page);
+        const testAccessionVersion = getAccessionVersionString(getTestSequences().testSequenceEntry);
 
-        expect(filePath).toBeTruthy();
-    });
+        await searchPage.getAccessionField().click();
 
-    test('should download raw nucleotide sequences when selected', async ({ searchPage, page, browserName }) => {
-        test.skip(browserName === 'webkit', 'Download tests are skipped on WebKit');
-        await searchPage.goto();
+        await searchPage.getAccessionField().fill(testAccessionVersion);
 
-        const filePath = await performDownload(page, { selectRawNucleotide: true });
+        await expect(searchPage.getAccessionField()).toHaveValue(testAccessionVersion);
 
-        expect(filePath).toBeTruthy();
+        await searchPage.clickResetButton();
+
+        await expect(searchPage.getAccessionField()).toHaveValue('');
     });
 });
