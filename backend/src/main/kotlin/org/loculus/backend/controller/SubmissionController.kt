331f8815848a11e0685ac4de4018769e9bae9ee5
Theo Sanderson <theo@theo.io>
2025-11-19 13:12:03 +0000

    backend try catch

--- backend/src/main/kotlin/org/loculus/backend/controller/SubmissionController.kt
+++ backend/src/main/kotlin/org/loculus/backend/controller/SubmissionController.kt
@@ -508,32 +508,29 @@ open class SubmissionController(
         MDC.put(REQUEST_ID_MDC_KEY, requestIdContext.requestId)
         MDC.put(ORGANISM_MDC_KEY, organism.name)
 
-        val outputStream = when (compressionFormat) {
-            CompressionFormat.ZSTD -> ZstdCompressorOutputStream(responseBodyStream)
-            null -> responseBodyStream
-        }
+        try {
+            val outputStream = when (compressionFormat) {
+                CompressionFormat.ZSTD -> ZstdCompressorOutputStream(responseBodyStream)
+                null -> responseBodyStream
+            }
 
-        outputStream.use { stream ->
-            transaction {
-                try {
+            outputStream.use { stream ->
+                transaction { // The Exposed transaction
                     iteratorStreamer.streamAsNdjson(sequenceProvider(), stream)
-                } catch (e: Exception) {
-                    val duration = System.currentTimeMillis() - startTime
-                    log.error(e) {
-                        "[$endpoint] An unexpected error occurred while streaming after ${duration}ms, aborting the stream: $e"
-                    }
-                    stream.write(
-                        "An unexpected error occurred while streaming, aborting the stream: ${e.message}".toByteArray(),
-                    )
                 }
             }
+
+            val duration = System.currentTimeMillis() - startTime
+            log.info { "[$endpoint] Streaming response completed in ${duration}ms" }
+
+        } catch (e: Exception) {
+            val duration = System.currentTimeMillis() - startTime
+            log.error(e) { "[$endpoint] An unexpected error occurred while streaming after ${duration}ms. Aborting and re-throwing to signal failure." }
+            throw e
+        } finally {
+            MDC.remove(REQUEST_ID_MDC_KEY)
+            MDC.remove(ORGANISM_MDC_KEY)
         }
-
-        val duration = System.currentTimeMillis() - startTime
-        log.info { "[$endpoint] Streaming response completed in ${duration}ms" }
-
-        MDC.remove(REQUEST_ID_MDC_KEY)
-        MDC.remove(ORGANISM_MDC_KEY)
     }
 
     fun parseFileMapping(fileMapping: String?, organism: Organism): SubmissionIdFilesMap? {
