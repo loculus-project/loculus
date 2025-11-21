b14c4c3aeaa65166b32035c5aa42219dd60d22a8
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-11-21 14:27:52 +0100

    fix

--- backend/src/main/kotlin/org/loculus/backend/model/SubmitModel.kt
+++ backend/src/main/kotlin/org/loculus/backend/model/SubmitModel.kt
@@ -142,7 +142,7 @@ class SubmitModel(
         submissionParams.files?.let { submittedFiles ->
             val fileSubmissionIds = submittedFiles.keys
             validateSubmissionIdSetsForFiles(metadataSubmissionIds, fileSubmissionIds)
-            validateFileExistenceAndGroupOwnership(submittedFiles, submissionParams, uploadId)
+            validateFileGroupOwnership(submittedFiles, submissionParams, uploadId)
         }
 
         if (submissionParams is SubmissionParams.OriginalSubmissionParams) {
@@ -369,7 +369,7 @@ class SubmitModel(
         }
     }
 
-    private fun validateFileExistenceAndGroupOwnership(
+    private fun validateFileGroupOwnership(
         submittedFiles: SubmissionIdFilesMap,
         submissionParams: SubmissionParams,
         uploadId: String,
@@ -377,6 +377,8 @@ class SubmitModel(
         log.debug {
             "Validating that submitted files belong to the group that their associated submission belongs to."
         }
+        val usedFileIds = submittedFiles.getAllFileIds()
+        val fileGroups = filesDatabaseService.getGroupIds(usedFileIds)
         if (submissionParams is SubmissionParams.OriginalSubmissionParams) {
             fileGroups.forEach {
                 if (it.value != submissionParams.groupId) {
