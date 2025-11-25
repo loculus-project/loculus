c6a0bc92800162ab9d294695843be2cd3e8c934f
Theo Sanderson <theo@theo.io>
2025-11-25 15:48:33 +0000

    update

--- website/tests/pages/sequences/accession.spec.ts
+++ website/tests/pages/sequences/accession.spec.ts
@@ -4,7 +4,6 @@ import { baseUrl, expect, test } from '../../e2e.fixture';
 import { getTestSequences } from '../../util/testSequenceProvider.ts';
 
 test.describe('The detailed sequence page', () => {
-
     test('check initial sequences and verify that banners are shown when revoked or revised', async ({
         sequencePage,
     }) => {
