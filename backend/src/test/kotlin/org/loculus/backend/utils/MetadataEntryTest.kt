0234736d624916b937756ec136566626f2c0cf25
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-11-27 20:23:07 +0100

    Add tests for duplicate detection

--- backend/src/test/kotlin/org/loculus/backend/utils/MetadataEntryTest.kt
+++ backend/src/test/kotlin/org/loculus/backend/utils/MetadataEntryTest.kt
@@ -222,6 +222,37 @@ class MetadataEntryTest {
         assert(exception.message!!.contains("record #2"))
         assert(exception.message!!.contains("foo2"))
     }
+
+    @Test
+    fun `test multiple duplicate fasta IDs are all reported`() {
+        val str = """
+            submissionId${'\t'}fastaId${'\t'}Country
+            foo${'\t'}seq1 seq2 seq1 seq2 seq3${'\t'}bar
+        """.trimIndent()
+        val inputStream = ByteArrayInputStream(str.toByteArray())
+        val exception = assertThrows<UnprocessableEntityException> {
+            metadataEntryStreamAsSequence(inputStream).toList()
+        }
+        assert(exception.message!!.contains("record #1"))
+        assert(exception.message!!.contains("foo"))
+        assert(exception.message!!.contains("duplicate fasta ids"))
+        assert(exception.message!!.contains("seq1"))
+        assert(exception.message!!.contains("seq2"))
+    }
+
+    @Test
+    fun `test duplicate detection works with maxSequencesPerEntry`() {
+        val str = """
+            submissionId${'\t'}fastaId${'\t'}Country
+            foo${'\t'}seq1 seq1${'\t'}bar
+        """.trimIndent()
+        val inputStream = ByteArrayInputStream(str.toByteArray())
+        val exception = assertThrows<UnprocessableEntityException> {
+            metadataEntryStreamAsSequence(inputStream, maxSequencesPerEntry = 3).toList()
+        }
+        assert(exception.message!!.contains("duplicate fasta ids"))
+        assert(exception.message!!.contains("seq1"))
+    }
 }
 
 class RevisionEntryTest {
@@ -353,5 +384,35 @@ class RevisionEntryTest {
         assert(entries.size == 1)
         assert(entries[0].submissionId == "foo")
         assert(entries[0].fastaIds == setOf("seq1"))
+    }
+
+    @Test
+    fun `test revision duplicate fasta IDs are rejected`() {
+        val str = """
+            submissionId${'\t'}accession${'\t'}fastaId${'\t'}Country
+            foo${'\t'}ACC123${'\t'}seq1 seq2 seq1${'\t'}bar
+        """.trimIndent()
+        val inputStream = ByteArrayInputStream(str.toByteArray())
+        val exception = assertThrows<UnprocessableEntityException> {
+            revisionEntryStreamAsSequence(inputStream).toList()
+        }
+        assert(exception.message!!.contains("record #1"))
+        assert(exception.message!!.contains("foo"))
+        assert(exception.message!!.contains("duplicate fasta ids"))
+        assert(exception.message!!.contains("seq1"))
+    }
+
+    @Test
+    fun `test revision duplicate detection works with maxSequencesPerEntry`() {
+        val str = """
+            submissionId${'\t'}accession${'\t'}fastaId${'\t'}Country
+            foo${'\t'}ACC123${'\t'}seq1 seq1${'\t'}bar
+        """.trimIndent()
+        val inputStream = ByteArrayInputStream(str.toByteArray())
+        val exception = assertThrows<UnprocessableEntityException> {
+            revisionEntryStreamAsSequence(inputStream, maxSequencesPerEntry = 3).toList()
+        }
+        assert(exception.message!!.contains("duplicate fasta ids"))
+        assert(exception.message!!.contains("seq1"))
     }
 }
