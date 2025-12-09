2238df3fc01c067eb18566f395c0192404f2ef51
maverbiest <59956362+maverbiest@users.noreply.github.com>
2025-12-09 07:57:19 +0100

    Update backend/src/main/kotlin/org/loculus/backend/api/GroupManagementTypes.kt
    Co-authored-by: Cornelius Roemer <cornelius.roemer@gmail.com>

--- backend/src/main/kotlin/org/loculus/backend/api/GroupManagementTypes.kt
+++ backend/src/main/kotlin/org/loculus/backend/api/GroupManagementTypes.kt
@@ -45,13 +45,7 @@ data class User(val name: String)
 
 data class GroupDetails(val group: Group, val users: List<User>) {
     fun returnPublicDetails(): GroupDetails {
-        val redactedGroup = Group(
-            groupId = this.group.groupId,
-            groupName = this.group.groupName,
-            institution = this.group.institution,
-            address = this.group.address,
-            contactEmail = "N/A",
-        )
+        val redactedGroup = group.copy(contactEmail = "N/A")
 
         return GroupDetails(redactedGroup, this.users)
     }
