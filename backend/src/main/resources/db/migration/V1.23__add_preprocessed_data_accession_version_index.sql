ab466e8e386e7ba1b8e9a296d318a9cc7b0e23e6
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-12-12 15:49:00 +0100

    Put all new indexes into a single migration

--- backend/src/main/resources/db/migration/V1.23__add_preprocessed_data_accession_version_index.sql
+++
@@ -1,4 +0,0 @@
--- Add index on (accession, version) without pipeline_version for faster lookups
--- This improves query performance on sequence_entries_view which joins on these columns
-CREATE INDEX IF NOT EXISTS sequence_entries_preprocessed_data_accession_version_idx
-ON sequence_entries_preprocessed_data (accession, version);
