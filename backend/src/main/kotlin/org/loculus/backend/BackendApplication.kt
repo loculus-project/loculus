f273ddde7979292cb65e22283b1fcbcdad9c8af5
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-12-17 18:33:35 +0100

    Apply suggestions from code review

--- backend/src/main/kotlin/org/loculus/backend/BackendApplication.kt
+++ backend/src/main/kotlin/org/loculus/backend/BackendApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(
+    // TODO(#5754) Remove shim once exposed supports spring boot 4
     excludeName = ["org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration"],
 )
 class BackendApplication
