cc09ece797b91e34af9e08f61c8d0bba741ed8c0
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-12-17 10:47:11 +0100

    chore(silo-import): better logging of failed downloads (#5731)
    The error was cryptic when backend errored. This should be better.
    
    Previously this was the error (instead of reporting bad status code):
    
    ```
    Traceback (most recent call last):
      File "/usr/local/lib/python3.11/site-packages/silo_import/runner.py", line 105, in run_forever
        runner.run_once()
      File "/usr/local/lib/python3.11/site-packages/silo_import/runner.py", line 54, in run_once
        download = self.download_manager.download_release(self.config, self.paths, last_etag)
                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/local/lib/python3.11/site-packages/silo_import/download_manager.py", line 155, in download_release
        raise RuntimeError(msg)
    RuntimeError: Response headers: {'x-request-id': '62954dca-fda1-48a8-9c2d-2ba40f8b8b2c', 'vary': 'Origin, Access-Control-Request-Method, Access-Control-Request-Headers', 'x-content-type-options': 'nosniff', 'x-xss-protection': '0', 'cache-control': 'no-cache, no-store, max-age=0, must-revalidate', 'pragma': 'no-cache', 'expires': '0', 'x-frame-options': 'DENY', 'content-type': 'application/problem+json', 'transfer-encoding': 'chunked', 'date': 'Tue, 16 Dec 2025 18:54:54 GMT', 'connection': 'close'} did not contain an ETag header
    ```

--- silo-import/src/silo_import/download_manager.py
+++ silo-import/src/silo_import/download_manager.py
@@ -6,6 +6,7 @@ import logging
 import time
 from collections.abc import Callable
 from dataclasses import dataclass
+from http.client import BAD_REQUEST
 from pathlib import Path
 
 import requests
@@ -141,6 +142,16 @@ class DownloadManager:
                 last_etag,
                 300,
             )
+
+            if response.status_code >= BAD_REQUEST:
+                msg = (
+                    f"Failed to download data: HTTP {response.status_code}."
+                    f"Headers: {response.headers} Body: "
+                    + data_path.read_text(encoding="utf-8", errors="replace")
+                    if data_path.exists()
+                    else "missing"
+                )
+                raise RuntimeError(msg)
 
             # Check for 304 Not Modified
             if response.status_code == 304:  # noqa: PLR2004
