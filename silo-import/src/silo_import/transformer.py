345cf808d45f0db8fd3bb8b36e9d34bcf6dd01ec
anna-parker <50943381+anna-parker@users.noreply.github.com>
2025-12-17 12:34:23 +0100

    testing

--- silo-import/src/silo_import/transformer.py
+++ silo-import/src/silo_import/transformer.py
@@ -8,7 +8,7 @@ def transform_data_format(data_path, transformed_path):
 
     cmd = (
         f"zstdcat {data_path} | "
-        f"../legacyNdjsonTransformer/target/release/legacy-ndjson-transformer | "
+        f"/usr/local/bin/legacy-ndjson-transformer | "
         f"zstd > {transformed_path}"
     )
 
