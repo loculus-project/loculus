2890e906728fcdae2511785c052e7f70012b3117
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-01-21 11:05:25 +0100

    fix imports

--- website/src/components/SearchPage/SearchForm.spec.tsx
+++ website/src/components/SearchPage/SearchForm.spec.tsx
@@ -11,7 +11,7 @@ import { MetadataFilterSchema, MetadataVisibility } from '../../utils/search.ts'
 import {
     MULTI_SEG_SINGLE_REF_REFERENCEGENOMES,
     SINGLE_SEG_SINGLE_REF_REFERENCEGENOMES,
-} from '../../types/referenceGenomesInfo.spec.ts';
+} from '../../types/referenceGenomes.spec.ts';
 
 global.ResizeObserver = class FakeResizeObserver implements ResizeObserver {
     observe() {}
