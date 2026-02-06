12796e4e2aae975f87bdbf040445290c3ac88ee0
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-02-06 12:56:42 +0100

    format

--- integration-tests/tests/specs/features/search/active-filters.dependent.spec.ts
+++ integration-tests/tests/specs/features/search/active-filters.dependent.spec.ts
@@ -39,8 +39,8 @@ test.describe('Search', () => {
     test('multi-segment mutation filter can be added and removed', async ({ page }) => {
         const mutation = 'S:G100A';
         await searchPage.cchf();
-        const segment = page.locator('details', { hasText: 'S' })
-        await segment.locator('summary').click()
+        const segment = page.locator('details', { hasText: 'S' });
+        await segment.locator('summary').click();
         await searchPage.enterMutation(mutation);
         await expect(page.getByText(`mutation:${mutation}`)).toBeVisible();
         await page.getByLabel('remove filter').click();
