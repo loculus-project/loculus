1e8897260f7396f7c02fa69a9f112f1a00451d29
maverbiest <maxaverbiest@gmail.com>
2026-02-19 16:20:22 +0100

    Remove default value for target_file

--- preprocessing/ncbi_tax_download/src/__main__.py
+++ preprocessing/ncbi_tax_download/src/__main__.py
@@ -16,7 +16,7 @@ def run() -> None:
 
     try:
         archive = download_ncbi_archive()
-        df_names = extract_ncbi_taxonomy_file(archive)
+        df_names = extract_ncbi_taxonomy_file(archive, "names.dmp")
         df_names_reformat = format_names_df(df_names)
         df_names_to_sqlite(df_names_reformat, "tests/ncbi_scientific_names.sql")
     except:
