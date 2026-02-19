ec6d53e79c4301784d3d6aa7e5aa1137a083db7a
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-02-19 18:02:27 +0100

    testing some more

---
+++ website/src/components/SearchPage/fields/ReferencePresetsField.tsx
@@ -0,0 +1,55 @@
+import { forwardRef, type FocusEventHandler } from 'react';
+
+import { TextField } from './TextField.tsx';
+import type { FieldValueUpdate, MetadataFilter, SetSomeFieldValues } from '../../../types/config.ts';
+import DisabledUntilHydrated from '../../DisabledUntilHydrated.tsx';
+
+export type FieldPresetMap = Record<string, Record<string, string>>;
+
+export type ReferencePresetsFieldProps = {
+  field: MetadataFilter;
+  setSomeFieldValues: SetSomeFieldValues;
+  multiline?: boolean;
+  onFocus?: FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  onBlur?: FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  fieldValue: string | number;
+  type?: 'string' | 'boolean' | 'float' | 'int' | 'authors';
+  fieldPresets?: FieldPresetMap;
+};
+
+export const ReferencePresetsField = forwardRef<HTMLInputElement, ReferencePresetsFieldProps>(
+  (props, ref) => {
+    const { field, setSomeFieldValues, multiline, onFocus, onBlur, fieldValue, fieldPresets } = props;
+
+    return (
+      <DisabledUntilHydrated>
+        <TextField
+          label={field.displayName ?? field.name}
+          type={field.type}
+          fieldValue={fieldValue}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onChange={(e) => {
+            const nextValue = e.target.value;
+
+            // Always update the edited field
+            const updates: FieldValueUpdate[] = [[field.name, nextValue]];
+
+            // If there are presets for this field name, apply them
+            const preset = fieldPresets?.[field.name];
+            if (preset) {
+              updates.push(
+                ...Object.entries(preset).map(([k, v]) => [k, v] as FieldValueUpdate)
+              );
+            }
+
+            setSomeFieldValues(...updates);
+          }}
+          autoComplete="off"
+          multiline={multiline}
+          ref={ref}
+        />
+      </DisabledUntilHydrated>
+    );
+  }
+);
