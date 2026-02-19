0967a6237d68065c52700ccaee9ec8bded189e11
maverbiest <maxaverbiest@gmail.com>
2026-02-19 11:48:20 +0100

    Initial logic for downloading NCBI scientific names and storing in sqlite db

---
+++ preprocessing/ncbi_tax_download/src/__init__.py
@@ -0,0 +1,1 @@
+from . import ncbi as ncbi
---
+++ preprocessing/ncbi_tax_download/src/__main__.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+from .ncbi import (
+    df_names_to_sqlite,
+    download_ncbi_archive,
+    extract_ncbi_taxonomy_file,
+    format_names_df,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def run() -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        archive = download_ncbi_archive()
+        df_names = extract_ncbi_taxonomy_file(archive)
+        df_names_reformat = format_names_df(df_names)
+        df_names_to_sqlite(df_names_reformat, "tests/ncbi_scientific_names.sql")
+    except:
+        logger.exception("NCBI taxonomy download pipeline failed")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    run()
