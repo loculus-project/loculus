a7b5b1c13e1ba25e106bdf91c9c4a607b45701d9
maverbiest <maxaverbiest@gmail.com>
2026-02-20 16:52:47 +0100

    Restructure and make installable, add test

--- ncbi_tax_download/src/ncbi_tax_download/__main__.py
+++ ncbi_tax_download/src/ncbi_tax_download/__main__.py
@@ -1,4 +1,6 @@
+import argparse
 import logging
+from pathlib import Path
 import sys
 
 from .ncbi import (
@@ -11,8 +13,24 @@ from .ncbi import (
 logger = logging.getLogger(__name__)
 
 
+def parse_cl() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-o", "--output_db", required=True, type=Path, help="Name to use for the output database"
+    )
+
+    return parser.parse_args()
+
+
 def run() -> None:
     logging.basicConfig(level=logging.INFO)
+
+    args = parse_cl()
+    # output_path = Path(args.output_db)
+    parent = args.output_db.parent if args.output_db.parent != Path("") else Path(".")
+    if not parent.exists():
+        raise FileNotFoundError(f"directory {parent} does not exist.")
 
     try:
         archive = download_ncbi_archive()
@@ -24,7 +42,7 @@ def run() -> None:
         )
         df_nodes = df_nodes.drop("rank_level", axis=1)
 
-        write_to_sqlite(df_names, df_nodes, "tests/ncbi_scientific_names.sqlite")
+        write_to_sqlite(df_names, df_nodes, args.output_db)
     except:
         logger.exception("NCBI taxonomy download pipeline failed")
         sys.exit(1)
