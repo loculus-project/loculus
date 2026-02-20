3fd87df2386248e7d06b633817fd4e703d0608c9
maverbiest <maxaverbiest@gmail.com>
2026-02-20 15:16:11 +0100

    Also parse nodes.dmp to get parent info

--- preprocessing/ncbi_tax_download/src/ncbi.py
+++ preprocessing/ncbi_tax_download/src/ncbi.py
@@ -11,6 +11,64 @@ logger = logging.getLogger(__name__)
 
 NCBI_URL = "https://ftp.ncbi.nih.gov/pub/taxonomy/"
 TAXONOMY_ARCHIVE = "taxdmp.zip"
+# Ordering of ranks in nodes.dmp as per ChatGPT -- not authoritative
+NCBI_RANK_ORDER = {
+    # we will order from specific -> general, so
+    # setting `no rank` to -1 means it will always come last
+    "no rank": -1,
+    # very high-level / viral and cellular roots
+    "acellular root": 1,
+    "cellular root": 2,
+    "root": 3,  # sometimes appears in NCBI
+    "superkingdom": 4,  # potentially, superkingdom and domain are synonyms in NCBI
+    "domain": 5,
+    "kingdom": 6,
+    "subkingdom": 7,
+    "superphylum": 8,
+    "phylum": 9,
+    "subphylum": 10,
+    "superclass": 11,
+    "class": 12,
+    "subclass": 13,
+    "infraclass": 14,
+    "cohort": 15,
+    "subcohort": 16,
+    "superorder": 17,
+    "order": 18,
+    "suborder": 19,
+    "infraorder": 20,
+    "parvorder": 21,
+    "superfamily": 22,
+    "family": 23,
+    "subfamily": 24,
+    "tribe": 25,
+    "subtribe": 26,
+    "genus": 27,
+    "subgenus": 28,
+    # between genus and species
+    "section": 29,
+    "subsection": 30,
+    "series": 31,
+    "species group": 32,
+    "species subgroup": 33,
+    # species level and below
+    "species": 34,
+    "subspecies": 35,
+    "varietas": 36,
+    "subvariety": 37,
+    "forma": 38,
+    "forma specialis": 39,
+    "strain": 40,
+    "isolate": 41,
+    "genotype": 42,
+    "serogroup": 43,
+    "serotype": 44,
+    "biotype": 45,
+    "pathogroup": 46,
+    "morph": 47,
+    # informal / flexible
+    # "clade": 48,
+}
 
 
 def download_ncbi_archive(
@@ -44,20 +102,60 @@ def extract_ncbi_taxonomy_file(archive: io.BytesIO, target_file: str) -> pd.Data
     return df
 
 
-def format_names_df(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.rename(columns={0: "tax_id", 1: "name_txt", 3: "name_class"}).loc[
-        :, ["tax_id", "name_txt", "name_class"]
-    ]
-    return (
-        df.loc[df["name_class"] == "scientific name", ["tax_id", "name_txt"]]
-        .drop_duplicates()
-        .reset_index(drop=True)
+def extract_names_df(archive: io.BytesIO) -> pd.DataFrame:
+    df = (
+        extract_ncbi_taxonomy_file(archive, "names.dmp")
+        .rename(columns={0: "tax_id", 1: "name_txt", 3: "name_class"})
+        .loc[:, ["tax_id", "name_txt", "name_class"]]
     )
 
+    df = df[df["name_class"].isin({"scientific name", "common name", "genbank common name"})]
+    df["name_class"] = (
+        df["name_class"].replace("genbank common name", "common name").astype("category")
+    )
 
-def df_names_to_sqlite(df: pd.DataFrame, output_db: str) -> None:
+    df_wide = (
+        df.pivot_table(
+            index="tax_id",
+            values="name_txt",
+            columns="name_class",
+            aggfunc=lambda x: "; ".join(sorted(x)),
+        )
+        .reset_index()
+        .rename(columns={"scientific name": "scientific_name", "common name": "common_name"})
+    )
+
+    return df_wide
+
+
+def extract_nodes_df(archive: io.BytesIO) -> pd.DataFrame:
+    df = (
+        extract_ncbi_taxonomy_file(archive, "nodes.dmp")
+        .rename(columns={0: "tax_id", 1: "parent_id", 2: "rank"})
+        .astype({"tax_id": "int", "parent_id": "int", "rank": "category"})
+        # entries with no/unrecognized ranks get assigned -1 -> 'no rank'
+        .assign(rank_level=lambda x: x["rank"].map(NCBI_RANK_ORDER).fillna(-1))
+        .loc[:, ["tax_id", "parent_id", "rank_level"]]
+    )
+
+    return df
+
+
+def write_to_sqlite(df_names: pd.DataFrame, df_nodes: pd.DataFrame, output_db: str) -> None:
     logger.info(f"saving NCBI taxonomic names to {output_db}")
+
+    df_rank_levels = pd.DataFrame(list(NCBI_RANK_ORDER.items()), columns=["rank", "rank_level"])
+
     with sqlite3.connect(output_db) as conn:
-        df.to_sql("scientific_names", conn, if_exists="replace", index=False)
-        conn.execute("CREATE INDEX IF NOT EXISTS idx_name_txt ON scientific_names(name_txt);")
-    logger.info("successfully wrote scientific_names table")
+        df_names.to_sql("tax_id_names_table", conn, if_exists="replace", index=False)
+        conn.execute(
+            "CREATE INDEX IF NOT EXISTS idx_scientific_name ON tax_id_names_table(scientific_name);"
+        )
+        conn.execute("CREATE INDEX IF NOT EXISTS idx_tax_id_names ON tax_id_names_table(tax_id);")
+
+        df_nodes.to_sql("parent_mappings_table", conn, if_exists="replace", index=False)
+        conn.execute("CREATE INDEX IF NOT EXISTS idx_tax_id_pmap ON parent_mappings_table(tax_id);")
+
+        df_rank_levels.to_sql("rank_levels_table", conn, if_exists="replace", index=False)
+
+    logger.info("successfully created database")
