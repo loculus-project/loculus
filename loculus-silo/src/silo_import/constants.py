f840f9cabd9c60710d64abc5fa73e296498857aa
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-02-25 15:17:46 +0100

    remove unused constants

--- loculus-silo/src/silo_import/constants.py
+++ loculus-silo/src/silo_import/constants.py
@@ -8,4 +8,3 @@ SPECIAL_ETAG_NONE = "0"
 # File names for downloaded data
 DATA_FILENAME = "untransformed_data.ndjson.zst"
 TRANSFORMED_DATA_FILENAME = "data.ndjson.zst"  # name is set by SILO
-LINEAGES_FILENAME = "lineage_definitions.yaml"  # default name if no lineage definition map provided
