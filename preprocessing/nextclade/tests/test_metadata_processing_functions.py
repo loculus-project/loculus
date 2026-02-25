bb5d1f7d7f3d49bfba2121ed6594fa2dab832433
maverbiest <maxaverbiest@gmail.com>
2026-02-25 08:19:12 +0100

    Extend test

--- preprocessing/nextclade/tests/test_metadata_processing_functions.py
+++ preprocessing/nextclade/tests/test_metadata_processing_functions.py
@@ -912,7 +912,7 @@ def test_concatenate() -> None:
     args: FunctionArgs = {
         "accession_version": "version.1",
         "order": ["someInt", "geoLocCountry", "ACCESSION_VERSION", "sampleCollectionDate"],
-        "type": ["integer", "string", "ACCESSION_VERSION", "string"],
+        "type": ["integer", "string", "ACCESSION_VERSION", "date"],
     }
 
     res_no_fallback_no_int = ProcessingFunctions.concatenate(
@@ -938,9 +938,18 @@ def test_concatenate() -> None:
         args,
     )
 
-    assert res_no_fallback_no_int.datum == "version.1/2025"
-    assert res_no_fallback.datum == "0//version.1/2025"
-    assert res_fallback.datum == "0/unknown/version.1/2025"
+    input_data["sampleCollectionDate"] = None
+    res_fallback_explicit_null = ProcessingFunctions.concatenate(
+        input_data,
+        output_field,
+        input_fields,
+        args,
+    )
+
+    assert res_no_fallback_no_int.datum == "version.1/2025-01-01"
+    assert res_no_fallback.datum == "0//version.1/2025-01-01"
+    assert res_fallback.datum == "0/unknown/version.1/2025-01-01"
+    assert res_fallback_explicit_null.datum == "0/unknown/version.1/unknown"
 
 
 if __name__ == "__main__":
