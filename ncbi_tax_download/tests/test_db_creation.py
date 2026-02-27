2dcc481e9718e4f0c49cdc13ad4d3865177b9a6e
maverbiest <maxaverbiest@gmail.com>
2026-02-27 09:26:57 +0100

    Improve tests

--- ncbi_tax_download/tests/test_db_creation.py
+++ ncbi_tax_download/tests/test_db_creation.py
@@ -2,7 +2,6 @@ from io import BytesIO
 from ncbi_tax_download.ncbi import (
     create_taxonomy_df,
     extract_names_df,
-    extract_ncbi_taxonomy_file,
     extract_nodes_df,
 )
 
@@ -16,11 +15,9 @@ def test_names_df_creation():
 
     expected_shape = (14, 3)
     expected_columns = ["tax_id", "common_name", "scientific_name"]
-    expected_common_name = "bacteria; eubacteria"
 
     assert df_names.shape == expected_shape
     assert all([i == j for i, j in zip(df_names.columns, expected_columns)])
-    assert df_names.set_index("tax_id").loc[2, "common_name"] == expected_common_name
 
 
 def test_nodes_df_creation():
@@ -47,8 +44,13 @@ def test_taxonomy_df_creation():
 
     expected_shape = (7, 5)
     expected_columns = ["tax_id", "common_name", "scientific_name", "parent_id", "depth"]
-    expected_children = [10, 11]
+    expected_partents = [1, 1, 2, 6, 1, 9, 9]
+    expected_depths = [0, 1, 2, 3, 1, 2, 2]
+    expected_common_name = "bacteria; eubacteria"
 
     assert df_taxonomy.shape == expected_shape
     assert all([i == j for i, j in zip(df_taxonomy.columns, expected_columns)])
-    assert all(df_taxonomy.loc[df_taxonomy["parent_id"] == 9, "tax_id"] == expected_children)
+
+    assert all(df_taxonomy["parent_id"] == expected_partents)
+    assert all(df_taxonomy["depth"] == expected_depths)
+    assert df_taxonomy.set_index("tax_id").loc[2, "common_name"] == expected_common_name
