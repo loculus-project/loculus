6db5ae28051acd1e93c9bd26e1a4453f5c2b9c7b
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-03-06 11:25:46 +0100

    testing

--- website/src/components/SequenceDetailsPage/DataTableEntry.tsx
+++ website/src/components/SequenceDetailsPage/DataTableEntry.tsx
@@ -17,7 +17,7 @@ const DataTableComponent: React.FC<Props> = ({ data, dataUseTermsHistory, segmen
             {type.kind === 'metadata' && (
                 <div className='text-sm grid my-1' style={{ gridTemplateColumns: '200px 1fr' }}>
                     <div className='font-medium text-gray-900 break-inside-avoid pr-4'>{label}</div>
-                    <DataTableEntryValue data={data} dataUseTermsHistory={dataUseTermsHistory} />
+                    <DataTableEntryValue data={data} dataUseTermsHistory={dataUseTermsHistory} segmentDisplayNameMap={segmentDisplayNameMap}/>
                 </div>
             )}
 
