66a395d345136c551475b7cf46b0e090947d77ea
tombch <t.brier@outlook.com>
2026-03-11 13:48:41 +0100

    Formatting

--- website/src/config.ts
+++ website/src/config.ts
@@ -74,7 +74,7 @@ export function getContactConfig(websiteConfig: WebsiteConfig) {
     return {
         gitHubIssuesUrl: websiteConfig.gitHubIssuesUrl,
         issuesEmail: websiteConfig.issuesEmail,
-    }
+    };
 }
 
 /**
