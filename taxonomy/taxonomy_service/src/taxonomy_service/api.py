34d14009d13dfbd739e6e8bf0843007936085344
maverbiest <maxaverbiest@gmail.com>
2026-03-17 10:15:44 +0100

    Clean up api

--- taxonomy/taxonomy_service/src/taxonomy_service/api.py
+++ taxonomy/taxonomy_service/src/taxonomy_service/api.py
@@ -1,7 +1,8 @@
 import logging
 import sqlite3
+from typing import Annotated
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 import uvicorn
 
 from .config import Config
@@ -11,70 +12,104 @@ logger = logging.getLogger()
 app = FastAPI(title="Taxonomy service", description="Loculus taxonomy API for hostname validation")
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Taxonomy service is running"}
+def get_db_connection():
+    conn = sqlite3.connect(app.state.config.db_path)
+    conn.row_factory = sqlite3.Row
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 
-def fetch_by_sci_name(db_path: str, name: str):
-    with sqlite3.connect(db_path) as conn:
-        conn.row_factory = sqlite3.Row
-        taxa = conn.execute("SELECT * FROM taxonomy WHERE scientific_name = ?", (name,)).fetchall()
-
-        if not taxa:
-            return None
-
-        return dict(max(taxa, key=lambda x: x["depth"]))
+DbConnection = Annotated[sqlite3.Connection, Depends(get_db_connection)]
 
 
-def fetch_by_id(db_path: str, tax_id: int):
-    with sqlite3.connect(db_path) as conn:
-        conn.row_factory = sqlite3.Row
-        taxon = conn.execute("SELECT * FROM taxonomy WHERE tax_id = ?", (tax_id,)).fetchone()
-        if not taxon:
-            return None
-        return dict(taxon)
+def fetch_by_common_name(
+    db_conn: sqlite3.Connection, name: str
+) -> dict[str, str | int | None] | None:
+    """This would be a bit complicated as there are often multiple common names
+    associtated with a single taxon.
+
+    We store these as a '; ' separated string for each taxon entry, so we'd have to figure out
+    how to do a substring search on that.
+    """
+    raise NotImplementedError
 
 
-def fetch_common_name(db_path: str, tax_id: int):
+def fetch_by_sci_name(db_conn: sqlite3.Connection, name: str) -> dict[str, str | int | None] | None:
+    """Check if a scientific name exists in the taxonomy and, if so, return the taxon
+
+    args:
+        db_conn (sqlite3.Connection):   connection to a database. The caller is responsible for closing it
+        name (str):                     scientific name to query the database with
+    """
+    taxa = db_conn.execute("SELECT * FROM taxonomy WHERE scientific_name = ?", (name,)).fetchall()
+
+    if not taxa:
+        return None
+
+    return dict(max(taxa, key=lambda x: x["depth"]))
+
+
+def fetch_by_id(db_conn: sqlite3.Connection, tax_id: int) -> dict[str, str | int | None] | None:
+    """Return the taxon associated with `tax_id`. Return None if `tax_id` does not exist in the DB
+
+    args:
+        db_conn (sqlite3.Connection):   connection to a database. The caller is responsible for closing it
+        tax_id (int):                   NCBI taxon ID to query the database with
+    """
+    taxon = db_conn.execute("SELECT * FROM taxonomy WHERE tax_id = ?", (tax_id,)).fetchone()
+    if not taxon:
+        return None
+    return dict(taxon)
+
+
+def fetch_common_name(
+    db_conn: sqlite3.Connection, tax_id: int
+) -> dict[str, str | int | None] | None:
     """Return a taxon with a common name
 
     If the supplied `tax_id` is associated with a common name, return the taxon.
 
     If there is no common name associated with the taxon, keep stepping up the taxonomy until
     a taxon is found with a common name, and return that
-    """
 
-    with sqlite3.connect(db_path) as conn:
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
-        while tax_id > 0:
-            taxon = cursor.execute("SELECT * FROM taxonomy WHERE tax_id = ?", (tax_id,)).fetchone()
-            if taxon is None:
-                raise HTTPException(status_code=404, detail=f"Taxon {tax_id} not found")
-            elif taxon["common_name"] is not None:
-                return dict(taxon)
-            tax_id = taxon["parent_id"]
-        return None
+    args:
+        db_conn (sqlite3.Connection):   connection to a database. The caller is responsible for closing it
+        tax_id (int):                   NCBI taxon ID of the taxon for which to find a common name
+    """
+    cursor = db_conn.cursor()
+    while tax_id > 0:
+        taxon = cursor.execute("SELECT * FROM taxonomy WHERE tax_id = ?", (tax_id,)).fetchone()
+        if taxon is None:
+            raise HTTPException(status_code=404, detail=f"Taxon {tax_id} not found")
+        elif taxon["common_name"] is not None:
+            return dict(taxon)
+        tax_id = taxon["parent_id"]
+    return None
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"message": "Taxonomy service is running"}
 
 
 @app.get("/taxa")
-def query_taxa(scientific_name: str):
-    """Very basic for the moment, only support scientific name queries.
+def query_taxa(name: str, db: DbConnection) -> dict[str, str | int | None]:
+    """Given a name, try to find a taxon associated with it.
 
-    Could consider adding support for common name queries as well.
+    We only check if `name` is a scientific name for the moment.
     """
-    taxon = fetch_by_sci_name(app.state.config.db_path, scientific_name)
-
+    taxon = fetch_by_sci_name(db, name)
     if taxon is None:
-        raise HTTPException(status_code=404, detail=f"'{scientific_name}' not found")
+        raise HTTPException(status_code=404, detail=f"'{name}' not found")
 
     return taxon
 
 
 @app.get("/taxa/{tax_id}")
-def get_taxon(tax_id: int):
-    taxon = fetch_by_id(app.state.config.db_path, tax_id)
+def get_taxon(tax_id: int, db: DbConnection) -> dict[str, str | int | None]:
+    taxon = fetch_by_id(db, tax_id)
 
     if taxon is None:
         raise HTTPException(status_code=404, detail=f"'{tax_id}' not found")
@@ -83,8 +118,8 @@ def get_taxon(tax_id: int):
 
 
 @app.get("/taxa/{tax_id}/common_name")
-def get_common_name(tax_id: int):
-    taxon = fetch_common_name(app.state.config.db_path, tax_id)
+def get_common_name(tax_id: int, db: DbConnection) -> dict[str, str | int | None]:
+    taxon = fetch_common_name(db, tax_id)
 
     if taxon is None:
         raise HTTPException(
