dea104c81c1c89164d7c15c9dc43c1f93e47f13e
maverbiest <maxaverbiest@gmail.com>
2026-03-18 10:15:37 +0100

    More tests, fix bug in fetch_common_name

--- taxonomy/taxonomy_service/test/test_api.py
+++ taxonomy/taxonomy_service/test/test_api.py
@@ -6,13 +6,48 @@ from unittest.mock import MagicMock, Mock, patch
 from fastapi.testclient import TestClient
 from requests import codes
 
-from taxonomy_service.api import app, get_db_connection
-from taxonomy_service.config import Config
+from taxonomy_service.api import app, get_db_connection, init_app
+from taxonomy_service.config import Config, get_config
 
 
 client = TestClient(app)
 
 config_file = Path(__file__).parent / "test_config.yaml"
+
+
+mock_taxa = {
+    "Homo sapiens": {
+        "tax_id": 9606,
+        "common_name": None,
+        "scientific_name": "Homo sapiens",
+        "parent_id": 9605,
+        "depth": 31,
+    },
+    "Homo": {
+        "tax_id": 9605,
+        "common_name": "humans",
+        "scientific_name": "Homo",
+        "parent_id": 207598,
+        "depth": 30,
+    },
+    "cellular organisms": {
+        "tax_id": 131567,
+        "common_name": None,
+        "scientific_name": "cellular organisms",
+        "parent_id": 1,
+        "depth": 1,
+    },
+    "root": {
+        "tax_id": 1,
+        "common_name": None,
+        "scientific_name": "root",
+        "parent_id": 1,
+        "depth": 0,
+    },
+}
+
+mock_missing_taxon = 123
+mock_missing_name = "Nonsense nonsensi"
 
 
 def get_test_db():
@@ -21,7 +56,8 @@ def get_test_db():
     conn.execute(
         "CREATE TABLE taxonomy (tax_id INTEGER, common_name TEXT, scientific_name TEXT, parent_id INTEGER, depth INTEGER)"
     )
-    conn.execute("INSERT INTO taxonomy VALUES (9606, 'human', 'Homo sapiens', 9605, 31)")
+    data = [tuple(v.values()) for v in mock_taxa.values()]
+    conn.executemany("INSERT INTO taxonomy VALUES (?, ?, ?, ?, ?)", data)
     conn.commit()
     try:
         yield conn
@@ -29,35 +65,44 @@ def get_test_db():
         conn.close()
 
 
-app.dependency_overrides[get_db_connection] = get_test_db
-
-mock_taxon = {
-    "tax_id": 9606,
-    "common_name": "human",
-    "scientific_name": "Homo sapiens",
-    "parent_id": 9605,
-    "depth": 31,
-}
-
-mock_missing_taxon = 123
-mock_missing_name = "Nonsense nonsensi"
-
-
 class ApiTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config: Config = get_config(config_file)
+        if self.config.db_path == ":memory:":
+            app.dependency_overrides[get_db_connection] = get_test_db
+
     def test_get_taxon_success(self):
-        response = client.get(f"/taxa/{mock_taxon['tax_id']}")
+        taxon = mock_taxa["Homo sapiens"]
+        response = client.get(f"/taxa/{taxon['tax_id']}")
         assert response.status_code == codes.ok
-        assert response.json()["scientific_name"] == mock_taxon["scientific_name"]
+        assert response.json()["scientific_name"] == taxon["scientific_name"]
 
     def test_get_taxon_not_found(self):
         response = client.get(f"/taxa/{mock_missing_taxon}")
         assert response.status_code == codes.not_found
 
     def test_query_taxon_succes(self):
-        response = client.get(f"/taxa?name={mock_taxon['scientific_name']}")
+        taxon = mock_taxa["Homo sapiens"]
+        response = client.get(f"/taxa?name={taxon['scientific_name']}")
         assert response.status_code == codes.ok
-        assert response.json()["scientific_name"] == mock_taxon["scientific_name"]
+        assert response.json()["scientific_name"] == taxon["scientific_name"]
 
     def test_query_taxon_not_found(self):
         response = client.get(f"/taxa?name={mock_missing_name}")
         assert response.status_code == codes.not_found
+
+    def test_get_common_name_success(self):
+        homo_sapiens = mock_taxa["Homo sapiens"]
+        homo = mock_taxa["Homo"]
+        response = client.get(f"/taxa/{homo_sapiens['tax_id']}/common_name")
+        assert response.status_code == codes.ok
+        assert response.json()["tax_id"] == homo["tax_id"]
+
+    def test_get_common_name_not_found(self):
+        cellular_organisms = mock_taxa["cellular organisms"]
+        response = client.get(f"/taxa/{cellular_organisms['tax_id']}/common_name")
+        assert response.status_code == codes.not_found
+        assert (
+            response.json()["detail"]
+            == f"Unable to find common name for taxon {cellular_organisms['tax_id']}"
+        )
