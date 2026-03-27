dd4f0d75828e8a3f59d4cea606ba10b980334ae4
maverbiest <maxaverbiest@gmail.com>
2026-03-27 16:04:23 +0100

    Typo

--- preprocessing/nextclade/src/loculus_preprocessing/processing_functions.py
+++ preprocessing/nextclade/src/loculus_preprocessing/processing_functions.py
@@ -1366,7 +1366,7 @@ class ProcessingFunctions:
             # if this record was ingested from ISNDC, we trust that they validated the hostNameScientific
             # and associated it with the proper hostTaxonId
             return ProcessingResult(
-                datum=input_data.get("hostTaxId"),
+                datum=input_data.get("hostTaxonId"),
                 warnings=[],
                 errors=[],
             )
