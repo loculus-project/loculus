ec1eea752cc5f6e19bf5a9b4e8c7933feea5c74e
tombch <t.brier@outlook.com>
2026-04-07 10:48:44 +0200

    Better spacing

--- website/src/components/SeqSetCitations/SeqSetItem.tsx
+++ website/src/components/SeqSetCitations/SeqSetItem.tsx
@@ -190,7 +190,7 @@ const SeqSetItemInner: FC<SeqSetItemProps> = ({
             </div>
             <SeqSetSectionSeparator />
             <SeqSetSection title='Statistics'>
-                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
                     <DatePlot
                         data={collectionDatesData}
                         description={`Sample collection dates for ${seqSetAccessionVersion} sequences`}
