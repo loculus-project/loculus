1ab676fcabb1ed06f10930e0b19a61c2982a2e6d
tombch <t.brier@outlook.com>
2026-04-09 16:52:13 +0200

    Sections for input and generated fields

--- website/src/components/OrganismMetadataTable/OrganismMetadataTable.tsx
+++ website/src/components/OrganismMetadataTable/OrganismMetadataTable.tsx
@@ -18,31 +18,27 @@ enum FieldType {
 
 export const OrganismMetadataTable: FC<Props> = ({ organism }) => {
     const [activeTab, setActiveTab] = useState<FieldType>(FieldType.INPUT);
-    const [expandedHeaders, setExpandedHeaders] = useState(new Set(Array.from(organism.groupedInputFields.keys())));
 
-    const toggleHeader = (header: string) => {
-        const updatedExpandedHeaders = new Set(expandedHeaders);
-        if (updatedExpandedHeaders.has(header)) {
-            updatedExpandedHeaders.delete(header); // Close the table if already expanded
-        } else {
-            updatedExpandedHeaders.add(header);
-        }
-        setExpandedHeaders(updatedExpandedHeaders);
-    };
+    const groupedGeneratedFields = useMemo(() => {
+        const groupedFields = new Map<string, Metadata[]>();
+        const inputFieldNames = new Set(
+            Array.from(organism.groupedInputFields.values())
+                .flat()
+                .map((field) => field.name),
+        );
 
-    const generatedFields = useMemo(
-        () =>
-            organism.metadata.filter(
-                (field) =>
-                    !Array.from(organism.groupedInputFields.values())
-                        .flat()
-                        .some((inputField) => inputField.name === field.name),
-            ),
-        [organism],
-    );
+        organism.metadata
+            .filter((field: Metadata) => !inputFieldNames.has(field.name))
+            .forEach((field) => {
+                const header = field.header ?? 'Other';
+                if (groupedFields.has(header)) groupedFields.get(header)?.push(field);
+                else groupedFields.set(header, [field]);
+            });
+        return groupedFields;
+    }, [organism]);
 
     return (
-        <div className='mt-6'>
+        <div className='mt-6 mb-2'>
             <h1 className='text-2xl font-bold mb-4'>{organism.displayName}</h1>
             <BoxWithTabsTabBar>
                 <BoxWithTabsTab
@@ -70,31 +66,26 @@ export const OrganismMetadataTable: FC<Props> = ({ organism }) => {
                             </a>
                         </div>
                         {Array.from(organism.groupedInputFields.entries()).map(([header, inputFields]) => (
-                            <div key={header} className='mb-8'>
-                                <h3
-                                    className='text-lg font-semibold mb-4 cursor-pointer'
-                                    onClick={() => toggleHeader(header)}
-                                >
-                                    {header}
-                                    <IwwaArrowDown
-                                        className={`inline-block -mt-1 ml-1 h-4 w-4 transition-transform duration-300 ${
-                                            expandedHeaders.has(header) ? '' : '-rotate-90'
-                                        }`}
-                                    />
-                                </h3>
-                                <div
-                                    className={`transition-all duration-300 ${expandedHeaders.has(header) ? 'block' : 'sr-only'}`}
-                                    data-table-header={header}
-                                >
-                                    <MetadataTable fields={inputFields} metadata={organism.metadata} isInputFields />
-                                </div>
-                            </div>
+                            <MetadataTableSection
+                                key={header}
+                                header={header}
+                                metadata={organism.metadata}
+                                fields={inputFields}
+                                isInputFields
+                            />
                         ))}
                     </div>
                 )}
                 {activeTab === FieldType.GENERATED && (
                     <div className='mt-4'>
-                        <MetadataTable fields={generatedFields} metadata={organism.metadata} />
+                        {Array.from(groupedGeneratedFields.entries()).map(([header, generatedFields]) => (
+                            <MetadataTableSection
+                                key={header}
+                                header={header}
+                                metadata={organism.metadata}
+                                fields={generatedFields}
+                            />
+                        ))}
                     </div>
                 )}
             </BoxWithTabsBox>
@@ -102,42 +93,76 @@ export const OrganismMetadataTable: FC<Props> = ({ organism }) => {
     );
 };
 
-type TableProps = {
-    fields: InputField[];
-    metadata: Metadata[];
-    isInputFields?: boolean;
+type MetadataTableProps =
+    | { metadata: Metadata[]; fields: Metadata[]; isInputFields?: false }
+    | { metadata: Metadata[]; fields: InputField[]; isInputFields: true };
+
+type MetadataTableSectionProps = MetadataTableProps & { header: string };
+
+const MetadataTableSection: FC<MetadataTableSectionProps> = ({ header, ...metadataTableProps }) => {
+    const [expandedHeader, setExpandedHeader] = useState<boolean>(true);
+
+    return (
+        <div key={header} className='mb-8'>
+            <h3
+                className='text-lg font-semibold mb-4 cursor-pointer'
+                onClick={() => setExpandedHeader((prev) => !prev)}
+            >
+                {header}
+                <IwwaArrowDown
+                    className={`inline-block -mt-1 ml-1 h-4 w-4 transition-transform duration-300 ${
+                        expandedHeader ? '' : '-rotate-90'
+                    }`}
+                />
+            </h3>
+            <div
+                className={`transition-all duration-300 ${expandedHeader ? 'block' : 'sr-only'}`}
+                data-table-header={header}
+            >
+                <MetadataTable {...metadataTableProps} />
+            </div>
+        </div>
+    );
 };
 
-const MetadataTable: FC<TableProps> = ({ fields, metadata, isInputFields = false }) => {
+const MetadataTable: FC<MetadataTableProps> = (props) => {
     return (
         <table className='table-auto border-collapse border border-gray-200 w-full'>
             <thead>
                 <tr>
                     <th className='border border-gray-300 px-4 py-2 w-[25%]'>Field name</th>
-                    {isInputFields && <th className='border border-gray-300 px-4 py-2 w-[13%]'>Type</th>}
+                    {props.isInputFields && <th className='border border-gray-300 px-4 py-2 w-[13%]'>Type</th>}
                     <th className='border border-gray-300 px-4 py-2 w-[37%]'>Description</th>
-                    {isInputFields && <th className='border border-gray-300 px-4 py-2 w-[25%]'>Example</th>}
+                    {props.isInputFields && <th className='border border-gray-300 px-4 py-2 w-[25%]'>Example</th>}
                 </tr>
             </thead>
             <tbody>
-                {fields.map((field) => {
-                    const metadataEntry = metadata.find((meta) => meta.name === field.name);
-
-                    return (
-                        <tr key={field.name}>
-                            <td className='border border-gray-300 px-4 py-2'>{field.name}</td>
-                            {isInputFields && (
-                                <td className='border border-gray-300 px-4 py-2'>{metadataEntry?.type ?? 'String'}</td>
-                            )}
-                            <td className='border border-gray-300 px-4 py-2'>
-                                <FormattedText text={[field.definition, field.guidance].filter(Boolean).join(' ')} />
-                            </td>
-                            {isInputFields && (
-                                <td className='border border-gray-300 px-4 py-2'>{field.example ?? ''}</td>
-                            )}
-                        </tr>
-                    );
-                })}
+                {props.isInputFields
+                    ? props.fields.map((field) => {
+                          const metadataEntry = props.metadata.find((meta) => meta.name === field.name);
+                          return (
+                              <tr key={field.name}>
+                                  <td className='border border-gray-300 px-4 py-2'>{field.name}</td>
+                                  <td className='border border-gray-300 px-4 py-2'>
+                                      {metadataEntry?.type ?? 'string'}
+                                  </td>
+                                  <td className='border border-gray-300 px-4 py-2'>
+                                      <FormattedText
+                                          text={[field.definition, field.guidance].filter(Boolean).join(' ')}
+                                      />
+                                  </td>
+                                  <td className='border border-gray-300 px-4 py-2'>{field.example ?? ''}</td>
+                              </tr>
+                          );
+                      })
+                    : props.fields.map((field) => (
+                          <tr key={field.name}>
+                              <td className='border border-gray-300 px-4 py-2'>{field.name}</td>
+                              <td className='border border-gray-300 px-4 py-2'>
+                                  <FormattedText text={field.definition ?? ''} />
+                              </td>
+                          </tr>
+                      ))}
             </tbody>
         </table>
     );
