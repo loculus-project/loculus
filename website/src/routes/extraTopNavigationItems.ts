0b7102cc8d7f8b71d6ae5d99507ecf71575636c8
tombch <t.brier@outlook.com>
2026-04-10 11:59:49 +0200

    Basic docs in loculus for demo

--- website/src/routes/extraTopNavigationItems.ts
+++ website/src/routes/extraTopNavigationItems.ts
@@ -1,7 +1,12 @@
 // The following code is in a separate file so that it can be overwritten by Pathoplexus.
 import type { TopNavigationItems } from './navigationItems.ts';
 
-export const extraStaticTopNavigationItems = [];
+export const extraStaticTopNavigationItems = [
+    {
+        text: 'Docs',
+        path: '/docs/',
+    },
+];
 
 export const extraSequenceRelatedTopNavigationItems = (_: string | undefined): TopNavigationItems => {
     return [];
