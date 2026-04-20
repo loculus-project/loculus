777d8f0a794b3b89b0aba96f37b2c69a8502908b
tombch <t.brier@outlook.com>
2026-04-20 14:19:36 +0200

    Removed docs from navigation bar

--- website/src/routes/extraTopNavigationItems.ts
+++ website/src/routes/extraTopNavigationItems.ts
@@ -1,12 +1,7 @@
 // The following code is in a separate file so that it can be overwritten by Pathoplexus.
 import type { TopNavigationItems } from './navigationItems.ts';
 
-export const extraStaticTopNavigationItems: TopNavigationItems = [
-    {
-        text: 'Docs',
-        path: '/docs/',
-    },
-];
+export const extraStaticTopNavigationItems = [];
 
 export const extraSequenceRelatedTopNavigationItems = (_: string | undefined): TopNavigationItems => {
     return [];
