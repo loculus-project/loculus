3fab47d892c44c29b893c75b0def07b520b814c5
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-04-21 16:27:11 +0200

    feat(deposition): ignore accessions in suppressed list (even if not revoked)

--- ena-submission/scripts/get_ena_submission_list.py
+++ ena-submission/scripts/get_ena_submission_list.py
@@ -183,12 +183,12 @@ def filter_for_submission(
         else:
             # If lower version had external metadata and this one doesn't, remove it from that set
             entries_with_external_metadata.discard(accession_version.accession)
+        if accession_version in suppressed_accessions:
+            logger.debug(f"Skipping suppressed accession: {accession_version}")
+            continue
         entries_to_submit[accession_version.accession] = entry
         if entry["metadata"].get("isRevocation", False):
-            if accession_version in suppressed_accessions:
-                logger.debug(f"Skipping suppressed accession: {accession_version}")
-                entries_to_submit.pop(accession_version.accession)
-            else:
+            if accession_version not in suppressed_accessions:
                 logger.debug(f"Found revoked sequence: {accession_version}")
                 revoked_entries.add(accession_version.accession)
             entries_with_external_metadata.discard(accession_version.accession)
