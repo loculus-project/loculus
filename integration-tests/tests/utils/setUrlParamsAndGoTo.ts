c7c3cb6927a06626808d9e018704c7f92da720a0
Cornelius Roemer <cornelius.roemer@gmail.com>
2026-05-07 17:17:45 +0200

    Address comments

--- integration-tests/tests/utils/setUrlParamsAndGoTo.ts
+++ integration-tests/tests/utils/setUrlParamsAndGoTo.ts
@@ -3,18 +3,14 @@ import { Page } from '@playwright/test';
 type UrlParamValue = string | number | boolean;
 
 /**
- * Safe way to add/update/remove URL search params and navigate to the new URL. Handles relative URLs correctly.
+ * Safe way to add/update URL search params and navigate to the new URL.
  * Should be used in place of `page.goto(page.url() + '?column_submissionId=true');`
  */
 export async function setUrlParamsAndGoTo(page: Page, params: Record<string, UrlParamValue>) {
     const url = new URL(page.url());
 
     for (const [key, value] of Object.entries(params)) {
-        if (value === undefined || value === null) {
-            url.searchParams.delete(key);
-        } else {
-            url.searchParams.set(key, String(value));
-        }
+        url.searchParams.set(key, String(value));
     }
 
     await page.goto(url.toString());
