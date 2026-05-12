2cebc829cc9b53f59f9a7ef42d0475bfdf43908f
tombch <t.brier@outlook.com>
2026-05-12 15:28:05 +0200

    Slighlty move tooltip

--- website/src/components/SearchPage/fields/MultiFieldSearchField.tsx
+++ website/src/components/SearchPage/fields/MultiFieldSearchField.tsx
@@ -30,7 +30,7 @@ export const MultiFieldSearchField = ({
                     autoComplete='off'
                 />
             </DisabledUntilHydrated>
-            <div className='absolute top-1/2 -translate-y-1/3 right-1'>
+            <div className='absolute top-1/2 -translate-y-1/3 right-1.5'>
                 <span
                     data-tooltip-id={tooltipId}
                     className='text-gray-400 hover:text-primary-600 cursor-help inline-flex'
