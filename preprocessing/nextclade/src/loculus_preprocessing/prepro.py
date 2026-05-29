75fd261dc4f48f36c1c4675cf7f7b434d7f5cc84
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-05-29 10:55:46 +0200

    also parse cladeNodeAttrFounderInfo.outbreak.aaMutations.*.privateSubstitutions correctly

--- preprocessing/nextclade/src/loculus_preprocessing/prepro.py
+++ preprocessing/nextclade/src/loculus_preprocessing/prepro.py
@@ -196,6 +196,9 @@ def add_nextclade_metadata(
         case "cladeFounderInfo.aaMutations.*.privateSubstitutions":
             result = None if raw is None else str(raw)
             return process_mutations_from_clade_founder(result, spec.args)
+        case "cladeNodeAttrFounderInfo.outbreak.aaMutations.*.privateSubstitutions":
+            result = None if raw is None else str(raw)
+            return process_mutations_from_clade_founder(result, spec.args)
         case "privateAaMutations.*.labeledSubstitutions.substitution":
             result = None if raw is None else str(raw)
             return process_labeled_mutations(result, spec.args)
