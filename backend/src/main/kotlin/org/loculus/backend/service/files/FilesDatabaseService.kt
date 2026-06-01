0443280e490120a933491fff15738c087e4e1dc0
maverbiest <maxaverbiest@gmail.com>
2026-06-01 11:18:45 +0200

    Update query

--- backend/src/main/kotlin/org/loculus/backend/service/files/FilesDatabaseService.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/files/FilesDatabaseService.kt
@@ -68,12 +68,10 @@ class FilesDatabaseService(private val dateProvider: DateProvider) {
 
     fun getOrphanedFileIds(threshold: LocalDateTime): Set<FileId> {
         val sql = """
+            -- check for files for which an upload was requested > threshold days ago
+            -- but are not referenced by a submission. For this, check the unprocessed_data
+            -- and processed_data jsonb objects, but not original_data
             WITH referenced AS (
-                SELECT (fil->>'fileId')::uuid AS file_id
-                FROM sequence_entries,
-                   LATERAL jsonb_each(COALESCE(original_data->'files','{}'::jsonb)) AS cat(k,v),
-                   LATERAL jsonb_array_elements(cat.v) AS fil
-                UNION
                 SELECT (fil->>'fileId')::uuid
                 FROM sequence_entries,
                    LATERAL jsonb_each(COALESCE(unprocessed_data->'files','{}'::jsonb)) AS cat(k,v),
