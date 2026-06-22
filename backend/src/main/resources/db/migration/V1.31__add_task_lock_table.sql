698431c08272c3c606e5655ce666f5d496986b6f
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-06-22 17:10:16 +0200

    remove default

--- backend/src/main/resources/db/migration/V1.31__add_task_lock_table.sql
+++ backend/src/main/resources/db/migration/V1.31__add_task_lock_table.sql
@@ -1,5 +1,5 @@
 create table task_lock (
     task_name  text primary key,
     started_at timestamptz not null default now(),
-    locked_until timestamptz not null default now()
+    locked_until timestamptz not null
 );
