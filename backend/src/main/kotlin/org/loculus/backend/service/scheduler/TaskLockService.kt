e326bc7913ace24c374dd395446bf981782bae08
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2026-06-22 17:39:53 +0200

    Update backend/src/main/kotlin/org/loculus/backend/service/scheduler/TaskLockService.kt
    Co-authored-by: Cornelius Roemer <cornelius.roemer@gmail.com>

--- backend/src/main/kotlin/org/loculus/backend/service/scheduler/TaskLockService.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/scheduler/TaskLockService.kt
@@ -13,6 +13,13 @@ private val log = KotlinLogging.logger {}
 const val TASK_LOCK_TABLE_NAME = "task_lock"
 
 @Service
+/**
+ * Lock can be acquired when current time > `locked_until` (or no row for this task exists)
+ * When acquiring, `locked_until` is set to `currentTime + maxLockFactor * frequencyIntervalSeconds`
+ * When releasing, `locked_until` is reduced to `started_at + minLockFactor * frequencyIntervalSeconds`
+ * maxLockFactor prevents concurrent execution even if tasks run longer than the interval
+ * minLockFactor prevents multiple backends from starting new tasks before the interval is almost over
+ */
 class TaskLockService(
     @Value("\${loculus.task-lock.min-lock-factor:0.9}") private val minLockFactor: Double,
     @Value("\${loculus.task-lock.max-lock-factor:5.0}") private val maxLockFactor: Double,
