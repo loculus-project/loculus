24ddc94f497e742947f1f026700460cd3a547d90
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-06-24 10:41:10 +0200

    use AOP for auxtables as well

--- backend/src/main/kotlin/org/loculus/backend/service/submission/CleanUpAuxTableTask.kt
+++ backend/src/main/kotlin/org/loculus/backend/service/submission/CleanUpAuxTableTask.kt
@@ -5,7 +5,7 @@ import kotlinx.datetime.minus
 import kotlinx.datetime.toLocalDateTime
 import org.loculus.backend.config.BackendSpringProperty
 import org.loculus.backend.log.AuditLogger
-import org.loculus.backend.service.scheduler.TaskLockService
+import org.loculus.backend.service.scheduler.TaskLock
 import org.loculus.backend.service.submission.UploadDatabaseService
 import org.loculus.backend.utils.DateProvider
 import org.springframework.beans.factory.annotation.Value
@@ -20,7 +20,6 @@ const val CLEAN_UP_AUX_TABLE_TASK_NAME = "clean-up-aux-table"
 @Component
 class CleanUpAuxTableTask(
     private val uploadDatabaseService: UploadDatabaseService,
-    private val taskLockService: TaskLockService,
     private val dateProvider: DateProvider,
     private val auditLogger: AuditLogger,
     @Value("\${${BackendSpringProperty.CLEAN_UP_AUX_TABLE_RUN_EVERY_HOURS}}")
@@ -32,33 +31,24 @@ class CleanUpAuxTableTask(
      * Deletes auxTable entries older than 24 hours.
      */
     @Scheduled(fixedDelay = 1, timeUnit = java.util.concurrent.TimeUnit.HOURS)
+    @TaskLock(
+        name = CLEAN_UP_AUX_TABLE_TASK_NAME,
+        intervalString = "\${${BackendSpringProperty.CLEAN_UP_AUX_TABLE_RUN_EVERY_HOURS}}",
+        timeUnit = TimeUnit.HOURS,
+    )
     fun task() {
-        if (!taskLockService.acquireLock(
-                CLEAN_UP_AUX_TABLE_TASK_NAME,
-                frequencyIntervalSeconds = TimeUnit.HOURS.toSeconds(runEveryHours),
-            )
-        ) {
-            return
-        }
-        try {
-            val hourCutoff = 24L
-            val now = dateProvider.getCurrentInstant()
-            val thresholdInstant = now.minus(
-                hourCutoff,
-                DateTimeUnit.HOUR,
-                DateProvider.timeZone,
-            ).toLocalDateTime(DateProvider.timeZone)
-            val deletedCount = uploadDatabaseService.deleteAuxTableEntriesOlderThan(thresholdInstant)
+        val hourCutoff = 24L
+        val now = dateProvider.getCurrentInstant()
+        val thresholdInstant = now.minus(
+            hourCutoff,
+            DateTimeUnit.HOUR,
+            DateProvider.timeZone,
+        ).toLocalDateTime(DateProvider.timeZone)
+        val deletedCount = uploadDatabaseService.deleteAuxTableEntriesOlderThan(thresholdInstant)
 
-            if (deletedCount > 0) {
-                log.info { "Deleted $deletedCount auxTable entries older than $hourCutoff" }
-                auditLogger.log("CLEANUP", "Deleted $deletedCount auxTable entries older than $hourCutoff hours.")
-            }
-        } finally {
-            taskLockService.releaseLock(
-                CLEAN_UP_AUX_TABLE_TASK_NAME,
-                frequencyIntervalSeconds = TimeUnit.HOURS.toSeconds(runEveryHours),
-            )
+        if (deletedCount > 0) {
+            log.info { "Deleted $deletedCount auxTable entries older than $hourCutoff" }
+            auditLogger.log("CLEANUP", "Deleted $deletedCount auxTable entries older than $hourCutoff hours.")
         }
     }
 }
