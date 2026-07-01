d3199ef3b358fec532228046451e21e5a0c556b5
tombch <t.brier@outlook.com>
2026-07-01 13:45:43 +0200

    Remove unused zero case handling

--- website/src/components/SeqSetCitations/CitationList.tsx
+++ website/src/components/SeqSetCitations/CitationList.tsx
@@ -26,25 +26,22 @@ const CitationList: FC<CitationListProps> = ({ citations, maxDisplayedCitations,
             >
                 <CitationTable isLoading={false} error={null} citations={citations} />
             </BaseDialog>
-            {citations.length > 0 && (
-                <div className='space-y-2'>
-                    <ul className='space-y-4'>
-                        {(maxDisplayedCitations !== undefined
-                            ? citations.slice(0, maxDisplayedCitations)
-                            : citations
-                        ).map((citation: SeqSetCitation | SequenceCitation) => (
+            <div className='space-y-2'>
+                <ul className='space-y-4'>
+                    {(maxDisplayedCitations !== undefined ? citations.slice(0, maxDisplayedCitations) : citations).map(
+                        (citation: SeqSetCitation | SequenceCitation) => (
                             <li key={citation.source.sourceDOI}>
                                 <CitationDetails citation={citation} className='text-sm' displayYear />
                             </li>
-                        ))}
-                    </ul>
-                    {displayCitationsModalButton && (
-                        <Button className='text-sm hover:underline' onClick={() => setIsOpen(true)}>
-                            View all citations ({citations.length})...
-                        </Button>
+                        ),
                     )}
-                </div>
-            )}
+                </ul>
+                {displayCitationsModalButton && (
+                    <Button className='text-sm hover:underline' onClick={() => setIsOpen(true)}>
+                        View all citations ({citations.length})...
+                    </Button>
+                )}
+            </div>
         </div>
     );
 };
