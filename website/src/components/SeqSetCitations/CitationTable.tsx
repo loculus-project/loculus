0f56d4c55580208fd48b6249e39c79703d03b8eb
theosanderson-agent <theo@theo.io>
2026-07-16 14:06:35 +0100

    fix(website): order citation formatter import

--- website/src/components/SeqSetCitations/CitationTable.tsx
+++ website/src/components/SeqSetCitations/CitationTable.tsx
@@ -1,8 +1,8 @@
 import { type FC } from 'react';
 
+import { formatCitationContributors } from './formatCitationContributors';
 import { routes } from '../../routes/routes';
 import { type SeqSetCitation, type SequenceCitation } from '../../types/seqSetCitation';
-import { formatCitationContributors } from './formatCitationContributors';
 
 interface CitationTableProps {
     isLoading: boolean;
