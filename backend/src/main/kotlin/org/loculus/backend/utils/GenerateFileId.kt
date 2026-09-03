67d02a8f9654d91db8889c697455b8b5ef82a949
maverbiest <maxaverbiest@gmail.com>
2026-09-03 16:48:34 +0200

    Make file id util, FileId UUID -> String

--- backend/src/main/kotlin/org/loculus/backend/utils/GenerateFileId.kt
+++ backend/src/main/kotlin/org/loculus/backend/utils/GenerateFileId.kt
@@ -1,5 +1,15 @@
 package org.loculus.backend.utils
 
-import java.util.UUID
+import org.loculus.backend.service.CODE_POINTS
+import org.loculus.backend.service.files.FileId
+import java.security.SecureRandom
 
-fun generateFileId() = UUID.randomUUID()
+private const val FILE_ID_PREFIX = "FILE_"
+private const val FILE_ID_RANDOM_PART_LENGTH = 12
+
+private val secureRandom = SecureRandom()
+
+fun generateFileId(): FileId = buildString {
+    append(FILE_ID_PREFIX)
+    repeat(FILE_ID_RANDOM_PART_LENGTH) { append(CODE_POINTS[secureRandom.nextInt(CODE_POINTS.length)]) }
+}
