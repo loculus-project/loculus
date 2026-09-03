e9480d6be08a58b4d6fac9d17d905bcc93ff6205
anna-parker <50943381+anna-parker@users.noreply.github.com>
2026-09-03 19:56:45 +0200

    refactor(deposition): replace add_to_*_table functions with one add_to_db function - this is useful as we will soon have more tables

--- ena-submission/src/ena_deposition/create_project.py
+++ ena-submission/src/ena_deposition/create_project.py
@@ -34,7 +34,7 @@ from .submission_db_helper import (
     Status,
     StatusAll,
     SubmissionTableEntry,
-    add_to_project_table,
+    add_to_db,
     db_init,
     find_conditions_in_db,
     find_errors_or_stuck_in_db,
@@ -206,7 +206,7 @@ def sync_state_with_submission_table(db_engine: Engine, config: Config):
             )
             continue
 
-        project_id = add_to_project_table(
+        project_row = add_to_db(
             db_engine,
             ProjectTableEntry(
                 group_id=row.group_id,
@@ -216,8 +216,9 @@ def sync_state_with_submission_table(db_engine: Engine, config: Config):
                 else None,
             ),
         )
-        if not project_id:
+        if not project_row:
             continue
+        project_id = project_row.project_id
         update_db_where_conditions(
             db_engine,
             model_class=SubmissionTableEntry,
